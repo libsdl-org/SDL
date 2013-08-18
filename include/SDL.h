a46437f798d1a2eb4159a5fba201e1669b6ff619
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2013-08-18 11:15:30 +0200

    Replaced introduction in header file with more recent version from README.txt.

--- include/SDL.h
+++ include/SDL.h
@@ -32,18 +32,20 @@
  *
  *  \section intro_sec Introduction
  *
- *  This is the Simple DirectMedia Layer, a general API that provides low
- *  level access to audio, keyboard, mouse, joystick, 3D hardware via OpenGL,
- *  and 2D framebuffer across multiple platforms.
+ *  Simple DirectMedia Layer is a cross-platform development library designed
+ *  to provide low level access to audio, keyboard, mouse, joystick, and
+ *  graphics hardware via OpenGL and Direct3D. It is used by video playback
+ *  software, emulators, and popular games including Valve's award winning
+ *  catalog and many Humble Bundle games.
  *
- *  SDL is written in C, but works with C++ natively, and has bindings to
- *  several other languages, including Ada, C#, Eiffel, Erlang, Euphoria,
- *  Guile, Haskell, Java, Lisp, Lua, ML, Objective C, Pascal, Perl, PHP,
- *  Pike, Pliant, Python, Ruby, and Smalltalk.
+ *  SDL officially supports Windows, Mac OS X, Linux, iOS, and Android.
+ *  Support for other platforms may be found in the source code.
  *
- *  This library is distributed under the zlib license, which can be
- *  found in the file  "COPYING".  This license allows you to use SDL
- *  freely for any purpose as long as you retain the copyright notice.
+ *  SDL is written in C, works natively with C++, and there are bindings 
+ *  available for several other languages, including C# and Python.
+ *
+ *  This library is distributed under the zlib license, which can be found
+ *  in the file "COPYING.txt".
  *
  *  The best way to learn how to use SDL is to check out the header files in
  *  the "include" subdirectory and the programs in the "test" subdirectory.
