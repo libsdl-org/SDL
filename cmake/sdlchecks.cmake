efa7502f5248a653a9ec7dc416ff5972b3ae9319
Gabriel Jacobo <gabomdq@gmail.com>
2013-08-22 14:56:07 -0300

    Separate EGL / GL ES detection in CMake

--- cmake/sdlchecks.cmake
+++ cmake/sdlchecks.cmake
@@ -563,25 +563,29 @@ macro(CheckOpenGLESX11)
   if(VIDEO_OPENGLES)
     check_c_source_compiles("
         #include <EGL/egl.h>
-        int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGLES)
-    if(HAVE_VIDEO_OPENGLES)
-      check_c_source_compiles("
-          #include <GLES/gl.h>
-          #include <GLES/glext.h>
-          int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGLES_V1)
-      if(HAVE_VIDEO_OPENGLES_V1)
+        int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGL_EGL)
+    if(HAVE_VIDEO_OPENGL_EGL)
+        set(SDL_VIDEO_OPENGL_EGL 1)
+    endif(HAVE_VIDEO_OPENGL_EGL) 
+    check_c_source_compiles("
+      #include <GLES/gl.h>
+      #include <GLES/glext.h>
+      int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGLES_V1)
+    if(HAVE_VIDEO_OPENGLES_V1)
+        set(HAVE_VIDEO_OPENGLES TRUE)
         set(SDL_VIDEO_OPENGL_ES 1)
         set(SDL_VIDEO_RENDER_OGL_ES 1)
-      endif(HAVE_VIDEO_OPENGLES_V1)
-      check_c_source_compiles("
-          #include <GLES2/gl2.h>
-          #include <GLES2/gl2ext.h>
-          int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGLES_V2)
-      if(HAVE_VIDEO_OPENGLES_V2)
+    endif(HAVE_VIDEO_OPENGLES_V1)
+    check_c_source_compiles("
+      #include <GLES2/gl2.h>
+      #include <GLES2/gl2ext.h>
+      int main (int argc, char** argv) {}" HAVE_VIDEO_OPENGLES_V2)
+    if(HAVE_VIDEO_OPENGLES_V2)
+        set(HAVE_VIDEO_OPENGLES TRUE)
         set(SDL_VIDEO_OPENGL_ES2 1)
         set(SDL_VIDEO_RENDER_OGL_ES2 1)
-      endif(HAVE_VIDEO_OPENGLES_V2)
-    endif(HAVE_VIDEO_OPENGLES)
+    endif(HAVE_VIDEO_OPENGLES_V2)
+
   endif(VIDEO_OPENGLES)
 endmacro(CheckOpenGLESX11)
 
