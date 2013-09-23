6004b764d86edb400a7a41aafb0fa9570bec91d8
David Ludwig <dludwig@pobox.com>
2013-09-22 23:17:25 -0400

    WinRT: added missing files

---
+++ src/core/winrt/SDL_winrtapp_common.cpp
@@ -0,0 +1,16 @@
+
+#include <SDL_system.h>
+#include "SDL_winrtapp_direct3d.h"
+#include "SDL_winrtapp_xaml.h"
+
+int (*WINRT_SDLAppEntryPoint)(int, char **) = NULL;
+
+extern "C" DECLSPEC int
+SDL_WinRTRunApp(int (*mainFunction)(int, char **), void * xamlBackgroundPanel)
+{
+    if (xamlBackgroundPanel) {
+        return SDL_WinRTInitXAMLApp(mainFunction, xamlBackgroundPanel);
+    } else {
+        return SDL_WinRTInitNonXAMLApp(mainFunction);
+    }
+}
