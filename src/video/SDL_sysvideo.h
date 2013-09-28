1a0f7719ad6db9a0339014a76892fa92da4ef0e6
Gabriel Jacobo <gabomdq@gmail.com>
2013-09-28 19:23:59 -0300

    Removes unused property use_egl from internal structure gl_config

--- src/video/SDL_sysvideo.h
+++ src/video/SDL_sysvideo.h
@@ -292,7 +292,6 @@ struct SDL_VideoDevice
         int minor_version;
         int flags;
         int profile_mask;
-        int use_egl;
         int share_with_current_context;
         int retained_backing;
         int driver_loaded;
