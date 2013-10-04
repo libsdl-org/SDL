9c489c7c0dae85262552e24e1f11e627bab23c34
Gabriel Jacobo <gabomdq@gmail.com>
2013-10-04 08:23:37 -0300

    Fix EGL/OpenGL ES paths

--- src/video/SDL_egl.c
+++ src/video/SDL_egl.c
@@ -25,12 +25,23 @@
 #include "SDL_sysvideo.h"
 #include "SDL_egl.h"
 
+
 #if SDL_VIDEO_DRIVER_RPI
+/* Raspbian places the OpenGL ES/EGL binaries in a non standard path */
 #define DEFAULT_EGL "/opt/vc/lib/libEGL.so"
 #define DEFAULT_OGL_ES2 "/opt/vc/lib/libGLESv2.so"
 #define DEFAULT_OGL_ES_PVR "/opt/vc/lib/libGLES_CM.so"
 #define DEFAULT_OGL_ES "/opt/vc/lib/libGLESv1_CM.so"
+
+#elif SDL_VIDEO_DRIVER_ANDROID
+/* Android */
+#define DEFAULT_EGL "libEGL.so"
+#define DEFAULT_OGL_ES2 "libGLESv2.so"
+#define DEFAULT_OGL_ES_PVR "libGLES_CM.so"
+#define DEFAULT_OGL_ES "libGLESv1_CM.so"
+
 #else
+/* Desktop Linux */
 #define DEFAULT_EGL "libEGL.so.1"
 #define DEFAULT_OGL_ES2 "libGLESv2.so.2"
 #define DEFAULT_OGL_ES_PVR "libGLES_CM.so.1"
