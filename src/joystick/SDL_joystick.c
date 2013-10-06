8f46bcfd6dfeea0fb476499d24758db5dced6379
Sam Lantinga <slouken@libsdl.org>
2013-10-06 13:49:23 -0700

    Check for NULL joystick in SDL_JoystickGetGUID()

--- src/joystick/SDL_joystick.c
+++ src/joystick/SDL_joystick.c
@@ -763,6 +763,11 @@ SDL_JoystickGUID SDL_JoystickGetDeviceGUID(int device_index)
 /* return the guid for this opened device */
 SDL_JoystickGUID SDL_JoystickGetGUID(SDL_Joystick * joystick)
 {
+    if (!SDL_PrivateJoystickValid(joystick)) {
+        SDL_JoystickGUID emptyGUID;
+        SDL_zero( emptyGUID );
+        return emptyGUID;
+    }
     return SDL_SYS_JoystickGetGUID( joystick );
 }
 
