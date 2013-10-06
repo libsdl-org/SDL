d8bdf0cf4bff28499f7b554bc0686c2ceb13f960
Sam Lantinga <slouken@libsdl.org>
2013-10-05 21:15:55 -0700

    Fixed bug where a Logitech wireless keyboard with built-in mouse touchpad didn't get recongized as both devices.

--- src/input/evdev/SDL_evdev.h
+++ src/input/evdev/SDL_evdev.h
@@ -29,17 +29,10 @@
 #include "SDL_events.h"
 #include <sys/stat.h>
 
-typedef enum
-{
-    SDL_EVDEV_DEVICE_MOUSE = 0x0001,
-    SDL_EVDEV_DEVICE_KEYBOARD
-} SDL_EVDEV_deviceclass;
-
 typedef struct SDL_evdevlist_item
 {
     char *path;
     int fd;
-    SDL_EVDEV_deviceclass devclass;
     struct SDL_evdevlist_item *next;
 } SDL_evdevlist_item;
 
