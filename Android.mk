4349cb57691125bbc24163e6a4f13b981262b2d8
Sam Lantinga <slouken@libsdl.org>
2013-10-10 21:51:39 -0700

    Fixed bug 2146 - Enable static linking of libSDL on Android
    Denis Bernard
    
    This patch to Android.mk adds support for static linking of libSDL for Android applications. A patched readme with static build instructions is also provided.
    
    It does not break existing build environments setup according to the README-android.txt since the static library version will not be built in not required.
    
    The static build uses the Android NDK module system (see docs/IMPORT-MODULE.html in the NDK folder and step 5 in the instructions below).
    
    Instructions:
    1. Copy the android-project directory wherever you want to keep your projects
       and rename it to the name of your project.
    2. Create a symlink to SDL/src/main/android/SDL_android_main.c as
       <project>/jni/src/SDL_android_main.c
    3. Rename <project>/jni/src/Android_static.mk to <project>/jni/src/Android.mk
       (overwrite the existing one)
    4. Edit <project>/jni/src/Android.mk to include your source files
    5. create and export an environment variable named NDK_MODULE_PATH that points
       to the parent directory of this SDL directory. e.g.:
    
       export NDK_MODULE_PATH="$PWD"/..
    
    6. Edit <project>/src/org/libsdl/app/SDLActivity.java and remove the call to
       System.loadLibrary("SDL2") line 42.
    7. Run 'ndk-build' (a script provided by the NDK). This compiles the C source
    
    Although this requires an environment variable to be setup, it can be added once and for all to the main Android.mk of the project.

--- Android.mk
+++ Android.mk
@@ -49,3 +49,20 @@ LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES
 LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
+
+###########################
+#
+# SDL static library
+#
+###########################
+
+LOCAL_MODULE := SDL2_static
+
+LOCAL_MODULE_FILENAME := libSDL2
+
+LOCAL_SRC_FILES += $(LOCAL_PATH)/src/main/android/SDL_android_main.c
+
+LOCAL_LDLIBS := 
+LOCAL_EXPORT_LDLIBS := -Wl,--undefined=Java_org_libsdl_app_SDLActivity_nativeInit -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
+
+include $(BUILD_STATIC_LIBRARY)
