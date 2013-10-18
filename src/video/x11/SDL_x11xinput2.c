b4a00144fb6fcd5ffef68c8c4ecc58129b43621e
Ryan C. Gordon <icculus@icculus.org>
2013-10-18 10:56:45 -0400

    Fixed the XInput2 X11 symbols.

--- src/video/x11/SDL_x11xinput2.c
+++ src/video/x11/SDL_x11xinput2.c
@@ -179,7 +179,7 @@ X11_InitXinput2Multitouch(_THIS)
     SDL_VideoData *data = (SDL_VideoData *) _this->driverdata;
     XIDeviceInfo *info;
     int ndevices,i,j;
-    info = XIQueryDevice(data->display, XIAllMasterDevices, &ndevices);
+    info = X11_XIQueryDevice(data->display, XIAllMasterDevices, &ndevices);
 
     for (i = 0; i < ndevices; i++) {
         XIDeviceInfo *dev = &info[i];
@@ -198,7 +198,7 @@ X11_InitXinput2Multitouch(_THIS)
             }
         }
     }
-    XIFreeDeviceInfo(info);
+    X11_XIFreeDeviceInfo(info);
 #endif
 }
 
