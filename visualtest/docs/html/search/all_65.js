8d83cf65fb892934008f596b6b26aff8eb7ef5fc
Ryan C. Gordon <icculus@icculus.org>
2013-11-10 00:32:23 -0500

    Added Apoorv Upreti's GSoC2013 work: visual test suite.
    See visualtest/README.txt for details.

---
+++ visualtest/docs/html/search/all_65.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['enum_5fvalues',['enum_values',['../struct_s_d_l_visual_test___s_u_t_option.html#a596ff3567c4b736561dba1915a2cd38d',1,'SDLVisualTest_SUTOption']]],
+  ['enumerated',['enumerated',['../union_s_d_l_visual_test___s_u_t_option_value.html#ad40e26afd4b8532327b61897d5b009e3',1,'SDLVisualTest_SUTOptionValue']]],
+  ['exit_5fstatus',['exit_status',['../struct_s_d_l___process_exit_status.html#a9324a9ff7dc6697dd77f02998d5e77d7',1,'SDL_ProcessExitStatus']]],
+  ['exit_5fsuccess',['exit_success',['../struct_s_d_l___process_exit_status.html#a51df50c07437f2e816d6ce7ce99e1cac',1,'SDL_ProcessExitStatus']]],
+  ['extra',['extra',['../struct_s_d_l_visual_test___action.html#a4626514a67f261290b54fb3d85ca8ddd',1,'SDLVisualTest_Action']]]
+];
---
+++ visualtest/docs/html/search/files_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rwhelper_2ec',['rwhelper.c',['../rwhelper_8c.html',1,'']]]
+];
