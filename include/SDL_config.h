143d760dad40ab2a9727f0c02146ddddd5fb87d7
Ryan C. Gordon <icculus@icculus.org>
2013-11-10 00:38:37 -0500

    Added Ben Henning's GSoC2013 work: premake build system.

--- include/SDL_config.h
+++ include/SDL_config.h
@@ -29,7 +29,9 @@
  */
 
 /* Add any platform that doesn't build using the configure system. */
-#if defined(__WIN32__)
+#ifdef USING_PREMAKE_CONFIG_H
+#include "SDL_config_premake.h"
+#elif defined(__WIN32__)
 #include "SDL_config_windows.h"
 #elif defined(__MACOSX__)
 #include "SDL_config_macosx.h"
