04afd61cd9b270e12be187c4d0040aac6cd790ea
Gabriel Jacobo <gabomdq@gmail.com>
2013-11-11 11:12:43 -0300

    [Android] Try to improve handling of some dpad+gamepad+keyboard corner cases

--- android-project/src/org/libsdl/app/SDLActivity.java
+++ android-project/src/org/libsdl/app/SDLActivity.java
@@ -612,10 +612,11 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
                 } else if (event.getAction() == KeyEvent.ACTION_UP) {
                     SDLActivity.onNativePadUp(id, keyCode);
                 }
+                return true;
             }
-            return true;
         }
-        else if( (event.getSource() & InputDevice.SOURCE_KEYBOARD) != 0) {
+        
+        if( (event.getSource() & InputDevice.SOURCE_KEYBOARD) != 0) {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
                 //Log.v("SDL", "key down: " + keyCode);
                 SDLActivity.onNativeKeyDown(keyCode);
