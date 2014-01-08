5fd5567c4c3dc7d9a33aabc9f2457d2331929373
Edward Rudd <urkle@outoforder.cc>
2014-01-08 14:53:08 -0500

    define a font-size for the "unicode" message box font so the text isn't unreadable on linux systems

--- src/video/x11/SDL_x11messagebox.c
+++ src/video/x11/SDL_x11messagebox.c
@@ -48,7 +48,7 @@
 #define MIN_DIALOG_HEIGHT       100     /* Minimum dialog height */
 
 static const char g_MessageBoxFontLatin1[] = "-*-*-medium-r-normal--0-120-*-*-p-0-iso8859-1";
-static const char g_MessageBoxFont[] = "-*-*-*-*-*-*-*-*-*-*-*-*-*-*";
+static const char g_MessageBoxFont[] = "-*-*-*-*-*-*-*-120-*-*-*-*-*-*";
 
 static const SDL_MessageBoxColor g_default_colors[ SDL_MESSAGEBOX_COLOR_MAX ] = {
     { 56,  54,  53  }, /* SDL_MESSAGEBOX_COLOR_BACKGROUND, */
