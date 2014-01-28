93b05b733ad1fa8738bec3211cce4ada74c8b8ca
Gabriel Jacobo <gabomdq@gmail.com>
2014-01-28 11:39:37 -0300

    [Wayland] Fixes segfault when mouse enters window

--- src/video/wayland/SDL_waylandmouse.c
+++ src/video/wayland/SDL_waylandmouse.c
@@ -181,6 +181,7 @@ Wayland_CreateCursor(SDL_Surface *surface, int hot_x, int hot_y)
                    surface->h * surface->pitch);
 
         data->surface = wl_compositor_create_surface(wd->compositor);
+        wl_surface_set_user_data(data->surface, NULL);
         wl_surface_attach(data->surface,
                           data->buffer,
                           0,
@@ -214,6 +215,7 @@ CreateCursorFromWlCursor(SDL_VideoData *d, struct wl_cursor *wlcursor)
          * reference to it here */
         data->buffer = NULL;
         data->surface = wl_compositor_create_surface(d->compositor);
+        wl_surface_set_user_data(data->surface, NULL);
         wl_surface_attach(data->surface,
                           WAYLAND_wl_cursor_image_get_buffer(wlcursor->images[0]),
                           0,
