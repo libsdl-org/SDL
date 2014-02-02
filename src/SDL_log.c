58edac3e6917467d91d9e3063bf6fed32fe6acc0
Sam Lantinga <slouken@libsdl.org>
2014-02-02 00:53:27 -0800

    Fixed bug 2374 - Update copyright for 2014...
    Is it that time already??

--- src/SDL_log.c
+++ src/SDL_log.c
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2013 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
