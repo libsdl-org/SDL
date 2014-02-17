245e125e0f86f6f6ef311ed652c7766771a01c2e
Ryan C. Gordon <icculus@icculus.org>
2014-02-17 11:46:23 -0500

    Corrected battery percentage on iOS (thanks, Felix!).
    Fixes Bugzilla #2397.

--- src/power/uikit/SDL_syspower.m
+++ src/power/uikit/SDL_syspower.m
@@ -87,7 +87,7 @@ SDL_GetPowerInfo_UIKit(SDL_PowerState * state, int *seconds, int *percent)
     }
 
     const float level = [uidev batteryLevel];
-    *percent = ( (level < 0.0f) ? -1 : (((int) (level + 0.5f)) * 100) );
+    *percent = ( (level < 0.0f) ? -1 : ((int) ((level * 100) + 0.5f)) );
     return SDL_TRUE;            /* always the definitive answer on iPhoneOS. */
 }
 
