86655b64efe341f9a837e61bd2c1f591d549eecd
Sam Lantinga <slouken@libsdl.org>
2014-03-01 11:40:41 -0800

    Fixed compiler warning

--- src/video/windows/SDL_windowsvideo.c
+++ src/video/windows/SDL_windowsvideo.c
@@ -261,7 +261,7 @@ DXGI_LoadDLL( void **pDXGIDLL , IDXGIFactory **pDXGIFactory )
 			"CreateDXGIFactory");
 		if (CreateDXGI) {
 			GUID dxgiGUID = {0x7b7166ec,0x21c7,0x44ae,{0xb2,0x1a,0xc9,0xae,0x32,0x1a,0xe3,0x69}};
-			if( !SUCCEEDED( CreateDXGI( &dxgiGUID, pDXGIFactory ))) {
+			if( !SUCCEEDED( CreateDXGI( &dxgiGUID, (void**)pDXGIFactory ))) {
 				*pDXGIFactory = NULL;
 			}
 		}
