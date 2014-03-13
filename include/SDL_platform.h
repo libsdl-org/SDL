7124034ba93e9bc225ae4788d6213e5751e73efb
Sam Lantinga <slouken@libsdl.org>
2014-03-12 23:44:23 -0700

    Windows XP toolchain fix from Bruce Dawson: Fix to allow using SDL when compiling with v110_xp or v120_xp -- compiling with VS2012/VS2013 with the XP targeting option.
    In order to ensure that we can target Windows XP we compile with the v120_xp toolset instead of v120. This means that we use an earlier SDK version and it means that winapifamily.h is not available. Compiling for this old SDK can be detected using the _USING_V110_SDK71_ define which is set through the %(PreprocessorDefinitions) option.

--- include/SDL_platform.h
+++ include/SDL_platform.h
@@ -116,7 +116,8 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__)
 /* Try to find out if we're compiling for WinRT or non-WinRT */
-#if defined(__MINGW32__) || (defined(_MSC_VER) && (_MSC_VER >= 1700))	/* _MSC_VER==1700 for MSVC 2012 */
+/* If _USING_V110_SDK71_ is defined it means we are using the v110_xp or v120_xp toolset. */
+#if defined(__MINGW32__) || (defined(_MSC_VER) && (_MSC_VER >= 1700) && !_USING_V110_SDK71_)	/* _MSC_VER==1700 for MSVC 2012 */
 #include <winapifamily.h>
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
 #undef __WINDOWS__
