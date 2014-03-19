ff3f49f0730122203770b0045968bba9154e59bc
Ryan C. Gordon <icculus@icculus.org>
2014-03-19 13:22:09 -0400

    Reenable static builds for now with static analysis.

--- build-scripts/checker-buildbot.sh
+++ build-scripts/checker-buildbot.sh
@@ -61,7 +61,7 @@ fi
 mkdir checker-buildbot
 cd checker-buildbot
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER="$CHECKERDIR/libexec/ccc-analyzer" -DSDL_STATIC=OFF ..
-CC="$CHECKERDIR/libexec/ccc-analyzer" CFLAGS="-O0" ../configure --disable-static
+CC="$CHECKERDIR/libexec/ccc-analyzer" CFLAGS="-O0" ../configure
 PATH="$CHECKERDIR:$PATH" scan-build -o analysis $MAKE
 mv analysis/* ../analysis
 rmdir analysis   # Make sure this is empty.
