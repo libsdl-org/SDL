4bb6afefa2b3bc61dc69dc295cf1c025a6b661e6
Gabriel Jacobo <gabomdq@gmail.com>
2014-04-17 12:44:08 -0300

    Comment out eglWaitGL from Android_GLES_SwapWindow
    Let's hope for the best! If anyone has a device affected by this removal,
    please file a bug!

--- src/video/android/SDL_androidgl.c
+++ src/video/android/SDL_androidgl.c
@@ -41,9 +41,13 @@ SDL_EGL_MakeCurrent_impl(Android)
 void
 Android_GLES_SwapWindow(_THIS, SDL_Window * window)
 {
-    /* FIXME: These two functions were in the Java code, do we really need them? */
-    _this->egl_data->eglWaitNative(EGL_CORE_NATIVE_ENGINE);
-    _this->egl_data->eglWaitGL();
+    /* The following two calls existed in the original Java code
+     * If you happen to have a device that's affected by their removal,
+     * please report to Bugzilla. -- Gabriel
+     */
+    
+    /*_this->egl_data->eglWaitNative(EGL_CORE_NATIVE_ENGINE);
+    _this->egl_data->eglWaitGL();*/
     SDL_EGL_SwapBuffers(_this, ((SDL_WindowData *) window->driverdata)->egl_surface);
 }
 
