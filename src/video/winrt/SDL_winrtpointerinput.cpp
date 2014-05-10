d4f1d2257eb1c6e8249af7ff7cf2ce7181e9c115
David Ludwig <dludwig@pobox.com>
2014-05-09 20:32:06 -0400

    Fixed bug: touch input on WinPhone 8.0 was wrong, in some cases
    When a Windows Phone 8.0 app was rotated to anything but Portrait mode, touch
    input coordinates, as well as virtual mouse coordinates, were usually getting
    reported as coming from the wrong part of the screen.

--- src/video/winrt/SDL_winrtpointerinput.cpp
+++ src/video/winrt/SDL_winrtpointerinput.cpp
@@ -83,7 +83,7 @@ WINRT_TransformCursorPosition(SDL_Window * window,
     // Compute coordinates normalized from 0..1.
     // If the coordinates need to be sized to the SDL window,
     // we'll do that after.
-#if (WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP) || (NTDDI_VERSION >= NTDDI_WINBLUE)
+#if (WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP) || (NTDDI_VERSION > NTDDI_WIN8)
     outputPosition.X = rawPosition.X / nativeWindow->Bounds.Width;
     outputPosition.Y = rawPosition.Y / nativeWindow->Bounds.Height;
 #else
