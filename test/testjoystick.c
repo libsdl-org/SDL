821562e625a8d7eda67718973c73f1fe525a66d8
Gabriel Jacobo <gabomdq@gmail.com>
2014-05-10 15:57:09 -0300

    A few more ANDROID for __ANDROID__ replacements

--- test/testjoystick.c
+++ test/testjoystick.c
@@ -243,7 +243,7 @@ main(int argc, char *argv[])
         }
     }
 
-#ifdef ANDROID
+#ifdef __ANDROID__
     if (SDL_NumJoysticks() > 0) {
 #else
     if (argv[1]) {
@@ -252,7 +252,7 @@ main(int argc, char *argv[])
         SDL_bool keepGoing = SDL_TRUE;
         SDL_Event event;
         int device;
-#ifdef ANDROID
+#ifdef __ANDROID__
         device = 0;
 #else
         device = atoi(argv[1]);
