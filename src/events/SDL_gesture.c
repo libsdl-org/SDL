0d196c3099c9084d3c9fdca5cd52c1eb854e1efe
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2014-05-17 21:52:05 +0200

    Removed unused standard library includes from gesture source.

--- src/events/SDL_gesture.c
+++ src/events/SDL_gesture.c
@@ -27,13 +27,9 @@
 #include "SDL_events_c.h"
 #include "SDL_gesture_c.h"
 
-#if !defined(__PSP__)
-#include <memory.h>
-#endif
-
-#include <string.h>
+/*
 #include <stdio.h>
-#include <math.h>
+*/
 
 /* TODO: Replace with malloc */
 
