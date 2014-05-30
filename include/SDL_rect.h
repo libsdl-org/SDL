7153c2dad1247bda5a77e7f4d1bf4c17724e7917
Ryan C. Gordon <icculus@icculus.org>
2014-05-30 09:50:47 -0400

    Fixed using SDL_PointInRect() from C++.

--- include/SDL_rect.h
+++ include/SDL_rect.h
@@ -73,7 +73,7 @@ typedef struct SDL_Rect
 SDL_FORCE_INLINE SDL_bool SDL_PointInRect(const SDL_Point *p, const SDL_Rect *r)
 {
     return ( (p->x >= r->x) && (p->x < (r->x + r->w)) &&
-             (p->y >= r->y) && (p->y < (r->y + r->h)) );
+             (p->y >= r->y) && (p->y < (r->y + r->h)) ) ? SDL_TRUE : SDL_FALSE;
 }
 
 /**
