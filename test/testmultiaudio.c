9c6f9927bc9ccdd11d25036d97a27bc08491fc60
Gabriel Jacobo <gabomdq@gmail.com>
2014-06-06 18:33:17 -0300

    Adds missing Makefile for NACL test and a couple of smaller fixes

--- test/testmultiaudio.c
+++ test/testmultiaudio.c
@@ -133,6 +133,16 @@ main(int argc, char **argv)
     }
 
     SDL_Log("Using audio driver: %s\n", SDL_GetCurrentAudioDriver());
+    
+    #if __NACL__
+    SDL_RWUmount("/");
+    SDL_RWMount(
+        "",  /* source */
+        "/",  /* target */
+        "httpfs",  /* filesystemtype */
+        0,  /* mountflags */
+        "");  /* data specific to the html5fs type */
+#endif
 
     devcount = SDL_GetNumAudioDevices(0);
     if (devcount < 1) {
