fd84a457e3c91d7fc4523d21faaa0e45ff7ead19
David Ludwig <dludwig@pobox.com>
2015-02-08 15:44:15 -0500

    WinRT: made note that VSync is always enabled on WinPhone, due to OS
    Windows Phone does not appear to allow VSync to be turned off.  Doing so appears
    to either result in content not getting drawn (when the D3D debug runtime is
    turned off), or forcing VSync back on and logging an error (when the D3D debug
    runtime is turned on).
    
    VSync had been getting turned on anyways, this change just notes such:
    - via the WinRT README
    - by always setting the SDL_RENDERER_PRESENTVSYNC flag when creating an
      SDL_Renderer on Windows Phone

--- src/render/direct3d11/SDL_render_d3d11.c
+++ src/render/direct3d11/SDL_render_d3d11.c
@@ -829,9 +829,24 @@ D3D11_CreateRenderer(SDL_Window * window, Uint32 flags)
     renderer->info.flags = (SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
     renderer->driverdata = data;
 
+#if WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP
+    /* VSync is required in Windows Phone, at least for Win Phone 8.0 and 8.1.
+     * Failure to use it seems to either result in:
+     *
+     *  - with the D3D11 debug runtime turned OFF, vsync seemingly gets turned
+     *    off (framerate doesn't get capped), but nothing appears on-screen
+     *
+     *  - with the D3D11 debug runtime turned ON, vsync gets automatically
+     *    turned back on, and the following gets output to the debug console:
+     *    
+     *    DXGI ERROR: IDXGISwapChain::Present: Interval 0 is not supported, changed to Interval 1. [ UNKNOWN ERROR #1024: ] 
+     */
+    renderer->info.flags |= SDL_RENDERER_PRESENTVSYNC;
+#else
     if ((flags & SDL_RENDERER_PRESENTVSYNC)) {
         renderer->info.flags |= SDL_RENDERER_PRESENTVSYNC;
     }
+#endif
 
     /* HACK: make sure the SDL_Renderer references the SDL_Window data now, in
      * order to give init functions access to the underlying window handle:
