6a126a48df8e8260e0a16036a36f431371280ecb
Ryan C. Gordon <icculus@icculus.org>
2015-04-07 23:40:01 -0400

    Do the "fix" for asserts with MSVC's /W4 warnings only on MSVC.
    Naturally, this way generates a warning on GCC and Clang instead.  :)

--- include/SDL_assert.h
+++ include/SDL_assert.h
@@ -88,7 +88,11 @@ disable assertions.
 
 /* "while (0,0)" fools Microsoft's compiler's /W4 warning level into thinking
     this condition isn't constant. And looks like an owl's face! */
+#ifdef _MSC_VER  /* stupid /W4 warnings. */
 #define SDL_NULL_WHILE_LOOP_CONDITION (0,0)
+#else
+#define SDL_NULL_WHILE_LOOP_CONDITION (0)
+#endif
 
 #define SDL_disabled_assert(condition) \
     do { (void) sizeof ((condition)); } while (SDL_NULL_WHILE_LOOP_CONDITION)
