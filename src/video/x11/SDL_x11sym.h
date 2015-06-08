ea43889bc41a2c2f3ce86d358ab53a4b47662f16
Ryan C. Gordon <icculus@icculus.org>
2015-06-08 01:13:51 -0400

    configure/cmake/x11: Removed SDL_VIDEO_DRIVER_X11_CONST_PARAM_XDATA32 test.
    This was the only thing that made SDL_config.h generate differently between
    32 and 64-bit versions of Linux, so instead we force a function cast in our
    X11 code to match our dynamic loader version, which removes the compile error
    on some machines that prompted this test in the first place.
    
    Xlib never wrote to this data, so if you're on an older Xlib where this param
    wasn't const, your data should still be intact when we force the caller to
    think it was actually const after all.
    
    Fixes Bugzilla #1893.

--- src/video/x11/SDL_x11sym.h
+++ src/video/x11/SDL_x11sym.h
@@ -203,11 +203,7 @@ SDL_X11_SYM(Bool,XShmQueryExtension,(Display* a),(a),return)
  */
 #ifdef LONG64
 SDL_X11_MODULE(IO_32BIT)
-#if SDL_VIDEO_DRIVER_X11_CONST_PARAM_XDATA32
 SDL_X11_SYM(int,_XData32,(Display *dpy,register _Xconst long *data,unsigned len),(dpy,data,len),return)
-#else
-SDL_X11_SYM(int,_XData32,(Display *dpy,register long *data,unsigned len),(dpy,data,len),return)
-#endif
 SDL_X11_SYM(void,_XRead32,(Display *dpy,register long *data,long len),(dpy,data,len),)
 #endif
 
