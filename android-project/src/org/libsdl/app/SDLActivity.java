2a4855f9ee0d8f2acbcdca8f1f9474f6dc06bcb4
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2015-10-15 22:26:00 +0200

    Android: Added missing import statement.
    It did not compile without.

--- android-project/src/org/libsdl/app/SDLActivity.java
+++ android-project/src/org/libsdl/app/SDLActivity.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Method;
 
 import android.app.*;
 import android.content.*;
+import android.text.InputType;
 import android.view.*;
 import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.EditorInfo;
