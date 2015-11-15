a5a80cd033073d4e4b9e5b1e4fb61f6324fafcf2
David Ludwig <dludwig@pobox.com>
2015-11-15 13:04:42 -0500

    WinRT: fixed crash in SDL_CondWaitTimeout, when using Win10's MSVC runtime

--- src/thread/stdcpp/SDL_syscond.cpp
+++ src/thread/stdcpp/SDL_syscond.cpp
@@ -128,7 +128,7 @@ SDL_CondWaitTimeout(SDL_cond * cond, SDL_mutex * mutex, Uint32 ms)
     }
 
     try {
-        std::unique_lock<std::recursive_mutex> cpp_lock(mutex->cpp_mutex, std::defer_lock_t());
+        std::unique_lock<std::recursive_mutex> cpp_lock(mutex->cpp_mutex, std::adopt_lock_t());
         if (ms == SDL_MUTEX_MAXWAIT) {
             cond->cpp_cond.wait(
                 cpp_lock
