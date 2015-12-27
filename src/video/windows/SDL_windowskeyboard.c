2befe01d39fcfda70d49f5e6555792bcfdb2b036
Ryan C. Gordon <icculus@icculus.org>
2015-12-27 18:56:46 -0500

    Removed a tabstop.

--- src/video/windows/SDL_windowskeyboard.c
+++ src/video/windows/SDL_windowskeyboard.c
@@ -102,7 +102,7 @@ WIN_InitKeyboard(_THIS)
     SDL_SetScancodeName(SDL_SCANCODE_APPLICATION, "Menu");
     SDL_SetScancodeName(SDL_SCANCODE_LGUI, "Left Windows");
     SDL_SetScancodeName(SDL_SCANCODE_RGUI, "Right Windows");
-	
+
     /* Are system caps/num/scroll lock active? Set our state to match. */
     if (GetKeyState(VK_CAPITAL) & 0x0001) {
         SDL_SendKeyboardKey(SDL_PRESSED, SDL_SCANCODE_CAPSLOCK);
