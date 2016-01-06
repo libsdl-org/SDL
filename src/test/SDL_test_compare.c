ae7b142cbd9270a7a813a6955770406d07b15a57
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2016-01-06 22:39:29 +0100

    Fixed compile warnings about uninitialized variables in test library.
    Found by buildbot.

--- src/test/SDL_test_compare.c
+++ src/test/SDL_test_compare.c
@@ -43,7 +43,7 @@ int SDLTest_CompareSurfaces(SDL_Surface *surface, SDL_Surface *referenceSurface,
    int bpp, bpp_reference;
    Uint8 *p, *p_reference;
    int dist;
-   int sampleErrorX, sampleErrorY, sampleDist;
+   int sampleErrorX = 0, sampleErrorY = 0, sampleDist = 0;
    Uint8 R, G, B, A;
    Uint8 Rd, Gd, Bd, Ad;
    char imageFilename[128];
