416d046663b6674ba9f0abc076988840dc0c3407
Ryan C. Gordon <icculus@icculus.org>
2016-01-07 14:02:37 -0500

    Mac: Implemented SDL_GetDisplayDPI (thanks, Kirill!).
    Fixes Bugzilla #3223.

--- src/video/cocoa/SDL_cocoamodes.h
+++ src/video/cocoa/SDL_cocoamodes.h
@@ -37,6 +37,7 @@ extern void Cocoa_InitModes(_THIS);
 extern int Cocoa_GetDisplayBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect);
 extern int Cocoa_GetDisplayUsableBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect);
 extern void Cocoa_GetDisplayModes(_THIS, SDL_VideoDisplay * display);
+extern int Cocoa_GetDisplayDPI(_THIS, SDL_VideoDisplay * display, float * ddpi, float * hpdi, float * vdpi);
 extern int Cocoa_SetDisplayMode(_THIS, SDL_VideoDisplay * display, SDL_DisplayMode * mode);
 extern void Cocoa_QuitModes(_THIS);
 
