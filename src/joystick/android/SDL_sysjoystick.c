ed1149cbf71b12780c6c6df25c32f0a3acb35c55
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2016-02-16 20:32:22 +0100

    Android: Changed an internal joystick function to return count instead of id.
    The returned value is currently not used by the caller. The instance id would
    also not be needed on Java side and providing it just complicated the function.
    
    Partially fixes Bugzilla #3234.

--- src/joystick/android/SDL_sysjoystick.c
+++ src/joystick/android/SDL_sysjoystick.c
@@ -340,7 +340,6 @@ Android_RemoveJoystick(int device_id)
         return -1;
     }
 
-    const int retval = item->device_instance;
     if (item->joystick) {
         item->joystick->hwdata = NULL;
     }
@@ -376,7 +375,7 @@ Android_RemoveJoystick(int device_id)
     
     SDL_free(item->name);
     SDL_free(item);
-    return retval;
+    return numjoysticks;
 }
 
 
