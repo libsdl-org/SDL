4e86bba76c2f71d84ab975c15f9290438ff506f6
Alex Szpakowski <slime73@gmail.com>
2016-10-01 19:12:58 -0300

    Fix bug 3436 - SDL_RaiseWindow not working on windows

--- src/video/windows/SDL_windowswindow.c
+++ src/video/windows/SDL_windowswindow.c
@@ -478,7 +478,8 @@ WIN_HideWindow(_THIS, SDL_Window * window)
 void
 WIN_RaiseWindow(_THIS, SDL_Window * window)
 {
-    WIN_SetWindowPositionInternal(_this, window, SWP_NOCOPYBITS | SWP_NOMOVE | SWP_NOSIZE);
+	HWND hwnd = ((SDL_WindowData *) window->driverdata)->hwnd;
+	SetForegroundWindow(hwnd);
 }
 
 void
