6a9ee8fa4be9b35d0c1e1d8b77f26a68fb10bf58
Sam Lantinga <slouken@libsdl.org>
2016-10-01 12:52:55 -0700

    Fixed bug 3276 - build-scripts/showrev.sh prints the tip, which isn't useful if you're on a different -# branch, or just not sync'd to the tip.
    winterknight
    
    The showrev.sh script shows the tip, instead of the current hg revision. This can be mismatched if one were to use hg update -r ??? to revert to a previous revision.
    
    Patch uses parents instead of tip, which will show the revision that the source is compiled with, instead of the latest revision that the user who is compiling has downloaded.

--- build-scripts/showrev.sh
+++ build-scripts/showrev.sh
@@ -2,6 +2,4 @@
 #
 # Print the current source revision, if available
 
-# FIXME: this prints the tip, which isn't useful if you're on a different
-#  branch, or just not sync'd to the tip.
-hg tip --template 'hg-{rev}:{node|short}' || (echo "hg-0:baadf00d"; exit 1)
+hg parents --template 'hg-{rev}:{node|short}' || (echo "hg-0:baadf00d"; exit 1)
