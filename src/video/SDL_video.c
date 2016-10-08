62b9e1c79784084355335cd71a9b95d7d08316bf
Sam Lantinga <slouken@libsdl.org>
2016-10-07 18:09:09 -0700

    Fixed bug 3061 - Selecting the dummy video driver on Mac OS X results in an error
    Darren Kulp
    
    The dummy video driver is not available on Mac OS X if SDL_VIDEO_OPENGL is set at library compilation time.
    
    In src/video/SDL_video.c, there is a compile-time check in SDL_CreateWindow() for (SDL_VIDEO_OPENGL && __MACOSX__). When it succeeds, SDL_WINDOW_OPENGL is always requested. Since the dummy video driver does not supply an OpenGL implementation, the error "No OpenGL support in video driver" is supplied to the user, and SDL_CreateWindow() is exited early.

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -1366,7 +1366,9 @@ SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags)
 
     /* Some platforms have OpenGL enabled by default */
 #if (SDL_VIDEO_OPENGL && __MACOSX__) || __IPHONEOS__ || __ANDROID__ || __NACL__
-    flags |= SDL_WINDOW_OPENGL;
+    if (SDL_strcmp(_this->name, "dummy") != 0) {
+        flags |= SDL_WINDOW_OPENGL;
+    }
 #endif
     if (flags & SDL_WINDOW_OPENGL) {
         if (!_this->GL_CreateContext) {
