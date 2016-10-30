98d188f5de967eb62d249afed212692a56465faf
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2016-10-30 21:01:46 +0100

    Added call to SDL_HasAVX2() in platform test program.

--- test/testplatform.c
+++ test/testplatform.c
@@ -145,6 +145,7 @@ TestCPUInfo(SDL_bool verbose)
         SDL_Log("SSE4.1 %s\n", SDL_HasSSE41()? "detected" : "not detected");
         SDL_Log("SSE4.2 %s\n", SDL_HasSSE42()? "detected" : "not detected");
         SDL_Log("AVX %s\n", SDL_HasAVX()? "detected" : "not detected");
+        SDL_Log("AVX2 %s\n", SDL_HasAVX2()? "detected" : "not detected");
         SDL_Log("System RAM %d MB\n", SDL_GetSystemRAM());
     }
     return (0);
