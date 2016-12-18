787a54c84b3181785a51d29ebecb9dfb966b4465
Alex Szpakowski <slime73@gmail.com>
2016-12-18 12:28:28 -0400

    iOS bug #3377: work around bugs in some third party iOS libraries (e.g. Google admob) where they assume the optional UIApplicationDelegate ?window? property always exists and will crash if it doesn?t.

--- src/video/uikit/SDL_uikitappdelegate.h
+++ src/video/uikit/SDL_uikitappdelegate.h
@@ -36,6 +36,12 @@
 
 - (void)hideLaunchScreen;
 
+/* This property is marked as optional, and is only intended to be used when
+ * the app's UI is storyboard-based. SDL is not storyboard-based, however
+ * several major third-party ad APIs (e.g. Google admob) incorrectly assume this
+ * property always exists, and will crash if it doesn't. */
+@property (nonatomic) UIWindow *window;
+
 @end
 
 /* vi: set ts=4 sw=4 expandtab: */
