d98aa949053ea617607c1f4ba874a37ef7b83a4f
Sam Lantinga <slouken@libsdl.org>
2016-12-27 09:59:36 -0800

    Added documentation for the game controller axis values

--- include/SDL_gamecontroller.h
+++ include/SDL_gamecontroller.h
@@ -247,6 +247,12 @@ extern DECLSPEC void SDLCALL SDL_GameControllerUpdate(void);
 
 /**
  *  The list of axes available from a controller
+ *
+ *  Thumbstick axis values range from SDL_JOYSTICK_AXIS_MIN to SDL_JOYSTICK_AXIS_MAX,
+ *  and are centered within ~8000 of zero, though advanced UI will allow users to set
+ *  or autodetect the dead zone, which varies between controllers.
+ *
+ *  Trigger axis values range from 0 to SDL_JOYSTICK_AXIS_MAX.
  */
 typedef enum
 {
