ae07ea9289250fc04684e137eea3584e270c483d
Sam Lantinga <slouken@libsdl.org>
2017-01-01 18:32:57 -0800

    Convert some files back into DOS line endings after updating copyright

--- build-scripts/update-copyright.sh
+++ build-scripts/update-copyright.sh
@@ -2,8 +2,15 @@
 
 find . -type f -exec grep -Il "Copyright" {} \;     \
 | grep -v \.hg                             \
-| while read i;                            \
+| while read $file;                            \
 do \
-  LC_ALL=C sed -ie "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$i"; \
-  rm "${i}e"; \
+  LC_ALL=C sed -ie "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$file"; \
+  rm "${file}e"; \
+done
+
+for file in COPYING.txt VisualC-WinRT/SDL2-WinRT.nuspec VisualC-WinRT/SDL2main-WinRT-NonXAML.nuspec src/main/windows/version.rc visualtest/COPYING.txt
+do if [ -f "$file" ]; then
+     sed 's/$'"/`echo \\\r`/" $file > $file.new
+     mv $file.new $file
+   fi
 done
