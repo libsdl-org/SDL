0badbddef92b21d3b924fe18a31bb1639d167f45
Sam Lantinga <slouken@libsdl.org>
2017-01-01 18:47:29 -0800

    Run sed in binary mode so we don't have to worry about fixing line endings

--- build-scripts/update-copyright.sh
+++ build-scripts/update-copyright.sh
@@ -4,13 +4,5 @@ find . -type f -exec grep -Il "Copyright" {} \;     \
 | grep -v \.hg                             \
 | while read file;                            \
 do \
-  LC_ALL=C sed -ie "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$file"; \
-  rm "${file}e"; \
-done
-
-for file in COPYING.txt VisualC-WinRT/SDL2-WinRT.nuspec VisualC-WinRT/SDL2main-WinRT-NonXAML.nuspec src/main/windows/version.rc visualtest/COPYING.txt
-do if [ -f "$file" ]; then
-     sed 's/$'"/`echo \\\r`/" $file > $file.new
-     mv $file.new $file
-   fi
+  LC_ALL=C sed -b -i "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$file"; \
 done
