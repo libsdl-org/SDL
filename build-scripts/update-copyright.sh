6182b7f527663e949e1679b55b654f3cdb928772
Sam Lantinga <slouken@libsdl.org>
2017-01-01 18:36:35 -0800

    Fixed typo in copyright update script

--- build-scripts/update-copyright.sh
+++ build-scripts/update-copyright.sh
@@ -2,7 +2,7 @@
 
 find . -type f -exec grep -Il "Copyright" {} \;     \
 | grep -v \.hg                             \
-| while read $file;                            \
+| while read file;                            \
 do \
   LC_ALL=C sed -ie "s/\(.*Copyright.*\)[0-9]\{4\}\( *Sam Lantinga\)/\1`date +%Y`\2/" "$file"; \
   rm "${file}e"; \
