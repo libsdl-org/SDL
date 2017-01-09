836b23e3c5e4ec2839040d837fbff2c7c119c830
Sam Lantinga <slouken@libsdl.org>
2017-01-09 11:30:29 -0800

    Fixed spacing

--- src/events/SDL_keyboard.c
+++ src/events/SDL_keyboard.c
@@ -968,8 +968,10 @@ SDL_GetKeyFromName(const char *name)
 {
     SDL_Keycode key;
 
-        /* Check input */
-        if (name == NULL) return SDLK_UNKNOWN;
+    /* Check input */
+    if (name == NULL) {
+        return SDLK_UNKNOWN;
+    }
 
     /* If it's a single UTF-8 character, then that's the keycode itself */
     key = *(const unsigned char *)name;
