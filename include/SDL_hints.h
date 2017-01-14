06d1d945cc82507ebf8eb6d32477f7c629321cf2
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2017-01-14 21:35:49 +0100

    Fixed doxygen warning and linking of function names.

--- include/SDL_hints.h
+++ include/SDL_hints.h
@@ -759,11 +759,11 @@ extern "C" {
  *    or emulator, e.g. those from ARM, Imagination or Qualcomm.
  *  - Resolving OpenGL ES function addresses at link time by linking with
  *    the OpenGL ES library instead of querying them at run time with
- *    SDL_GL_GetProcAddress.
+ *    SDL_GL_GetProcAddress().
  *
  *  Caution: for an application to work with the default behaviour across
- *  different OpenGL drivers it \i must query the OpenGL ES function
- *  addresses at run time using SDL_GL_GetProcAddress.
+ *  different OpenGL drivers it must query the OpenGL ES function
+ *  addresses at run time using SDL_GL_GetProcAddress().
  *
  *  This variable is ignored on most platforms because OpenGL ES is native
  *  or not supported.
