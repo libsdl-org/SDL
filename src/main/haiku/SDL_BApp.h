fa46d692f210ab57a243200265b8b2599b0b9887
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2017-01-21 22:01:17 +0100

    Haiku: Removed unused variable.
    Found by Cppcheck.

--- src/main/haiku/SDL_BApp.h
+++ src/main/haiku/SDL_BApp.h
@@ -385,7 +385,6 @@ private:
     /* Members */
     std::vector<SDL_Window*> _window_map; /* Keeps track of SDL_Windows by index-id */
 
-    display_mode *_saved_mode;
     BGLView      *_current_context;
 };
 
