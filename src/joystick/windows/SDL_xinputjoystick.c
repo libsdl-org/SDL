00c6fa91808d68d2ea344c24f7d470b604c6c4aa
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2017-02-03 23:30:43 +0100

    Windows: Fixed warning about unused variable.
    Found by buildbot.

--- src/joystick/windows/SDL_xinputjoystick.c
+++ src/joystick/windows/SDL_xinputjoystick.c
@@ -119,7 +119,7 @@ GuessXInputDevice(Uint8 userid, Uint16 *pVID, Uint16 *pPID, Uint16 *pVersion)
 #ifndef __WINRT__   /* TODO: remove this ifndef __WINRT__ block, but only after integrating with UWP/WinRT's HID API */
 
     PRAWINPUTDEVICELIST devices = NULL;
-    UINT i, j, found_count = 0, device_count = 0;
+    UINT i, j, device_count = 0;
 
     if ((GetRawInputDeviceList(NULL, &device_count, sizeof(RAWINPUTDEVICELIST)) == -1) || (!device_count)) {
         return;  /* oh well. */
