2ac0ce25f5d234b842a5888c58daef57124e98aa
Ryan C. Gordon <icculus@icculus.org>
2017-05-19 14:49:16 -0400

    assert: Check for Clang _and_ GCC, in case they ever drop compatibility.

--- include/SDL_assert.h
+++ include/SDL_assert.h
@@ -51,7 +51,7 @@ assert can have unique static variables associated with it.
 /* Don't include intrin.h here because it contains C++ code */
     extern void __cdecl __debugbreak(void);
     #define SDL_TriggerBreakpoint() __debugbreak()
-#elif (!defined(__NACL__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)))
+#elif ( (!defined(__NACL__)) && ((defined(__GNUC__) || defined(__clang__)) && (defined(__i386__) || defined(__x86_64__))) )
     #define SDL_TriggerBreakpoint() __asm__ __volatile__ ( "int $3\n\t" )
 #elif defined(HAVE_SIGNAL_H)
     #include <signal.h>
