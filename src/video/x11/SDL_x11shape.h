c66f0471e1633376484e90767c0fb8415d17654e
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2017-05-25 23:00:43 +0200

    Removed unused internal window shape functions.

--- src/video/x11/SDL_x11shape.h
+++ src/video/x11/SDL_x11shape.h
@@ -32,7 +32,6 @@ typedef struct {
     Uint32 bitmapsize;
 } SDL_ShapeData;
 
-extern SDL_Window* X11_CreateShapedWindow(const char *title,unsigned int x,unsigned int y,unsigned int w,unsigned int h,Uint32 flags);
 extern SDL_WindowShaper* X11_CreateShaper(SDL_Window* window);
 extern int X11_ResizeWindowShape(SDL_Window* window);
 extern int X11_SetWindowShape(SDL_WindowShaper *shaper,SDL_Surface *shape,SDL_WindowShapeMode *shapeMode);
