22241ed0b010d844cc6e0f313969645856e0941a
Ryan C. Gordon <icculus@icculus.org>
2017-07-01 17:50:47 -0400

    Support for QNX 7.0 (thanks, Elad!).
    Fixes Bugzilla #3686.

--- src/dynapi/SDL_dynapi.c
+++ src/dynapi/SDL_dynapi.c
@@ -216,7 +216,7 @@ static SDL_INLINE void *get_sdlapi_entry(const char *fname, const char *sym)
     return retval;
 }
 
-#elif defined(unix) || defined(__unix__) || defined(__APPLE__) || defined(__HAIKU__)
+#elif defined(unix) || defined(__unix__) || defined(__APPLE__) || defined(__HAIKU__) || defined(__QNX__)
 #include <dlfcn.h>
 static SDL_INLINE void *get_sdlapi_entry(const char *fname, const char *sym)
 {
@@ -302,4 +302,3 @@ SDL_InitDynamicAPI(void)
 #endif  /* SDL_DYNAMIC_API */
 
 /* vi: set ts=4 sw=4 expandtab: */
-
