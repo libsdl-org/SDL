c3708ae8cd5add4085b7a40da3f99cff12ab6d67
Ryan C. Gordon <icculus@icculus.org>
2017-08-09 01:01:41 -0400

    haiku: Patched SDL_bopengl.cc to compile on x86-64 Haiku.
    Fixes Bugzilla #3729.

--- src/video/haiku/SDL_bopengl.cc
+++ src/video/haiku/SDL_bopengl.cc
@@ -54,7 +54,7 @@ int BE_GL_LoadLibrary(_THIS, const char *path)
         if( get_image_symbol(info.id, "glBegin", B_SYMBOL_TYPE_ANY,
                 &location) == B_OK) {
 
-            _this->gl_config.dll_handle = (void *) info.id;
+            _this->gl_config.dll_handle = (void *) (size_t) info.id;
             _this->gl_config.driver_loaded = 1;
             SDL_strlcpy(_this->gl_config.driver_path, "libGL.so",
                     SDL_arraysize(_this->gl_config.driver_path));
@@ -69,7 +69,7 @@ void *BE_GL_GetProcAddress(_THIS, const char *proc)
         void *location = NULL;
         status_t err;
         if ((err =
-            get_image_symbol((image_id) _this->gl_config.dll_handle,
+            get_image_symbol((image_id) (size_t) _this->gl_config.dll_handle,
                               proc, B_SYMBOL_TYPE_ANY,
                               &location)) == B_OK) {
             return location;
