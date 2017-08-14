ea9bc659f59efc38654cc3a4d3e42ccb90b3e84b
Sam Lantinga <slouken@libsdl.org>
2017-08-13 20:39:00 -0700

    Added check for XBOX in addition to Xbox and X-Box

--- src/joystick/SDL_gamecontroller.c
+++ src/joystick/SDL_gamecontroller.c
@@ -892,7 +892,7 @@ static ControllerMapping_t *SDL_PrivateGetControllerMappingForNameAndGUID(const 
 #endif /* __LINUX__ */
 
     if (!mapping && name) {
-        if (SDL_strstr(name, "Xbox") || SDL_strstr(name, "X-Box")) {
+        if (SDL_strstr(name, "Xbox") || SDL_strstr(name, "X-Box") || SDL_strstr(name, "XBOX")) {
             mapping = s_pXInputMapping;
         }
     }
