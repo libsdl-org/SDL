c68d3ab78527fb9fd6dc845d7e5bc63d60e4ef1e
Ozkan Sezer <sezeroz@gmail.com>
2017-08-17 21:35:46 -0400

    Watcom supports __FUNCTION__ identifier (and surely not __PRETTY_FUNCTION__)
    Partially fixes Bugzilla #3758.

--- include/SDL_assert.h
+++ include/SDL_assert.h
@@ -65,7 +65,7 @@ assert can have unique static variables associated with it.
 
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) /* C99 supports __func__ as a standard. */
 #   define SDL_FUNCTION __func__
-#elif ((__GNUC__ >= 2) || defined(_MSC_VER))
+#elif ((__GNUC__ >= 2) || defined(_MSC_VER) || defined (__WATCOMC__))
 #   define SDL_FUNCTION __FUNCTION__
 #else
 #   define SDL_FUNCTION "???"
--- src/render/opengles2/SDL_render_gles2.c
+++ src/render/opengles2/SDL_render_gles2.c
@@ -258,7 +258,7 @@ GL_CheckAllErrors (const char *prefix, SDL_Renderer *renderer, const char *file,
 
 #if 0
 #define GL_CheckError(prefix, renderer)
-#elif defined(_MSC_VER)
+#elif defined(_MSC_VER) || defined(__WATCOMC__)
 #define GL_CheckError(prefix, renderer) GL_CheckAllErrors(prefix, renderer, __FILE__, __LINE__, __FUNCTION__)
 #else
 #define GL_CheckError(prefix, renderer) GL_CheckAllErrors(prefix, renderer, __FILE__, __LINE__, __PRETTY_FUNCTION__)
