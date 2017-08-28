003d491f86a2e9cd14185eebf645d573c7a198be
Sam Lantinga <slouken@libsdl.org>
2017-08-27 19:05:57 -0700

    Fixed bug 3724 - Allow Angle Static Link
    Carlos
    
    We would like to add a switch (define) that allows us to compile Angle statically with SDL. That is, getting rid of the OpenGL DLL. Usually you need OpenGL to be loaded dynamically as DLL because implementation is provided by the system but no need with Angle.
    
    Only 2 files need modification and it shouldn't affect current behaivor:
    include/SDL_egl.h and src/video/SDL_egl.c, as in here
    
    https://github.com/native-toolkit/sdl/pull/10/files
    
    The flag name could be SDL_VIDEO_STATIC_ANGLE (instead of NATIVE_TOOLKIT_STATIC_ANGLE) as discussed here https://github.com/native-toolkit/sdl/pull/10
    
    We have tested this with both Windows and UWP, using NME engine (https://github.com/haxenme/nme).
    
    Releated issue: https://bugzilla.libsdl.org/show_bug.cgi?id=1820

--- include/SDL_egl.h
+++ include/SDL_egl.h
@@ -132,7 +132,7 @@
 *-------------------------------------------------------------------------
 * This precedes the return type of the function in the function prototype.
 */
-#if defined(_WIN32) && !defined(__SCITECH_SNAP__)
+#if defined(_WIN32) && !defined(__SCITECH_SNAP__) && !defined(SDL_VIDEO_STATIC_ANGLE)
 #   define KHRONOS_APICALL __declspec(dllimport)
 #elif defined (__SYMBIAN32__)
 #   define KHRONOS_APICALL IMPORT_C
