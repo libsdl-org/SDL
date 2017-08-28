1475c11cdfef3a4f1583bf8d146cb75663155f80
Sam Lantinga <slouken@libsdl.org>
2017-08-27 18:48:51 -0700

    SDL_thread.h: fix os/2 defines (rev 11340:2688d85b817c was a missing patch)

--- include/SDL_thread.h
+++ include/SDL_thread.h
@@ -119,17 +119,24 @@ SDL_CreateThread(SDL_ThreadFunction fn, const char *name, void *data,
 #endif
 
 #elif defined(__OS2__)
-
 /*
  * just like the windows case above:  We compile SDL2
  * into a dll with Watcom's runtime statically linked.
  */
 #define SDL_PASSED_BEGINTHREAD_ENDTHREAD
 #include <process.h>
-typedef int (*pfnSDL_CurrentBeginThread)(void (*func)(void *), void *, unsigned, void *arg);
+typedef int (*pfnSDL_CurrentBeginThread)(void (*func)(void *), void *, unsigned, void * /*arg*/);
 typedef void (*pfnSDL_CurrentEndThread)(void);
+extern DECLSPEC SDL_Thread *SDLCALL
+SDL_CreateThread(SDL_ThreadFunction fn, const char *name, void *data,
+                 pfnSDL_CurrentBeginThread pfnBeginThread,
+                 pfnSDL_CurrentEndThread pfnEndThread);
+#if defined(SDL_CreateThread) && SDL_DYNAMIC_API
 #undef SDL_CreateThread
+#define SDL_CreateThread(fn, name, data) SDL_CreateThread_REAL(fn, name, data, (pfnSDL_CurrentBeginThread)_beginthread, (pfnSDL_CurrentEndThread)_endthread)
+#else
 #define SDL_CreateThread(fn, name, data) SDL_CreateThread(fn, name, data, (pfnSDL_CurrentBeginThread)_beginthread, (pfnSDL_CurrentEndThread)_endthread)
+#endif
 
 #else
 
