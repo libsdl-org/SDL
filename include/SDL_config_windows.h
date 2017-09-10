16b8c4ea439a3dc81b52d1c164df62ca484b5765
Sam Lantinga <slouken@libsdl.org>
2017-09-10 10:30:25 -0700

    Fixed bug 3811 - change HAVE_COPYSIGN to HAVE__COPYSIGN in SDL_config_windows.h
    Ozkan Sezer
    
    The patch below changes HAVE_COPYSIGN macro in SDL_config_windows.h to
    HAVE__COPYSIGN, so that _copysign() can be used in SDL_stdlib.h which
    is available in many more windows-targeting toolchains such as MinGW,
    MSVC >= 6, etc, and not just  MSVC >= 2013. SDL_stdlib.c already has a
    specific check for HAVE__COPYSIGN, so I believe this is reasonable.

--- include/SDL_config_windows.h
+++ include/SDL_config_windows.h
@@ -142,7 +142,7 @@ typedef unsigned int uintptr_t;
 #define HAVE_SQRTF 1
 #define HAVE_TAN 1
 #define HAVE_TANF 1
-#define HAVE_COPYSIGN 1
+#define HAVE__COPYSIGN 1
 #if defined(_MSC_VER)
 /* These functions were added with the VC++ 2013 C runtime library */
 #if _MSC_VER >= 1800
