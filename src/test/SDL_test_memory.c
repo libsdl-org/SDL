149c99ea40647db96425ecd40d991ee7a27b12a1
Sam Lantinga <slouken@libsdl.org>
2017-10-12 14:44:54 -0700

    Fixed compiler warning

--- src/test/SDL_test_memory.c
+++ src/test/SDL_test_memory.c
@@ -250,14 +250,14 @@ void SDLTest_LogAllocations()
     total_allocated = 0;
     for (index = 0; index < SDL_arraysize(s_tracked_allocations); ++index) {
         for (entry = s_tracked_allocations[index]; entry; entry = entry->next) {
-            SDL_snprintf(line, sizeof(line), "Allocation %d: %lu bytes\n", count, entry->size);
+            SDL_snprintf(line, sizeof(line), "Allocation %d: %d bytes\n", count, (int)entry->size);
             ADD_LINE();
             /* Start at stack index 1 to skip our tracking functions */
             for (stack_index = 1; stack_index < SDL_arraysize(entry->stack); ++stack_index) {
                 if (!entry->stack[stack_index]) {
                     break;
                 }
-                SDL_snprintf(line, sizeof(line), "\t0x%llx: %s\n", entry->stack[stack_index], entry->stack_names[stack_index]);
+                SDL_snprintf(line, sizeof(line), "\t0x%"SDL_PRIX64": %s\n", entry->stack[stack_index], entry->stack_names[stack_index]);
                 ADD_LINE();
             }
             total_allocated += entry->size;
