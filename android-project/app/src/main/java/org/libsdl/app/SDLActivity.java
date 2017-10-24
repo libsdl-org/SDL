c7885b3eee3bf29376756ab681f65ca95ab04c4b
Sam Lantinga <slouken@libsdl.org>
2017-10-23 23:23:34 -0700

    Fixed build with older Android SDK

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.lang.reflect.Method;
-import java.util.Objects;
 
 import android.app.*;
 import android.content.*;
