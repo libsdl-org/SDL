76cdce440cf9d471e7537d7d9011b3e8ca0de05f
Sam Lantinga <slouken@libsdl.org>
2017-10-23 23:23:34 -0700

    Fixed build with older Android SDK

--- android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 import android.content.Context;
 import android.os.*;
