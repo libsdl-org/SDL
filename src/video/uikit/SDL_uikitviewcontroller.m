4c27f9ea337c68b68ad42fc012a861399acd86f6
Sam Lantinga <slouken@libsdl.org>
2017-12-19 10:57:21 -0800

    Fixed bug 4004 - iOS: don't hide keyboard on RETURN
    Dominik Reichardt
    
    As discussed in 2012 the iOS onscreen keyboard hides when you hit RETURN (see https://discourse.libsdl.org/t/on-screen-keyboard-change/19216).
    IMO this is a bad idea to not be able to influence this behavior and just recently this was fixed for Android by adding the hint SDL_HINT_ANDROID_RETURN_HIDES_IME in changeset 11768	6ce3bb5e38a5.

--- src/video/uikit/SDL_uikitviewcontroller.m
+++ src/video/uikit/SDL_uikitviewcontroller.m
@@ -387,7 +387,9 @@ SDL_AppleTVControllerUIHintChanged(void *userdata, const char *name, const char 
 {
     SDL_SendKeyboardKey(SDL_PRESSED, SDL_SCANCODE_RETURN);
     SDL_SendKeyboardKey(SDL_RELEASED, SDL_SCANCODE_RETURN);
-    SDL_StopTextInput();
+    if (SDL_GetHintBoolean(SDL_HINT_RETURN_KEY_HIDES_IME, SDL_FALSE)) {
+         SDL_StopTextInput();
+    }
     return YES;
 }
 
