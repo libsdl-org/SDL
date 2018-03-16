2fbbac994661c4e7a61d1d1f9d428785249787de
Sam Lantinga <slouken@libsdl.org>
2018-03-16 11:08:53 -0700

    Reimplemented Android cursor API support using reflection so it builds with older SDKs

--- src/core/android/SDL_android.h
+++ src/core/android/SDL_android.h
@@ -104,8 +104,8 @@ int Android_JNI_ShowMessageBox(const SDL_MessageBoxData *messageboxdata, int *bu
 
 /* Cursor support */
 int Android_JNI_CreateCustomCursor(SDL_Surface *surface, int hot_x, int hot_y);
-void Android_JNI_SetCustomCursor(int cursorID);
-void Android_JNI_SetSystemCursor(int cursorID);
+SDL_bool Android_JNI_SetCustomCursor(int cursorID);
+SDL_bool Android_JNI_SetSystemCursor(int cursorID);
 
 /* Ends C function definitions when using C++ */
 #ifdef __cplusplus
