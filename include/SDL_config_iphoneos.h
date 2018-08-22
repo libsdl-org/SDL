74e99f741678d537d27f247e8caa21f32818dfd1
Sam Lantinga <slouken@libsdl.org>
2018-08-21 20:34:09 -0700

    Don't use CoreMotion on tvOS

--- include/SDL_config_iphoneos.h
+++ include/SDL_config_iphoneos.h
@@ -139,8 +139,12 @@
 #define SDL_JOYSTICK_MFI 1
 #define SDL_JOYSTICK_HIDAPI 1
 
+#ifdef __IPHONEOS__
 /* Enable the CoreMotion sensor driver */
 #define SDL_SENSOR_COREMOTION   1
+#else
+#define SDL_SENSOR_DUMMY    1
+#endif
 
 /* Enable Unix style SO loading */
 #define SDL_LOADSO_DLOPEN 1
