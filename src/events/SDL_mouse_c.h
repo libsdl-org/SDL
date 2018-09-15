6b3e893105e5775e3f0792d7d2d8921ee0144012
Sam Lantinga <slouken@libsdl.org>
2018-09-14 19:26:26 -0700

    Added hints  SDL_HINT_MOUSE_DOUBLE_CLICK_TIME and SDL_HINT_MOUSE_DOUBLE_CLICK_RADIUS to allow tuning double-click sensitivity. Also increased the default double-click radius to 32 pixels to be more forgiving for touch interfaces

--- src/events/SDL_mouse_c.h
+++ src/events/SDL_mouse_c.h
@@ -90,6 +90,8 @@ typedef struct
     float relative_speed_scale;
     float scale_accum_x;
     float scale_accum_y;
+    Uint32 double_click_time;
+    int double_click_radius;
     SDL_bool touch_mouse_events;
 
     /* Data for double-click tracking */
@@ -111,9 +113,6 @@ extern int SDL_MouseInit(void);
 
 /* Get the mouse state structure */
 SDL_Mouse *SDL_GetMouse(void);
-
-/* Set the default double-click interval */
-extern void SDL_SetDoubleClickTime(Uint32 interval);
 
 /* Set the default mouse cursor */
 extern void SDL_SetDefaultCursor(SDL_Cursor * cursor);
