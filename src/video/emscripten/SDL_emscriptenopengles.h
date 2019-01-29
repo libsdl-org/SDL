f66af75e7e79ed4774ee7ccb0094884d1ab13e7d
Charlie Birks <charlie@daftgames.net>
2019-01-29 12:19:32 +0000

    Emscripten: remove GLES_DeleteContext implementation It was calling glClear without a context. The issue it was trying to solve was actually that after destroying a window and creating a new one , the contents of the old window were preserved. This no longer happens since we resize the window to nothing on destroy.

--- src/video/emscripten/SDL_emscriptenopengles.h
+++ src/video/emscripten/SDL_emscriptenopengles.h
@@ -34,9 +34,9 @@
 #define Emscripten_GLES_UnloadLibrary SDL_EGL_UnloadLibrary
 #define Emscripten_GLES_SetSwapInterval SDL_EGL_SetSwapInterval
 #define Emscripten_GLES_GetSwapInterval SDL_EGL_GetSwapInterval
+#define Emscripten_GLES_DeleteContext SDL_EGL_DeleteContext
 
 extern int Emscripten_GLES_LoadLibrary(_THIS, const char *path);
-extern void Emscripten_GLES_DeleteContext(_THIS, SDL_GLContext context);
 extern SDL_GLContext Emscripten_GLES_CreateContext(_THIS, SDL_Window * window);
 extern int Emscripten_GLES_SwapWindow(_THIS, SDL_Window * window);
 extern int Emscripten_GLES_MakeCurrent(_THIS, SDL_Window * window, SDL_GLContext context);
