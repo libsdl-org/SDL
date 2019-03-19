a0ac5ffc9ffd3843aeaa732d3a9704278a99f507
Sam Lantinga <slouken@libsdl.org>
2019-03-19 10:59:41 -0700

    Fixed building with C++

--- include/SDL_main.h
+++ include/SDL_main.h
@@ -118,7 +118,7 @@ extern "C" {
  *  The prototype for the application's main() function
  */
 typedef int (*SDL_main_func)(int argc, char *argv[]);
-extern C_LINKAGE SDLMAIN_DECLSPEC int SDL_main(int argc, char *argv[]);
+extern SDLMAIN_DECLSPEC int SDL_main(int argc, char *argv[]);
 
 
 /**
