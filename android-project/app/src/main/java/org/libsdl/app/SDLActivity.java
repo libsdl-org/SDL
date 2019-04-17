1820f1d06bb651f68f1a604c1ff3b41ff084bbbe
Sam Lantinga <slouken@libsdl.org>
2019-04-16 20:00:14 -0700

    Explicitly load hidapi as a dependency of the SDL library This fixes loading on Android 4.2

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -136,6 +136,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
      */
     protected String[] getLibraries() {
         return new String[] {
+            "hidapi",
             "SDL2",
             // "SDL2_image",
             // "SDL2_mixer",
