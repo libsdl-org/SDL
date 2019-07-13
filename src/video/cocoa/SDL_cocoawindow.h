8fb8adfc90fa5ffb059d9b38cec061f7bbcbfc49
Alex Szpakowski <slime73@gmail.com>
2019-07-13 17:04:02 -0300

    macOS: Fix SDL_GL_CreateContext/MakeCurrent on non-main threads causing a Main Thread Checker warning when built with Xcode 11 / the macOS 10.15 SDK.
    Fixes bug #4714.

--- src/video/cocoa/SDL_cocoawindow.h
+++ src/video/cocoa/SDL_cocoawindow.h
@@ -113,6 +113,7 @@ struct SDL_WindowData
 {
     SDL_Window *window;
     NSWindow *nswindow;
+    NSView *sdlContentView; /* nil if window is created via CreateWindowFrom */
     NSMutableArray *nscontexts;
     SDL_bool created;
     SDL_bool inWindowMove;
