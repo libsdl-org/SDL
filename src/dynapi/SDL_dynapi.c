ac99dfac156082ef853d0f8f53bb828c97ab30fa
Ozkan Sezer <sezeroz@gmail.com>
2019-07-31 00:05:28 +0300

    minor build fixes.

--- src/dynapi/SDL_dynapi.c
+++ src/dynapi/SDL_dynapi.c
@@ -251,12 +251,12 @@ static SDL_INLINE void *get_sdlapi_entry(const char *fname, const char *sym)
     HMODULE hmodule;
     PFN retval = NULL;
     char error[256];
-    if (DosLoadModule(&error, sizeof(error), fname, &hmodule) == NO_ERROR) {
+    if (DosLoadModule(error, sizeof(error), fname, &hmodule) == NO_ERROR) {
         if (DosQueryProcAddr(hmodule, 0, sym, &retval) != NO_ERROR) {
             DosFreeModule(hmodule);
         }
     }
-    return (void *) retval;
+    return (void *)retval;
 }
 
 #else
