97fefd050976bbbfca9608499f6a7d9fb86e70db
Sam Lantinga <slouken@libsdl.org>
2019-07-30 11:00:00 -0700

    Fixed bug 4538 - validate image size when loading BMP files

--- src/video/SDL_bmp.c
+++ src/video/SDL_bmp.c
@@ -226,6 +226,11 @@ SDL_LoadBMP_RW(SDL_RWops * src, int freesrc)
             SDL_RWseek(src, (biSize - headerSize), RW_SEEK_CUR);
         }
     }
+    if (biWidth <= 0 || biHeight == 0) {
+        SDL_SetError("BMP file with bad dimensions (%dx%d)", biWidth, biHeight);
+        was_error = SDL_TRUE;
+        goto done;
+    }
     if (biHeight < 0) {
         topDown = SDL_TRUE;
         biHeight = -biHeight;
