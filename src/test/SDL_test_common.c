4af76119e61eb411a2c1185ffc82f5ebf92155b4
Ryan C. Gordon <icculus@icculus.org>
2020-01-30 13:47:19 -0500

    test: change path in a comment from '\\' to '/' char. Nothing serious here.

--- src/test/SDL_test_common.c
+++ src/test/SDL_test_common.c
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-/* Ported from original test\common.c file. */
+/* Ported from original test/common.c file. */
 
 #include "SDL_config.h"
 #include "SDL_test.h"
