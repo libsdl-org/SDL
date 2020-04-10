d292f6bd4f5e50ed4813cf7deac8aba667059a3c
Ryan C. Gordon <icculus@icculus.org>
2020-04-10 12:17:14 -0400

    stdlib: Add SDL_trunc and SDL_truncf

--- src/dynapi/SDL_dynapi_overrides.h
+++ src/dynapi/SDL_dynapi_overrides.h
@@ -761,3 +761,5 @@
 #define SDL_UnlockSensors SDL_UnlockSensors_REAL
 #define SDL_Metal_GetLayer SDL_Metal_GetLayer_REAL
 #define SDL_Metal_GetDrawableSize SDL_Metal_GetDrawableSize_REAL
+#define SDL_trunc SDL_trunc_REAL
+#define SDL_truncf SDL_truncf_REAL
