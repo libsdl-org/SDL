f69f2174076b852c7f813750a4f39cb9e38c3a74
Ryan C. Gordon <icculus@icculus.org>
2020-04-21 01:30:36 -0400

    SDL_error: simplified error string management.
    This patch removes deferred error string formatting: now we do it during
    SDL_SetError(), so there's no limit on printf-style arguments used.
    
    Also removes stub for managing error string translations; we don't have the
    facilities to maintain that and the way we set arbitrary error strings
    doesn't really make this practical anyhow.
    
    Since the final error string is set right away and unique to the thread,
    we no longer need a static buffer for legacy SDL_GetError(), and we don't
    have to allocate 5x 128-byte argument fields per-thread. Also, since we now
    use SDL_vsnprintf instead of parsing the format string ourselves, there's a
    lot of code deleted and we have access to more robust formatting powers now.
    
    This does mean the final error strings can't be more than 128 bytes, down
    from the theoretical maximum of around 768, but I think this is probably okay.
    They might truncate but they will always be null-terminated!
    
    Fixes Bugzilla #5092.

--- src/SDL_error_c.h
+++ src/SDL_error_c.h
@@ -28,33 +28,11 @@
 #define SDL_error_c_h_
 
 #define ERR_MAX_STRLEN  128
-#define ERR_MAX_ARGS    5
 
 typedef struct SDL_error
 {
-    /* This is a numeric value corresponding to the current error */
-    int error;
-
-    /* This is a key used to index into a language hashtable containing
-       internationalized versions of the SDL error messages.  If the key
-       is not in the hashtable, or no hashtable is available, the key is
-       used directly as an error message format string.
-     */
-    char key[ERR_MAX_STRLEN];
-
-    /* These are the arguments for the error functions */
-    int argc;
-    union
-    {
-        void *value_ptr;
-#if 0                           /* What is a character anyway?  (UNICODE issues) */
-        unsigned char value_c;
-#endif
-        int value_i;
-        long value_l;
-        double value_f;
-        char buf[ERR_MAX_STRLEN];
-    } args[ERR_MAX_ARGS];
+    int error; /* This is a numeric value corresponding to the current error */
+    char str[ERR_MAX_STRLEN];
 } SDL_error;
 
 /* Defined in SDL_thread.c */
