68e1731e02fab9f94b51bc739932547c3a651ee2
Sam Lantinga <slouken@libsdl.org>
2020-05-29 15:40:17 -0700

    Don't bother logging security exception getting the device serial number
    This can happen anytime we haven't opened the device yet

--- android-project/app/src/main/java/org/libsdl/app/HIDDeviceUSB.java
+++ android-project/app/src/main/java/org/libsdl/app/HIDDeviceUSB.java
@@ -57,8 +57,7 @@ class HIDDeviceUSB implements HIDDevice {
                 result = mDevice.getSerialNumber();
             }
             catch (SecurityException exception) {
-                Log.w(TAG, "App permissions mean we cannot get serial number for device " + getDeviceName() + " message: " + exception.getMessage());
-                result = "";
+                //Log.w(TAG, "App permissions mean we cannot get serial number for device " + getDeviceName() + " message: " + exception.getMessage());
             }
         }
         if (result == null) {
