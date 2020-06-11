85f575773bb795aed384341514606b0454537233
Ethan Lee <flibitijibibo@flibitijibibo.com>
2020-06-11 12:03:33 -0400

    Add SDL_SIMDRealloc

--- src/dynapi/SDL_dynapi_overrides.h
+++ src/dynapi/SDL_dynapi_overrides.h
@@ -716,6 +716,7 @@
 #define SDL_UIKitRunApp SDL_UIKitRunApp_REAL
 #define SDL_SIMDGetAlignment SDL_SIMDGetAlignment_REAL
 #define SDL_SIMDAlloc SDL_SIMDAlloc_REAL
+#define SDL_SIMDRealloc SDL_SIMDRealloc_REAL
 #define SDL_SIMDFree SDL_SIMDFree_REAL
 #define SDL_RWsize SDL_RWsize_REAL
 #define SDL_RWseek SDL_RWseek_REAL
