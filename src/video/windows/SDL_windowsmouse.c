3aec3ee32d4c50f945acd13112d3c750b448aa21
Cameron Gutman <aicommander@gmail.com>
2020-08-25 21:22:00 -0700

    windows: Fix handling of swapped mouse buttons
    GetAsyncKeyState() and GetRawInputData() report the state of the physical
    buttons without applying the user's primary/secondary mouse button swap
    preference. Swap the buttons returned from these functions, so we expose a
    consistent view of the buttons to SDL callers. This new behavior also matches
    the behavior of macOS and X11 backends.
    
    See the Remarks section of the GetAsyncKeyState() function on MSDN.

--- src/video/windows/SDL_windowsmouse.c
+++ src/video/windows/SDL_windowsmouse.c
@@ -276,12 +276,14 @@ WIN_GetGlobalMouseState(int *x, int *y)
 {
     Uint32 retval = 0;
     POINT pt = { 0, 0 };
+    SDL_bool swapButtons = GetSystemMetrics(SM_SWAPBUTTON) != 0;
+
     GetCursorPos(&pt);
     *x = (int) pt.x;
     *y = (int) pt.y;
 
-    retval |= GetAsyncKeyState(VK_LBUTTON) & 0x8000 ? SDL_BUTTON_LMASK : 0;
-    retval |= GetAsyncKeyState(VK_RBUTTON) & 0x8000 ? SDL_BUTTON_RMASK : 0;
+    retval |= GetAsyncKeyState(!swapButtons ? VK_LBUTTON : VK_RBUTTON) & 0x8000 ? SDL_BUTTON_LMASK : 0;
+    retval |= GetAsyncKeyState(!swapButtons ? VK_RBUTTON : VK_LBUTTON) & 0x8000 ? SDL_BUTTON_RMASK : 0;
     retval |= GetAsyncKeyState(VK_MBUTTON) & 0x8000 ? SDL_BUTTON_MMASK : 0;
     retval |= GetAsyncKeyState(VK_XBUTTON1) & 0x8000 ? SDL_BUTTON_X1MASK : 0;
     retval |= GetAsyncKeyState(VK_XBUTTON2) & 0x8000 ? SDL_BUTTON_X2MASK : 0;
