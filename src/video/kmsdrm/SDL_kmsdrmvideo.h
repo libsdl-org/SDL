f4e02a5c1bdd3e9fe212df31470f2a672af8bcda
Manuel Alfayate Corchete <redwindwanderer@gmail.com>
2020-08-31 19:17:17 +0200

    kmsdrm: Do NOT modify window size manually from the backend: doing so caused renderer scaling params miscalculation.

--- src/video/kmsdrm/SDL_kmsdrmvideo.h
+++ src/video/kmsdrm/SDL_kmsdrmvideo.h
@@ -127,6 +127,8 @@ typedef struct SDL_WindowData
 #endif
 
     /* For scaling and AR correction. */
+    int32_t src_w;
+    int32_t src_h;
     int32_t output_w;
     int32_t output_h;
     int32_t output_x;
