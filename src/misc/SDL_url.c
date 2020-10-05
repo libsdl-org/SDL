5f68812249973b4aa5247115716c2f4ea64a7546
Ryan C. Gordon <icculus@icculus.org>
2020-10-05 13:52:52 -0400

    url: patched to compile on Android.

--- src/misc/SDL_url.c
+++ src/misc/SDL_url.c
@@ -18,7 +18,6 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
-
 #include "../SDL_internal.h"
 #include "SDL_error.h"
 
