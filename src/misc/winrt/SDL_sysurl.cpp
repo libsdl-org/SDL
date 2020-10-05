57e3003d3f68d6cf0036711c8d7a3b8ad4929adb
Ryan C. Gordon <icculus@icculus.org>
2020-10-05 15:47:47 -0400

    url: Attempt at WinRT support.

---
+++ src/misc/winrt/SDL_sysurl.cpp
@@ -0,0 +1,43 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include <Windows.h>
+
+#include "../SDL_sysurl.h"
+
+int
+SDL_SYS_OpenURL(const char *url)
+{
+    auto uri = ref new Windows::Foundation::Uri(url);
+    concurrency::task<bool> launchUriOperation(Windows::System::Launcher::LaunchUriAsync(uri));
+    int retval = -1;
+    launchUriOperation.then([](bool success) {
+        if (success) {
+            retval = 0;
+        } else {
+            retval = SDL_SetError("URL failed to launch");
+        }
+    });
+    return retval;
+}
+
+/* vi: set ts=4 sw=4 expandtab: */
+
