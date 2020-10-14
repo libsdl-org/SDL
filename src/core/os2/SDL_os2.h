b6965a177456c4dfe57b1652106773963ea8f447
Ozkan Sezer <sezeroz@gmail.com>
2020-10-14 23:01:05 +0300

    os2: Andrey Vasilkin agreed that his code to be put under Zlib license:
    add SDL license headers to geniconv sources.

--- src/core/os2/SDL_os2.h
+++ src/core/os2/SDL_os2.h
@@ -1,3 +1,23 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
 #ifndef SDL_os2_h_
 #define SDL_os2_h_
 
@@ -20,11 +40,11 @@
 #endif /* OS2DEBUG */
 
 
-// StrUTF8New() - geniconv/sys2utf8.c.
+/* StrUTF8New() - geniconv/sys2utf8.c */
 #define OS2_SysToUTF8(S) StrUTF8New( 1, S, SDL_strlen( S ) + 1 )
 #define OS2_UTF8ToSys(S) StrUTF8New( 0, (char *)(S), SDL_strlen( S ) + 1 )
 
-// SDL_OS2Quit() will be called from SDL_QuitSubSystem().
+/* SDL_OS2Quit() will be called from SDL_QuitSubSystem() */
 void SDL_OS2Quit();
 
 #endif /* SDL_os2_h_ */
