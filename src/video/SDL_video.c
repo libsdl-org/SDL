2d64e37e41eb6ab4795bba90aee26269100ff770
Ivan Epifanov <isage.dna@gmail.com>
2020-11-02 18:09:43 +0300

    Initial rebase of xerpi's port

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -94,6 +94,9 @@ static VideoBootStrap *bootstrap[] = {
 #if SDL_VIDEO_DRIVER_PSP
     &PSP_bootstrap,
 #endif
+#if SDL_VIDEO_DRIVER_VITA
+    &VITA_bootstrap,
+#endif
 #if SDL_VIDEO_DRIVER_KMSDRM
     &KMSDRM_bootstrap,
 #endif
