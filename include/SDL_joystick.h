c63bbb06e9c0ee3f7f600c701ff70e5ac1cbe51d
Sam Lantinga <slouken@libsdl.org>
2020-11-24 06:55:33 -0800

    Including SDL_config_iphoneos.h enables MFI controller code

--- include/SDL_joystick.h
+++ include/SDL_joystick.h
@@ -105,6 +105,11 @@ typedef enum
     SDL_JOYSTICK_POWER_MAX
 } SDL_JoystickPowerLevel;
 
+/* Set max recognized G-force from accelerometer
+   See src/joystick/uikit/SDL_sysjoystick.m for notes on why this is needed
+ */
+#define SDL_IPHONE_MAX_GFORCE 5.0
+
 
 /* Function prototypes */
 
