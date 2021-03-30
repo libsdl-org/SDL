07a2d71e90d8c3dc67f2301cdc88756d3d846dba
Charlie Birks <charlie@daft.games>
2021-03-30 11:56:38 +0100

    emscripten: Feed silence to device when paused

--- src/audio/emscripten/SDL_emscriptenaudio.c
+++ src/audio/emscripten/SDL_emscriptenaudio.c
@@ -59,6 +59,9 @@ HandleAudioProcess(_THIS)
         if (this->stream) {
             SDL_AudioStreamClear(this->stream);
         }
+
+        SDL_memset(this->work_buffer, this->spec.silence, this->spec.size);
+        FeedAudioDevice(this, this->work_buffer, this->spec.size);
         return;
     }
 
