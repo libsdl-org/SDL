e13c2971b718c5a02cf173f28c3df57814d2bd6e
Vladislav Dmitrievich Turbanov <vladislav@turbanov.ru>
2021-04-02 12:05:45 +0300

    * Support for intrinsics in MSW + Clang scenario. Utility polyfill is provided, removed the no-longer-needed conditionals.

--- include/SDL_endian.h
+++ include/SDL_endian.h
@@ -30,7 +30,23 @@
 
 #include "SDL_stdinc.h"
 
-#if defined(_MSC_VER) && !defined(__clang__)
+#ifdef _MSC_VER
+/* As of Clang 11, '_m_prefetchw' is conflicting with the winnt.h's version,
+   so we define the needed '_m_prefetch' here as a pseudo-header, until the issue is fixed. */
+
+#ifdef __clang__
+#ifndef __PRFCHWINTRIN_H
+#define __PRFCHWINTRIN_H
+
+static __inline__ void __attribute__((__always_inline__, __nodebug__))
+_m_prefetch(void *__P)
+{
+  __builtin_prefetch (__P, 0, 3 /* _MM_HINT_T0 */);
+}
+
+#endif /* __PRFCHWINTRIN_H */
+#endif /* __clang__ */
+
 #include <intrin.h>
 #endif
 
