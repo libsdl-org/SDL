d448896234dfae8356b36096faae1f96f1d82b0a
Sam Clegg <sbc@chromium.org>
2021-05-19 08:24:33 -0700

    Add SDL_config_emscripten.h
    Currently we maintain a config file in the emscripten
    repo, but this seems like what other embedded/cross
    platforms do.

--- include/SDL_config.h
+++ include/SDL_config.h
@@ -43,6 +43,8 @@
 #include "SDL_config_psp.h"
 #elif defined(__OS2__)
 #include "SDL_config_os2.h"
+#elif defined(__EMSCRIPTEN__)
+#include "SDL_config_emscripten.h"
 #else
 /* This is a minimal configuration just to get SDL running on new platforms. */
 #include "SDL_config_minimal.h"
