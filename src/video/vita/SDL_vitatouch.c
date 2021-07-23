7b0092db6ac689d8e19459ee983d94744a94cdac
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-07-23 18:58:25 +0200

    vita: touch: Mark the back panel as an indirect absolute touch device
    It's not a touchscreen, but an additional touch panel on the back of
    the device.

--- src/video/vita/SDL_vitatouch.c
+++ src/video/vita/SDL_vitatouch.c
@@ -65,7 +65,7 @@ VITA_InitTouch(void)
 
     // Support passing both front and back touch devices in events
     SDL_AddTouch((SDL_TouchID)0, SDL_TOUCH_DEVICE_DIRECT, "Front");
-    SDL_AddTouch((SDL_TouchID)1, SDL_TOUCH_DEVICE_DIRECT,  "Back");
+    SDL_AddTouch((SDL_TouchID)1, SDL_TOUCH_DEVICE_INDIRECT_ABSOLUTE,  "Back");
 }
 
 void 
