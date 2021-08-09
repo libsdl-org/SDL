libsdl-org/SDL

commit b08b1bde66d1cfa0987093e0719388c5c3203130

linux: remove d-bus lazy init dead code
Lazy init in X11/Wayland is dead code since dbdbae4

src/video/wayland/SDL_waylandvideo.h
@@ -29,7 +29,6 @@
 #include "wayland-util.h"
 
 #include "../SDL_sysvideo.h"
-#include "../../core/linux/SDL_dbus.h"
 #include "../../core/linux/SDL_ime.h"
 
 struct xkb_context;

src/video/x11/SDL_x11video.h
@@ -56,7 +56,6 @@
 #include <X11/extensions/xf86vmode.h>
 #endif
 
-#include "../../core/linux/SDL_dbus.h"
 #include "../../core/linux/SDL_ime.h"
 
 #include "SDL_x11dyn.h"

src/video/wayland/SDL_waylandvideo.c
@@ -578,10 +578,6 @@ Wayland_VideoInit(_THIS)
 
     Wayland_InitKeyboard(_this);
 
-#if SDL_USE_LIBDBUS
-    SDL_DBus_Init();
-#endif
-
     return 0;
 }
 

src/video/x11/SDL_x11video.c
@@ -453,10 +453,6 @@ X11_VideoInit(_THIS)
 
     X11_InitTouch(_this);
 
-#if SDL_USE_LIBDBUS
-    SDL_DBus_Init();
-#endif
-
     return 0;
 }
 

commit a8a6abc08d74232646d05cd2f89e64743da19d33

x11/wayland: fix screensaver suspension via D-Bus
b08b1bde introduced a subtle bug. Despite not using D-Bus types directly,
the code used the SDL_USE_LIBDBUS definition set by SDL_dbus.h to conditionally
compile calls SDL_DBus_ScreensaverTickle() and SDL_DBus_ScreensaverInhibit().
As a result, it still compiled without SDL_dbus.h included, but screensaver
suspension silently failed to work.

The D-Bus stuff could probably use some tweaks to be harder to accidentally
break, but for now just restore the header includes.

src/video/wayland/SDL_waylandvideo.h
@@ -29,6 +29,7 @@
 #include "wayland-util.h"
 
 #include "../SDL_sysvideo.h"
+#include "../../core/linux/SDL_dbus.h"
 #include "../../core/linux/SDL_ime.h"
 
 struct xkb_context;

src/video/x11/SDL_x11video.h
@@ -56,6 +56,7 @@
 #include <X11/extensions/xf86vmode.h>
 #endif
 
+#include "../../core/linux/SDL_dbus.h"
 #include "../../core/linux/SDL_ime.h"
 
 #include "SDL_x11dyn.h"