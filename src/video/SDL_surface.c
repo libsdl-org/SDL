93bed9ccce3eedb197c0fee0e2686c8ab908f5da
dmitry.gapkalov <dmitry.gapkalov@zodiacsystems.com>
2021-08-23 15:19:24 -0400

    fix blend mode for converted surface with alpha

--- src/video/SDL_surface.c
+++ src/video/SDL_surface.c
@@ -1298,7 +1298,7 @@ SDL_ConvertSurface(SDL_Surface * surface, const SDL_PixelFormat * format,
 
     /* Enable alpha blending by default if the new surface has an
      * alpha channel or alpha modulation */
-    if ((surface->format->Amask && format->Amask) ||
+    if ((convert->format->Amask && format->Amask) ||
         (palette_has_alpha && format->Amask) ||
         (copy_flags & SDL_COPY_MODULATE_ALPHA)) {
         SDL_SetSurfaceBlendMode(convert, SDL_BLENDMODE_BLEND);
