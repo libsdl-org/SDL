4ef44bbaf647451b9cc16b85564df4ae6f137aba
Brick <6098371+0x1F9F1@users.noreply.github.com>
2021-09-29 19:11:33 +0100

    Tweak documentation to indicate SDL_PumpEvents isn't always called

--- include/SDL_events.h
+++ include/SDL_events.h
@@ -801,7 +801,7 @@ extern DECLSPEC void SDLCALL SDL_FlushEvents(Uint32 minType, Uint32 maxType);
  * If `event` is NULL, it simply returns 1 if there is an event in the queue,
  * but will not remove it from the queue.
  *
- * As this function implicitly calls SDL_PumpEvents(), you can only call this
+ * As this function may implicitly call SDL_PumpEvents(), you can only call this
  * function in the thread that set the video mode.
  *
  * SDL_PollEvent() is the favored way of receiving system events since it can
@@ -841,7 +841,7 @@ extern DECLSPEC int SDLCALL SDL_PollEvent(SDL_Event * event);
  * If `event` is not NULL, the next event is removed from the queue and stored
  * in the SDL_Event structure pointed to by `event`.
  *
- * As this function implicitly calls SDL_PumpEvents(), you can only call this
+ * As this function may implicitly call SDL_PumpEvents(), you can only call this
  * function in the thread that initialized the video subsystem.
  *
  * \param event the SDL_Event structure to be filled in with the next event
@@ -862,7 +862,7 @@ extern DECLSPEC int SDLCALL SDL_WaitEvent(SDL_Event * event);
  * If `event` is not NULL, the next event is removed from the queue and stored
  * in the SDL_Event structure pointed to by `event`.
  *
- * As this function implicitly calls SDL_PumpEvents(), you can only call this
+ * As this function may implicitly call SDL_PumpEvents(), you can only call this
  * function in the thread that initialized the video subsystem.
  *
  * \param event the SDL_Event structure to be filled in with the next event
