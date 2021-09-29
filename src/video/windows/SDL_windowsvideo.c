8f4dd6cf8c25cf5f2079f6660381edbc0234f834
Zach Reedy <zreedy@opera.com>
2021-09-29 16:51:05 -0400

    Added: SDL_ClearComposition function

--- src/video/windows/SDL_windowsvideo.c
+++ src/video/windows/SDL_windowsvideo.c
@@ -214,6 +214,7 @@ WIN_CreateDevice(int devindex)
     device->StartTextInput = WIN_StartTextInput;
     device->StopTextInput = WIN_StopTextInput;
     device->SetTextInputRect = WIN_SetTextInputRect;
+    device->ClearComposition = WIN_ClearComposition;
 
     device->SetClipboardText = WIN_SetClipboardText;
     device->GetClipboardText = WIN_GetClipboardText;
