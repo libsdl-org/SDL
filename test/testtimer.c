e2ba84a53220ce0ebed68861428db575497c78c4
Eddy Jansson <eddy@klopper.net>
2021-11-02 19:30:17 +0100

    testtimer: Be more like the C90's.

--- test/testtimer.c
+++ test/testtimer.c
@@ -60,12 +60,10 @@ main(int argc, char *argv[])
         start64 = SDL_GetTicks64();
         start32 = SDL_GetTicks();
         SDL_Delay(1);
-        now64 = SDL_GetTicks64();
-        now32 = SDL_GetTicks();
-        Uint32 dt32 = now32-start32;
-        Uint64 dt64 = now64-start64;
-        if (dt32 > 100 || dt64 > 100) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "testtimer.c: Delta time erratic at iter %d. Delay 1ms = %d ms in ticks, %d ms in ticks64\n", i, (int)dt32, (int)dt64);
+        now64 = SDL_GetTicks64() - start64;
+        now32 = SDL_GetTicks() - start32;
+        if (now32 > 100 || now64 > 100) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "testtimer.c: Delta time erratic at iter %d. Delay 1ms = %d ms in ticks, %d ms in ticks64\n", i, (int)now32, (int)now64);
             SDL_Quit();
             return 1;
         }
