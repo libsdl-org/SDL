6657cbce8dcea9abe4501c8bbf85f2b40e6c927a
Ryan C. Gordon <icculus@icculus.org>
2021-11-07 20:43:49 -0500

    include: Updated SDL_config_emscripten.h from emscripten-ports.
    Reference issue #4623.

--- include/SDL_config_emscripten.h
+++ include/SDL_config_emscripten.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2021 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -38,27 +38,24 @@
 #define HAVE_GCC_ATOMICS 1
 
 /* Useful headers */
-#define STDC_HEADERS 1
 #define HAVE_ALLOCA_H 1
-#define HAVE_CTYPE_H 1
-#define HAVE_ICONV_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_LIMITS_H 1
-#define HAVE_MALLOC_H 1
-#define HAVE_MATH_H 1
-#define HAVE_MEMORY_H 1
-#define HAVE_SIGNAL_H 1
-#define HAVE_STDARG_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDIO_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_STRING_H 1
 #define HAVE_SYS_TYPES_H 1
-#define HAVE_WCHAR_H 1
+#define HAVE_STDIO_H 1
+#define STDC_HEADERS 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_MALLOC_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_CTYPE_H 1
+#define HAVE_MATH_H 1
+#define HAVE_ICONV_H 1
+#define HAVE_SIGNAL_H 1
 
 /* C library functions */
-#define HAVE_DLOPEN 1
 #define HAVE_MALLOC 1
 #define HAVE_CALLOC 1
 #define HAVE_REALLOC 1
@@ -75,20 +72,11 @@
 #define HAVE_MEMCPY 1
 #define HAVE_MEMMOVE 1
 #define HAVE_MEMCMP 1
-#define HAVE_WCSLEN 1
-#define HAVE_WCSDUP 1
-#define HAVE_WCSSTR 1
-#define HAVE_WCSCMP 1
-#define HAVE_WCSNCMP 1
-#define HAVE_WCSCASECMP 1
-#define HAVE_WCSNCASECMP 1
 #define HAVE_STRLEN 1
-#define HAVE_STRLCPY 1
-#define HAVE_STRLCAT 1
+#define HAVE_STRDUP 1
 #define HAVE_STRCHR 1
 #define HAVE_STRRCHR 1
 #define HAVE_STRSTR 1
-#define HAVE_STRTOK_R 1
 #define HAVE_STRTOL 1
 #define HAVE_STRTOUL 1
 #define HAVE_STRTOLL 1
@@ -100,52 +88,35 @@
 #define HAVE_STRNCMP 1
 #define HAVE_STRCASECMP 1
 #define HAVE_STRNCASECMP 1
-#define HAVE_SSCANF 1
 #define HAVE_VSSCANF 1
 #define HAVE_VSNPRINTF 1
 #define HAVE_M_PI 1
-#define HAVE_ACOS 1
-#define HAVE_ACOSF 1
-#define HAVE_ASIN 1
-#define HAVE_ASINF 1
 #define HAVE_ATAN 1
-#define HAVE_ATANF 1
 #define HAVE_ATAN2 1
-#define HAVE_ATAN2F 1
+#define HAVE_ACOS 1
+#define HAVE_ASIN 1
 #define HAVE_CEIL 1
-#define HAVE_CEILF 1
 #define HAVE_COPYSIGN 1
-#define HAVE_COPYSIGNF 1
 #define HAVE_COS 1
 #define HAVE_COSF 1
-#define HAVE_EXP 1
-#define HAVE_EXPF 1
+#define HAVE_EXP
+#define HAVE_EXPF
 #define HAVE_FABS 1
-#define HAVE_FABSF 1
 #define HAVE_FLOOR 1
-#define HAVE_FLOORF 1
-#define HAVE_FMOD 1
-#define HAVE_FMODF 1
+#define HAVE_FMOD
+#define HAVE_FMODF
 #define HAVE_LOG 1
 #define HAVE_LOGF 1
 #define HAVE_LOG10 1
 #define HAVE_LOG10F 1
-#define HAVE_LROUND 1
-#define HAVE_LROUNDF 1
 #define HAVE_POW 1
-#define HAVE_POWF 1
-#define HAVE_ROUND  1
-#define HAVE_ROUNDF 1
 #define HAVE_SCALBN 1
-#define HAVE_SCALBNF 1
 #define HAVE_SIN 1
 #define HAVE_SINF 1
 #define HAVE_SQRT 1
 #define HAVE_SQRTF 1
 #define HAVE_TAN 1
 #define HAVE_TANF 1
-#define HAVE_TRUNC 1
-#define HAVE_TRUNCF 1
 #define HAVE_FSEEKO 1
 #define HAVE_FSEEKO64 1
 #define HAVE_SIGACTION 1
@@ -175,9 +146,6 @@
 /* Enable various input drivers */
 #define SDL_JOYSTICK_EMSCRIPTEN 1
 
-/* Enable various sensor drivers */
-#define SDL_SENSOR_DUMMY 1
-
 /* Enable various shared object loading systems */
 #define SDL_LOADSO_DLOPEN 1
 
@@ -200,9 +168,9 @@
 #define SDL_VIDEO_OPENGL_ES2 1
 /* #undef SDL_VIDEO_OPENGL_BGL */
 /* #undef SDL_VIDEO_OPENGL_CGL */
+#define SDL_VIDEO_OPENGL_EGL 1
 /* #undef SDL_VIDEO_OPENGL_GLX */
 /* #undef SDL_VIDEO_OPENGL_WGL */
-#define SDL_VIDEO_OPENGL_EGL 1
 /* #undef SDL_VIDEO_OPENGL_OSMESA */
 /* #undef SDL_VIDEO_OPENGL_OSMESA_DYNAMIC */
 
