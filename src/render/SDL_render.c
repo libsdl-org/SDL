c331cdc4772a9d3c71167176906a98c4ec76122a
Eric Wasylishen <ewasylishen@gmail.com>
2021-11-09 21:28:26 -0700

    SDL_render.c: alternate SDL_RenderWindowToLogical/SDL_RenderLogicalToWindow

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -2492,70 +2492,39 @@ SDL_RenderGetScale(SDL_Renderer * renderer, float *scaleX, float *scaleY)
 }
 
 void
-SDL_RenderWindowToLogical(SDL_Renderer * renderer, int windowX, int windowY, float *logicalX, float *logicalY){
-    int window_w;
-    int window_h;
-    int window_midpoint_x, window_midpoint_y;
-    int renderer_real_w, renderer_real_h;
-    int renderer_midpoint_x, renderer_midpoint_y;
-    int window_renderer_midpoint_delta_x, window_renderer_midpoint_delta_y;
-    int renderer_real_x, renderer_real_y;
+SDL_RenderWindowToLogical(SDL_Renderer * renderer, int windowX, int windowY, float *logicalX, float *logicalY)
+{
+    float window_physical_x, window_physical_y;
 
     CHECK_RENDERER_MAGIC(renderer, );
 
-    SDL_GetWindowSize(renderer->window, &window_w, &window_h);
-    
-    window_midpoint_x = window_w>>1;
-    window_midpoint_y = window_h>>1;
+    window_physical_x = ((float) windowX) / renderer->dpi_scale.x;
+    window_physical_y = ((float) windowY) / renderer->dpi_scale.y;
 
-    renderer_real_w = (float)renderer->logical_w*renderer->scale.x;
-    renderer_real_h = (float)renderer->logical_h*renderer->scale.y;
-
-    renderer_midpoint_x = renderer_real_w>>1;
-    renderer_midpoint_y = renderer_real_h>>1;
-
-    /* Compare with midpoints because a renderer that is logically sized will be centered and have a border
-     if the window aspect ratio differs from the renderer's.
-    */
-    window_renderer_midpoint_delta_x = window_midpoint_x - renderer_midpoint_x;
-    window_renderer_midpoint_delta_y = window_midpoint_y - renderer_midpoint_y;
-
-    renderer_real_x = windowX - window_renderer_midpoint_delta_x; 
-    renderer_real_y = windowY - window_renderer_midpoint_delta_y;
-
-    *logicalX = (float)renderer_real_x / (float)renderer_real_w * (float)renderer->logical_w; 
-    *logicalY = (float)renderer_real_y / (float)renderer_real_h * (float)renderer->logical_h; 
+    if (logicalX) {
+        *logicalX = (window_physical_x - renderer->viewport.x) / renderer->scale.x;
+    }
+    if (logicalY) {
+        *logicalY = (window_physical_y - renderer->viewport.y) / renderer->scale.y;
+    }
 }
 
-void SDLCALL SDL_RenderLogicalToWindow(SDL_Renderer * renderer, float logicalX, float logicalY, int *windowX, int *windowY){
-    int window_w;
-    int window_h;
-    int window_midpoint_x, window_midpoint_y;
-    int renderer_real_w, renderer_real_h;
-    int renderer_midpoint_x, renderer_midpoint_y;
-    int window_renderer_midpoint_delta_x, window_renderer_midpoint_delta_y;
+void
+SDL_RenderLogicalToWindow(SDL_Renderer * renderer, float logicalX, float logicalY, int *windowX, int *windowY)
+{
+    float window_physical_x, window_physical_y;
 
     CHECK_RENDERER_MAGIC(renderer, );
 
-    SDL_GetWindowSize(renderer->window, &window_w, &window_h);
-    
-    window_midpoint_x = window_w>>1;
-    window_midpoint_y = window_h>>1;
+    window_physical_x = (logicalX * renderer->scale.x) + renderer->viewport.x;
+    window_physical_y = (logicalY * renderer->scale.y) + renderer->viewport.y;
 
-    renderer_real_w = (float)renderer->logical_w*renderer->scale.x;
-    renderer_real_h = (float)renderer->logical_h*renderer->scale.y;
-
-    renderer_midpoint_x = renderer_real_w>>1;
-    renderer_midpoint_y = renderer_real_h>>1;
-
-    /* Compare with midpoints because a renderer that is logically sized will be centered and have a border
-     if the window aspect ratio differs from the renderer's.
-    */
-    window_renderer_midpoint_delta_x = window_midpoint_x - renderer_midpoint_x;
-    window_renderer_midpoint_delta_y = window_midpoint_y - renderer_midpoint_y;
-
-    *windowX = ((float)renderer_real_w * logicalX / (float)renderer->logical_w) + (float)window_renderer_midpoint_delta_x;
-    *windowY = ((float)renderer_real_h * logicalY / (float)renderer->logical_h) + (float)window_renderer_midpoint_delta_y;
+    if (windowX) {
+        *windowX = (int)(window_physical_x * renderer->dpi_scale.x);
+    }
+    if (windowY) {
+        *windowY = (int)(window_physical_y * renderer->dpi_scale.y);
+    }
 }
 
 int
