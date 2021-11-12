544d2e6aa0332c683821785d527cc8ca3788d23f
Sam Lantinga <slouken@libsdl.org>
2021-11-11 20:47:19 -0800

    Don't try to load hidapi at startup on Android

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -164,7 +164,6 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
      */
     protected String[] getLibraries() {
         return new String[] {
-            "hidapi",
             "SDL2",
             // "SDL2_image",
             // "SDL2_mixer",
