ede73b4cfc28c8b62be1b346beedefc41d3557c5
Ozkan Sezer <sezeroz@gmail.com>
2021-11-28 23:02:02 +0300

    testnativeos2.c: make its code style to match SDL2.

--- test/testnativeos2.c
+++ test/testnativeos2.c
@@ -14,46 +14,44 @@
 
 #ifdef TEST_NATIVE_OS2
 
-#define WIN_CLIENT_CLASS         "SDL Test"
+#define WIN_CLIENT_CLASS    "SDL Test"
 
 static void *CreateWindowNative(int w, int h);
 static void DestroyWindowNative(void *window);
 
 NativeWindowFactory OS2WindowFactory = {
-  "DIVE",
-  CreateWindowNative,
-  DestroyWindowNative
+    "DIVE",
+    CreateWindowNative,
+    DestroyWindowNative
 };
 
 static void *CreateWindowNative(int w, int h)
 {
-  HWND     hwnd;
-  HWND     hwndFrame;
-  ULONG    ulFrameFlags = FCF_TASKLIST | FCF_DLGBORDER | FCF_TITLEBAR |
-                          FCF_SYSMENU | FCF_SHELLPOSITION |
-                          FCF_SIZEBORDER | FCF_MINBUTTON | FCF_MAXBUTTON;
+    HWND    hwnd, hwndFrame;
+    ULONG   ulFrameFlags = FCF_TASKLIST | FCF_DLGBORDER | FCF_TITLEBAR |
+                           FCF_SYSMENU  | FCF_SHELLPOSITION |
+                           FCF_SIZEBORDER | FCF_MINBUTTON | FCF_MAXBUTTON;
 
-  WinRegisterClass( 0, WIN_CLIENT_CLASS, WinDefWindowProc,
-                    CS_SIZEREDRAW | CS_MOVENOTIFY,
-                    sizeof(ULONG) );        // We should have minimum 4 bytes.
+    WinRegisterClass(0, WIN_CLIENT_CLASS, WinDefWindowProc,
+                     CS_SIZEREDRAW | CS_MOVENOTIFY,
+                     sizeof(ULONG)); /* We should have minimum 4 bytes. */
 
-  hwndFrame = WinCreateStdWindow( HWND_DESKTOP, 0, &ulFrameFlags,
-                              WIN_CLIENT_CLASS, "SDL Test", 0, 0, 1, &hwnd );
-  if ( hwndFrame == NULLHANDLE )
-  {
-    return 0;
-  }
+    hwndFrame = WinCreateStdWindow(HWND_DESKTOP, 0, &ulFrameFlags,
+                                   WIN_CLIENT_CLASS, "SDL Test", 0, 0, 1, &hwnd);
+    if (hwndFrame == NULLHANDLE) {
+        return NULL;
+    }
 
-  WinSetWindowPos( hwndFrame, HWND_TOP, 0, 0, w, h,
-                   SWP_ZORDER | SWP_ACTIVATE | SWP_SIZE | SWP_SHOW );
+    WinSetWindowPos(hwndFrame, HWND_TOP, 0, 0, w, h,
+                    SWP_ZORDER | SWP_ACTIVATE | SWP_SIZE | SWP_SHOW);
 
-  return (void *)hwndFrame; // We may returns client or frame window handle
-                            // for SDL_CreateWindowFrom().
+    return (void *)hwndFrame;   /* We may return client or frame window
+                                   handle for SDL_CreateWindowFrom(). */
 }
 
 static void DestroyWindowNative(void *window)
 {
-  WinDestroyWindow( (HWND)window );
+    WinDestroyWindow((HWND) window);
 }
 
 #endif /* TEST_NATIVE_OS2 */
