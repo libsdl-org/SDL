9cdb2daef04409e54166983e4983a6800ac4618e
Wouter Wijsman <wouter.wijsman@qbeyond.de>
2021-11-29 20:05:53 +0100

    Make SDL2 able to build or PSP with cmake

--- src/libm/math_private.h
+++ src/libm/math_private.h
@@ -27,7 +27,7 @@
 #define libm_hidden_def(x)
 #define strong_alias(x, y)
 
-#ifndef __HAIKU__ /* already defined in a system header. */
+#if !defined(__HAIKU__) && !defined(__PSP__) /* already defined in a system header. */
 typedef unsigned int u_int32_t;
 #endif
 
