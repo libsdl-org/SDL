8d14e6ea4415da0780df70b24abe2602c68d6e3e
Ozkan Sezer <sezeroz@gmail.com>
2021-12-08 14:01:02 +0300

    replaced use of _MAX_PATH with CCHMAXPATH in os/2 code,
    it now compiles without HAVE_LIBC

--- include/SDL_config_os2.h
+++ include/SDL_config_os2.h
@@ -61,19 +61,20 @@
 
 #define HAVE_LIBC 1
 
+#define HAVE_STDARG_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDINT_H 1
+
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_STDIO_H 1
 #define STDC_HEADERS 1
 #define HAVE_STDLIB_H 1
-#define HAVE_STDARG_H 1
-#define HAVE_STDDEF_H 1
 #define HAVE_MALLOC_H 1
 #define HAVE_MEMORY_H 1
 #define HAVE_STRING_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_WCHAR_H 1
 #define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
 #define HAVE_LIMITS_H 1
 #define HAVE_CTYPE_H 1
 #define HAVE_MATH_H 1
