df1474b46e65e00acef2fc4a490d0e94fc65e686
Ozkan Sezer <sezeroz@gmail.com>
2022-01-08 11:55:10 +0300

    psp/SDL_sysfilesystem.c: fixed a minor compiler warning.
    src/filesystem/psp/SDL_sysfilesystem.c: In function 'SDL_GetPrefPath':
    src/filesystem/psp/SDL_sysfilesystem.c:71: warning: passing argument 1 of 'free' discards qualifiers from pointer target type

--- src/filesystem/psp/SDL_sysfilesystem.c
+++ src/filesystem/psp/SDL_sysfilesystem.c
@@ -51,7 +51,7 @@ SDL_GetPrefPath(const char *org, const char *app)
 {
   char *retval = NULL;
   size_t len;
-  const char *base = SDL_GetBasePath();
+  char *base = SDL_GetBasePath();
   if (!app) {
     SDL_InvalidParamError("app");
     return NULL;
@@ -62,7 +62,7 @@ SDL_GetPrefPath(const char *org, const char *app)
 
   len = SDL_strlen(base) + SDL_strlen(org) + SDL_strlen(app) + 4;
   retval = (char *) SDL_malloc(len);
-    
+
   if (*org) {
     SDL_snprintf(retval, len, "%s%s/%s/", base, org, app);
   } else {
