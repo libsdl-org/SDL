d224a63bda6a109c6b2cd63100d9fd6d98a49485
Sylvain <sylvain.becker@gmail.com>
2022-02-01 11:33:35 +0100

    Fix compilation for non Android

--- src/dynapi/SDL_dynapi_procs.h
+++ src/dynapi/SDL_dynapi_procs.h
@@ -924,4 +924,6 @@ SDL_DYNAPI_PROC(SDL_bool,SDL_GameControllerHasRumble,(SDL_GameController *a),(a)
 SDL_DYNAPI_PROC(SDL_bool,SDL_GameControllerHasRumbleTriggers,(SDL_GameController *a),(a),return)
 SDL_DYNAPI_PROC(void,SDL_hid_ble_scan,(SDL_bool a),(a),)
 SDL_DYNAPI_PROC(int,SDL_PremultiplyAlpha,(int a, int b, Uint32 c, const void *d, int e, Uint32 f, void *g, int h),(a,b,c,d,e,f,g,h),return)
+#ifdef __ANDROID__
 SDL_DYNAPI_PROC(int,SDL_AndroidSendMessage,(Uint32 a, int b),(a,b),return)
+#endif
