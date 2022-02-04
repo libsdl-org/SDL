a569a089c139c6e7c3adb13e3d8dc09c140b69ba
DominusExult <domiman+github@gmail.com>
2022-02-04 21:52:39 +0100

    iOS >= 10.0 silence CBCentralManagerState deprecation warnings

--- src/hidapi/ios/hid.m
+++ src/hidapi/ios/hid.m
@@ -58,6 +58,17 @@
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED <= __IPHONE_9_3 
+#define CBManagerState CBCentralManagerState 
+#define CBManagerStateUnknown CBCentralManagerStateUnknown 
+#define CBManagerStateResetting CBCentralManagerStateResetting 
+#define CBManagerStateUnsupported CBCentralManagerStateUnsupported 
+#define CBManagerStateUnauthorized CBCentralManagerStateUnauthorized 
+#define CBManagerStatePoweredOff CBCentralManagerStatePoweredOff 
+#define CBManagerStatePoweredOn CBCentralManagerStatePoweredOn 
+#endif
+
 // enables detailed NSLog logging of feature reports
 #define FEATURE_REPORT_LOGGING	0
 
