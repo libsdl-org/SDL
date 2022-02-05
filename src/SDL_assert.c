12211df669c26ed820d08dc4930ccf3992e8bd10
pionere <pionere@freemail.hu>
2022-02-05 10:44:26 +0100

    hide SDL_GenerateAssertionReport in case SDL_ASSERT_LEVEL is 0 otherwise SDL_PromptAssertion (and SDL_GenerateAssertionReport) can not be eliminated

--- src/SDL_assert.c
+++ src/SDL_assert.c
@@ -411,6 +411,7 @@ SDL_ReportAssertion(SDL_assert_data *data, const char *func, const char *file,
 
 void SDL_AssertionsQuit(void)
 {
+#if SDL_ASSERT_LEVEL > 0
     SDL_GenerateAssertionReport();
 #ifndef SDL_THREADS_DISABLED
     if (assertion_mutex != NULL) {
@@ -418,6 +419,7 @@ void SDL_AssertionsQuit(void)
         assertion_mutex = NULL;
     }
 #endif
+#endif /* SDL_ASSERT_LEVEL > 0 */
 }
 
 void SDL_SetAssertionHandler(SDL_AssertionHandler handler, void *userdata)
