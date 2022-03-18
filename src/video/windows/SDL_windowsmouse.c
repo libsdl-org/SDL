0387bf821ad96f57b4a22790bd1d163b85a2cb2b
Sam Lantinga <slouken@libsdl.org>
2022-03-17 17:55:28 -0700

    Fixed memory leak in WIN_CreateBlankCursor()

--- src/video/windows/SDL_windowsmouse.c
+++ src/video/windows/SDL_windowsmouse.c
@@ -175,11 +175,13 @@ WIN_CreateCursor(SDL_Surface * surface, int hot_x, int hot_y)
 static SDL_Cursor *
 WIN_CreateBlankCursor()
 {
+    SDL_Cursor *cursor = NULL;
     SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 32, 32, 32, SDL_PIXELFORMAT_ARGB8888);
     if (surface) {
-        return WIN_CreateCursor(surface, 0, 0);
+        cursor = WIN_CreateCursor(surface, 0, 0);
+        SDL_FreeSurface(surface);
     }
-    return NULL;
+    return cursor;
 }
 
 static SDL_Cursor *
