6c56193a2ad679572ed92d4413f1b88899bb5afd
Sylvain <sylvain.becker@gmail.com>
2022-03-24 18:09:45 +0100

    Fixed bug #1650: X11 doesn't set KMOD_NUM and KMOD_CAPS to system state

--- src/video/x11/SDL_x11events.h
+++ src/video/x11/SDL_x11events.h
@@ -27,6 +27,7 @@ extern void X11_PumpEvents(_THIS);
 extern int  X11_WaitEventTimeout(_THIS, int timeout);
 extern void X11_SendWakeupEvent(_THIS, SDL_Window *window);
 extern void X11_SuspendScreenSaver(_THIS);
+extern void X11_ReconcileKeyboardState(_THIS);
 
 #endif /* SDL_x11events_h_ */
 
