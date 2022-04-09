727eef7064e02aea89281493d0c5f16ad9e3c16f
Ryan C. Gordon <icculus@icculus.org>
2022-04-09 10:12:49 -0400

    audio: SDL_ConvertStereoToMono_SSE3 missed an unaligned load.

--- src/audio/SDL_audiocvt.c
+++ src/audio/SDL_audiocvt.c
@@ -80,7 +80,7 @@ SDL_ConvertStereoToMono_SSE3(SDL_AudioCVT * cvt, SDL_AudioFormat format)
        Just use unaligned load/stores, if the memory at runtime is
        aligned it'll be just as fast on modern processors */
     while (i >= 4) {   /* 4 * float32 */
-        _mm_storeu_ps(dst, _mm_mul_ps(_mm_hadd_ps(_mm_load_ps(src), _mm_loadu_ps(src+4)), divby2));
+        _mm_storeu_ps(dst, _mm_mul_ps(_mm_hadd_ps(_mm_loadu_ps(src), _mm_loadu_ps(src+4)), divby2));
         i -= 4; src += 8; dst += 4;
     }
 
