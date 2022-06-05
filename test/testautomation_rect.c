46624b4e1da7e71a456959416ae8041798ee3344
Christoph Reichenbach <creichen@gmail.com>
2022-06-05 06:53:15 +0000

    Removed two distracting gcc-11 warnings

--- test/testautomation_rect.c
+++ test/testautomation_rect.c
@@ -1231,7 +1231,7 @@ int rect_testEnclosePointsParam(void *arg)
 {
     SDL_Point points[1];
     int count;
-    SDL_Rect clip;
+    SDL_Rect clip = { 0 };
     SDL_Rect result;
     SDL_bool anyEnclosed;
 
@@ -1457,7 +1457,7 @@ int rect_testUnionRectInside(void *arg)
  */
 int rect_testUnionRectParam(void *arg)
 {
-    SDL_Rect rectA, rectB;
+    SDL_Rect rectA, rectB = { 0 };
     SDL_Rect result;
 
     /* invalid parameter combinations */
