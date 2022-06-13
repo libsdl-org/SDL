52f00833b0b38129b6d887617112d978e1d9ee29
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-06-13 20:58:18 +0200

    Add Thread drivers

--- src/thread/SDL_thread_c.h
+++ src/thread/SDL_thread_c.h
@@ -32,6 +32,8 @@
 #include "pthread/SDL_systhread_c.h"
 #elif SDL_THREAD_WINDOWS
 #include "windows/SDL_systhread_c.h"
+#elif SDL_THREAD_PS2
+#include "ps2/SDL_systhread_c.h"
 #elif SDL_THREAD_PSP
 #include "psp/SDL_systhread_c.h"
 #elif SDL_THREAD_VITA
