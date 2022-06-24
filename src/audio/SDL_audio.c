82dc64303e881e7bc147d4dc61c891b7ce7c68de
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-06-24 16:43:20 +0200

    Initial Audio driver

--- src/audio/SDL_audio.c
+++ src/audio/SDL_audio.c
@@ -95,6 +95,9 @@ static const AudioBootStrap *const bootstrap[] = {
 #if SDL_AUDIO_DRIVER_ANDROID
     &ANDROIDAUDIO_bootstrap,
 #endif
+#if SDL_AUDIO_DRIVER_PS2
+    &PS2AUDIO_bootstrap,
+#endif
 #if SDL_AUDIO_DRIVER_PSP
     &PSPAUDIO_bootstrap,
 #endif
