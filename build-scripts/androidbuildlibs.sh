2fdedd17be03678c89e65bc2bd0672b48f38c5e9
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-23 17:29:08 +0200

    Revert 6fa7d62 and 856c99e5

--- build-scripts/androidbuildlibs.sh
+++ build-scripts/androidbuildlibs.sh
@@ -68,6 +68,6 @@ ndk-build \
     NDK_LIBS_OUT=$lib \
     APP_BUILD_SCRIPT=Android.mk \
     APP_ABI="armeabi-v7a arm64-v8a x86 x86_64" \
-    APP_PLATFORM=android-19 \
+    APP_PLATFORM=android-16 \
     APP_MODULES="SDL2 SDL2_main" \
     $ndk_args
