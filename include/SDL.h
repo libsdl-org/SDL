cbb3f4ca379097b98bc8a2bbe40cd539a3f93991
Sam Lantinga <slouken@libsdl.org>
2022-07-25 19:52:46 -0700

    Add SDL_guid.h to the global SDL header

--- include/SDL.h
+++ include/SDL.h
@@ -41,6 +41,7 @@
 #include "SDL_events.h"
 #include "SDL_filesystem.h"
 #include "SDL_gamecontroller.h"
+#include "SDL_guid.h"
 #include "SDL_haptic.h"
 #include "SDL_hidapi.h"
 #include "SDL_hints.h"
