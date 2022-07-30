ce5a23bd57f0393d39c48a3cffcb90c24bc56dae
Sam Lantinga <slouken@libsdl.org>
2022-07-30 16:32:21 -0700

    Applied DragonFly BSD patch
    https://raw.githubusercontent.com/DragonFlyBSD/DeltaPorts/master/ports/devel/sdl20/dragonfly/patch-include_SDL__endian.h

--- include/SDL_endian.h
+++ include/SDL_endian.h
@@ -59,7 +59,7 @@ _m_prefetch(void *__P)
 #ifdef __linux__
 #include <endian.h>
 #define SDL_BYTEORDER  __BYTE_ORDER
-#elif defined(__OpenBSD__)
+#elif defined(__OpenBSD__) || defined(__DragonFly__)
 #include <endian.h>
 #define SDL_BYTEORDER  BYTE_ORDER
 #elif defined(__FreeBSD__) || defined(__NetBSD__)
