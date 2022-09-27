71fb91f7e43c5f046a037bf5ca59214d93fe2d51
Cameron Gutman <aicommander@gmail.com>
2022-09-26 21:38:09 -0500

    evdev: Fix build with libudev and joystick subsystem disabled

--- src/core/linux/SDL_evdev_capabilities.h
+++ src/core/linux/SDL_evdev_capabilities.h
@@ -25,8 +25,6 @@
 #ifndef SDL_evdev_capabilities_h_
 #define SDL_evdev_capabilities_h_
 
-#if HAVE_LIBUDEV_H || defined(SDL_JOYSTICK_LINUX)
-
 #include <linux/input.h>
 
 /* A device can be any combination of these classes */
@@ -52,8 +50,6 @@ extern int SDL_EVDEV_GuessDeviceClass(unsigned long bitmask_ev[NBITS(EV_MAX)],
                                       unsigned long bitmask_abs[NBITS(ABS_MAX)],
                                       unsigned long bitmask_key[NBITS(KEY_MAX)],
                                       unsigned long bitmask_rel[NBITS(REL_MAX)]);
-
-#endif /* HAVE_LIBUDEV_H || defined(SDL_JOYSTICK_LINUX) */
 
 #endif /* SDL_evdev_capabilities_h_ */
 
