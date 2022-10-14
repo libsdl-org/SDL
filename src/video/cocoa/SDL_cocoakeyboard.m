91ff88451bb01104eda608bca855f0d14cd2d7cf
Sam Lantinga <slouken@libsdl.org>
2022-10-13 23:56:17 -0700

    Disable "The key you just pressed is not recognized by SDL." message by default

--- src/video/cocoa/SDL_cocoakeyboard.m
+++ src/video/cocoa/SDL_cocoakeyboard.m
@@ -558,9 +558,9 @@ Cocoa_HandleKeyEvent(_THIS, NSEvent *event)
         }
 
         SDL_SendKeyboardKey(SDL_PRESSED, code);
-#if 1
+#ifdef DEBUG_SCANCODES
         if (code == SDL_SCANCODE_UNKNOWN) {
-            fprintf(stderr, "The key you just pressed is not recognized by SDL. To help get this fixed, report this to the SDL forums/mailing list <https://discourse.libsdl.org/> or to Christian Walther <cwalther@gmx.ch>. Mac virtual key code is %d.\n", scancode);
+            SDL_Log("The key you just pressed is not recognized by SDL. To help get this fixed, report this to the SDL forums/mailing list <https://discourse.libsdl.org/> or to Christian Walther <cwalther@gmx.ch>. Mac virtual key code is %d.\n", scancode);
         }
 #endif
         if (SDL_EventState(SDL_TEXTINPUT, SDL_QUERY)) {
