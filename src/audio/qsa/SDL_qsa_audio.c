ab61c594223290a6955e357e62a0e85306da10f7
Hubert Maier <raziel-@users.noreply.github.com>
2022-10-30 08:34:47 +0100

    SDL_QSA_AUDIO.C: Correct spelling mistake
    occured -> occurred

--- src/audio/qsa/SDL_qsa_audio.c
+++ src/audio/qsa/SDL_qsa_audio.c
@@ -117,7 +117,7 @@ QSA_WaitDevice(_THIS)
     int result;
 
     /* Setup timeout for playing one fragment equal to 2 seconds          */
-    /* If timeout occured than something wrong with hardware or driver    */
+    /* If timeout occurred than something wrong with hardware or driver    */
     /* For example, Vortex 8820 audio driver stucks on second DAC because */
     /* it doesn't exist !                                                 */
     result = SDL_IOReady(this->hidden->audio_fd,
