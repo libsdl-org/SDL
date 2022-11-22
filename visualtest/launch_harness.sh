d8864ea11de4804b49b5a7bbb573c0aa919dc0d8
Ozkan Sezer <sezeroz@gmail.com>
2022-11-22 18:47:03 +0300

    removed visualtest

--- visualtest/launch_harness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./testharness.exe --config testsprite2_crashtest.config > testrun.log 2>&1
-if [ "$?" != "0" ]; then
-  echo TEST RUN FAILED (see testrun.log)
-  # report error code to CI
-fi
