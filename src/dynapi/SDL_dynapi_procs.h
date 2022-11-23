896e200d945227ace529e8c492f35fefab3dee96
Ciro Mondueri <ciro@mondueri.com>
2022-11-23 16:41:14 -0300

    adds GDK suspend/resume basic handling (#6596)
    (cherry picked from commit f6144dfe03fc5e76e0e22242b67891fd97053787)

--- src/dynapi/SDL_dynapi_procs.h
+++ src/dynapi/SDL_dynapi_procs.h
@@ -955,6 +955,7 @@ SDL_DYNAPI_PROC(size_t,SDL_utf8strnlen,(const char *a, size_t b),(a,b),return)
 #if defined(__GDK__)
 SDL_DYNAPI_PROC(int,SDL_GDKGetTaskQueue,(XTaskQueueHandle *a),(a),return)
 SDL_DYNAPI_PROC(int,SDL_GDKRunApp,(SDL_main_func a, void *b),(a,b),return)
+SDL_DYNAPI_PROC(void,SDL_GDKSuspendComplete,(void),(),return)
 #endif
 SDL_DYNAPI_PROC(void,SDL_GetOriginalMemoryFunctions,(SDL_malloc_func *a, SDL_calloc_func *b, SDL_realloc_func *c, SDL_free_func *d),(a,b,c,d),)
 SDL_DYNAPI_PROC(void,SDL_ResetKeyboard,(void),(),)
