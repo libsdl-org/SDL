4a0fabd47a806dd026f893d439e03005654f2814
Dimitriy Ryazantcev <dimitriy.ryazantcev@gmail.com>
2022-11-26 18:37:10 +0200

    Cleanup WIN_CreateCursor() code a bit
    Closes #6476

--- src/video/SDL_shape_internals.h
+++ src/video/SDL_shape_internals.h
@@ -51,7 +51,7 @@ typedef struct SDL_ShapeTree
 
 typedef void (*SDL_TraversalFunction)(SDL_ShapeTree *, void *);
 
-extern void SDL_CalculateShapeBitmap(SDL_WindowShapeMode mode, SDL_Surface *shape, Uint8 *bitmap, Uint8 ppb);
+extern void SDL_CalculateShapeBitmap(SDL_WindowShapeMode mode, SDL_Surface *shape, Uint8 *bitmap, Uint8 ppb, Uint8 alignBytes);
 extern SDL_ShapeTree *SDL_CalculateShapeTree(SDL_WindowShapeMode mode, SDL_Surface *shape);
 extern void SDL_TraverseShapeTree(SDL_ShapeTree *tree, SDL_TraversalFunction function, void *closure);
 extern void SDL_FreeShapeTree(SDL_ShapeTree **shape_tree);
