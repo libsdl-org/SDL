1f05b0940e475c817b695214596be22bf5e72387
Sam Lantinga <slouken@libsdl.org>
2022-11-30 09:06:58 -0800

    clang-format: don't format keymap tables

--- src/core/linux/SDL_evdev_kbd_default_keymap.h
+++ src/core/linux/SDL_evdev_kbd_default_keymap.h
@@ -19,6 +19,8 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+/* *INDENT-OFF* */ /* clang-format off */
+
 static unsigned short default_key_map_0[NR_KEYS] = {
     0xf200, 0xf01b, 0xf031, 0xf032, 0xf033, 0xf034, 0xf035, 0xf036, 
     0xf037, 0xf038, 0xf039, 0xf030, 0xf02d, 0xf03d, 0xf07f, 0xf009, 
@@ -4373,6 +4375,8 @@ static unsigned short default_key_map_127[NR_KEYS] = {
     0xf200, 0xf200, 0xf200, 0xf200, 0xf200, 0xf200, 0xf200, 0xf200, 
 };
 #endif /* INCLUDE_EXTENDED_KEYMAP */
+
+/* *INDENT-ON* */ /* clang-format on */
 
 static unsigned short *default_key_maps[MAX_NR_KEYMAPS] = {
     default_key_map_0,
