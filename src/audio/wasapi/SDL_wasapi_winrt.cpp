e986c7984b60d96c13c9e29225233922808f91a1
Ryan C. Gordon <icculus@icculus.org>
2022-12-12 16:07:48 -0500

    wasapi: Fixed incorrect assertions.
    Fixes #6795.
    
    (cherry picked from commit 12486e144be0e7a37abd87b8bf90547a992ff66b)

--- src/audio/wasapi/SDL_wasapi_winrt.cpp
+++ src/audio/wasapi/SDL_wasapi_winrt.cpp
@@ -182,13 +182,13 @@ void SDL_WasapiDeviceEventHandler::OnEnumerationCompleted(DeviceWatcher ^ sender
 
 void SDL_WasapiDeviceEventHandler::OnDefaultRenderDeviceChanged(Platform::Object ^ sender, DefaultAudioRenderDeviceChangedEventArgs ^ args)
 {
-    SDL_assert(this->iscapture);
+    SDL_assert(!this->iscapture);
     SDL_AtomicAdd(&SDL_IMMDevice_DefaultPlaybackGeneration, 1);
 }
 
 void SDL_WasapiDeviceEventHandler::OnDefaultCaptureDeviceChanged(Platform::Object ^ sender, DefaultAudioCaptureDeviceChangedEventArgs ^ args)
 {
-    SDL_assert(!this->iscapture);
+    SDL_assert(this->iscapture);
     SDL_AtomicAdd(&SDL_IMMDevice_DefaultCaptureGeneration, 1);
 }
 
