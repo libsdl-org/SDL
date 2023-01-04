a80c29c4ca3e0a053e463ffd54be072a438bfe17
Sam Lantinga <slouken@libsdl.org>
2023-01-04 08:46:58 -0800

    Fixed integral constant overflow

--- src/timer/ngage/SDL_systimer.cpp
+++ src/timer/ngage/SDL_systimer.cpp
@@ -47,7 +47,7 @@ SDL_GetPerformanceFrequency(void)
 
 void SDL_DelayNS(Uint64 ns)
 {
-    const Uint64 max_delay = 0x7fffffff * SDL_NS_PER_US;
+    const Uint64 max_delay = 0x7fffffffLLU * SDL_NS_PER_US;
     if (ns > max_delay) {
         ns = max_delay;
     }
