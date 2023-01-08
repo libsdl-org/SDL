c49d02cd13916d381f3589f0a8d9b20e91af625e
Ozkan Sezer <sezeroz@gmail.com>
2023-01-09 01:30:50 +0300

    cmake (CheckPTHREAD): remove _GNU_SOURCE define after bug #7026 fix

--- cmake/sdlchecks.cmake
+++ cmake/sdlchecks.cmake
@@ -949,7 +949,6 @@ macro(CheckPTHREAD)
       list(APPEND SDL_CFLAGS ${PTHREAD_CFLAGS})
 
       check_c_source_compiles("
-        #define _GNU_SOURCE 1
         #include <pthread.h>
         int main(int argc, char **argv) {
           pthread_mutexattr_t attr;
@@ -960,7 +959,6 @@ macro(CheckPTHREAD)
         set(SDL_THREAD_PTHREAD_RECURSIVE_MUTEX 1)
       else()
         check_c_source_compiles("
-            #define _GNU_SOURCE 1
             #include <pthread.h>
             int main(int argc, char **argv) {
               pthread_mutexattr_t attr;
@@ -991,7 +989,6 @@ macro(CheckPTHREAD)
       check_include_files("pthread_np.h" HAVE_PTHREAD_NP_H)
       if (HAVE_PTHREAD_H)
         check_c_source_compiles("
-            #define _GNU_SOURCE 1
             #include <pthread.h>
             int main(int argc, char **argv) {
               pthread_setname_np(pthread_self(), \"\");
