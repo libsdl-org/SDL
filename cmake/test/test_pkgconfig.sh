131e797b43255f6e1155bec57ac10f3458d323d8
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-01-15 16:54:22 +0100

    ci: don't require EXTRA_ prefix on input environment variables

--- cmake/test/test_pkgconfig.sh
+++ cmake/test/test_pkgconfig.sh
@@ -13,7 +13,7 @@ case "$machine" in
     *android* )
         EXEPREFIX="lib"
         EXESUFFIX=".so"
-        EXTRA_LDFLAGS="$EXTRA_LDFLAGS -shared"
+        LDFLAGS="$LDFLAGS -shared"
         ;;
     * )
         EXEPREFIX=""
@@ -25,20 +25,20 @@ set -e
 
 # Get the canonical path of the folder containing this script
 testdir=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
-CFLAGS="$( pkg-config sdl2 --cflags )"
-LDFLAGS="$( pkg-config sdl2 --libs )"
-STATIC_LDFLAGS="$( pkg-config sdl2 --libs --static )"
+SDL_CFLAGS="$( pkg-config sdl2 --cflags )"
+SDL_LDFLAGS="$( pkg-config sdl2 --libs )"
+SDL_STATIC_LDFLAGS="$( pkg-config sdl2 --libs --static )"
 
-compile_cmd="$CC -c "$testdir/main_gui.c" -o main_gui_pkgconfig.c.o $CFLAGS $EXTRA_CFLAGS"
-link_cmd="$CC main_gui_pkgconfig.c.o -o ${EXEPREFIX}main_gui_pkgconfig${EXESUFFIX} $LDFLAGS $EXTRA_LDFLAGS"
-static_link_cmd="$CC main_gui_pkgconfig.c.o -o ${EXEPREFIX}main_gui_pkgconfig_static${EXESUFFIX} $STATIC_LDFLAGS $EXTRA_LDFLAGS"
+compile_cmd="$CC -c "$testdir/main_gui.c" -o main_gui_pkgconfig.c.o $SDL_CFLAGS $CFLAGS"
+link_cmd="$CC main_gui_pkgconfig.c.o -o ${EXEPREFIX}main_gui_pkgconfig${EXESUFFIX} $SDL_LDFLAGS $LDFLAGS"
+static_link_cmd="$CC main_gui_pkgconfig.c.o -o ${EXEPREFIX}main_gui_pkgconfig_static${EXESUFFIX} $SDL_STATIC_LDFLAGS $LDFLAGS"
 
-echo "-- CC:                $CC"
-echo "-- CFLAGS:            $CFLAGS"
-echo "-- EXTRA_CFLAGS:      $EXTRA_CFLAGS"
-echo "-- LDFLASG:           $LDFLAGS"
-echo "-- STATIC_LDFLAGS:    $STATIC_LDFLAGS"
-echo "-- EXTRA_LDFLAGS:     $EXTRA_LDFLAGS"
+echo "-- CC:                 $CC"
+echo "-- CFLAGS:             $CFLAGS"
+echo "-- LDFLASG:            $LDFLAGS"
+echo "-- SDL_CFLAGS:         $SDL_CFLAGS"
+echo "-- SDL_LDFLAGS:        $SDL_LDFLAGS"
+echo "-- SDL_STATIC_LDFLAGS: $SDL_STATIC_LDFLAGS"
 
 echo "-- COMPILE:       $compile_cmd"
 echo "-- LINK:          $link_cmd"
