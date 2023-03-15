899ee8f75a1c796575d8131622ded363717ff16c
Sylvain <sylvain.becker@gmail.com>
2023-03-15 22:06:41 +0100

    Change SDL_BLENDMODE_MUL for gl renderers Add FIXME for PSP

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -85,7 +85,7 @@ this should probably be removed at some point in the future.  --ryan. */
 
 #define SDL_BLENDMODE_MUL_FULL                                                                                    \
     SDL_COMPOSE_BLENDMODE(SDL_BLENDFACTOR_DST_COLOR, SDL_BLENDFACTOR_ONE_MINUS_SRC_ALPHA, SDL_BLENDOPERATION_ADD, \
-                          SDL_BLENDFACTOR_DST_ALPHA, SDL_BLENDFACTOR_ONE_MINUS_SRC_ALPHA, SDL_BLENDOPERATION_ADD)
+                          SDL_BLENDFACTOR_ZERO, SDL_BLENDFACTOR_ONE, SDL_BLENDOPERATION_ADD)
 
 #if !SDL_RENDER_DISABLED
 static const SDL_RenderDriver *render_drivers[] = {
