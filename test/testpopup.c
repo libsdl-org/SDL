b07a04992312f145932f6576c878d98a38ed8309
Sam Lantinga <slouken@libsdl.org>
2023-03-15 10:35:26 -0700

    Fixed window positioning in testpopup

--- test/testpopup.c
+++ test/testpopup.c
@@ -153,14 +153,10 @@ static void loop()
             }
             tooltip_timer = SDL_GetTicks() + TOOLTIP_DELAY;
 
-            if (num_menus > 0) {
-                int parent_x = 0, parent_y = 0;
-                int x = event.motion.x;
-                int y = event.motion.y;
+            if (num_menus > 0 && event.motion.windowID == SDL_GetWindowID(menus[0].parent)) {
+                int x = (int)event.motion.x;
+                int y = (int)event.motion.y;
 
-                SDL_GetWindowPosition(menus[0].parent, &parent_x, &parent_y);
-                x -= parent_x;
-                y -= parent_y;
                 SDL_SetWindowPosition(menus[0].win, x, y);
             }
         } else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
