37bee486a2083d6a6164f5acb3722db5eefb0305
Sylvain <sylvain.becker@gmail.com>
2023-03-25 10:19:31 +0100

    x11/SDL2 backport fix: 4b1378fe8e3f72b67e98510db20b8057baf2d2b9
    X11: fix size/position (test video_setWindowCenteredOnDisplay)
    
    this fix x11 backend to correctly pass video_setWindowCenteredOnDisplay()
    - get border values early  (eg status bar)
    - wait for size/position change to get valid values
    
    d4d26e0ddb912ca006e179c33978a933f004e0cc
    
    testautomation_video: if SDL_SetWindowSize/Position isn't honored, we should check there is an event
    x11: send the events if various occasions

--- src/video/x11/SDL_x11events.h
+++ src/video/x11/SDL_x11events.h
@@ -28,6 +28,7 @@ extern int X11_WaitEventTimeout(_THIS, int timeout);
 extern void X11_SendWakeupEvent(_THIS, SDL_Window *window);
 extern void X11_SuspendScreenSaver(_THIS);
 extern void X11_ReconcileKeyboardState(_THIS);
+extern void X11_GetBorderValues(void /*SDL_WindowData*/ *data);
 
 #endif /* SDL_x11events_h_ */
 
