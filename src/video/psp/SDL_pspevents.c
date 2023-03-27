b8ecb78f2b6fc7491118eab351e49568204ac930
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-27 16:07:26 +0200

    Fix -Wundef warnings due to use of unguarded SDL_VIDEO_DRIVER_PSP

--- src/video/psp/SDL_pspevents.c
+++ src/video/psp/SDL_pspevents.c
@@ -20,7 +20,7 @@
 */
 #include "SDL_internal.h"
 
-#if SDL_VIDEO_DRIVER_PSP
+#ifdef SDL_VIDEO_DRIVER_PSP
 
 /* Being a null driver, there's no event stream. We just define stubs for
    most of the API. */
