5bbff323c714ae8fe87fe9cefac8a2e09dac79a6
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-27 15:20:19 +0200

    Fix -Wundef warnings due to use of unguarded __APPLE__

--- src/file/SDL_rwops.c
+++ src/file/SDL_rwops.c
@@ -570,7 +570,7 @@ SDL_RWFromFile(const char *file, const char *mode)
     rwops->type = SDL_RWOPS_WINFILE;
 #elif HAVE_STDIO_H
     {
-#if __APPLE__ && !SDL_FILE_DISABLED // TODO: add dummy?
+#if defined(__APPLE__) && !SDL_FILE_DISABLED // TODO: add dummy?
         FILE *fp = SDL_OpenFPFromBundleOrFallback(file, mode);
 #elif __WINRT__
         FILE *fp = NULL;
