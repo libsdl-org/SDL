baf655dd6a484631e596d0bb2ca623da08d491fa
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-27 22:00:28 +0200

    Fix -Wundef warnings due to use of unguarded SDL_THREAD_STDCPP

--- src/thread/SDL_thread_c.h
+++ src/thread/SDL_thread_c.h
@@ -38,7 +38,7 @@
 #include "vita/SDL_systhread_c.h"
 #elif defined(SDL_THREAD_N3DS)
 #include "n3ds/SDL_systhread_c.h"
-#elif SDL_THREAD_STDCPP
+#elif defined(SDL_THREAD_STDCPP)
 #include "stdcpp/SDL_systhread_c.h"
 #elif SDL_THREAD_NGAGE
 #include "ngage/SDL_systhread_c.h"
