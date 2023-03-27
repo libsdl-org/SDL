90d72321e0f6a96748cfd1bb20a67017d50a5cea
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-27 20:39:24 +0200

    Fix -Wundef warnings due to use of unguarded SDL_VIDEO_RENDER_OGL

--- src/render/opengl/SDL_render_gl.c
+++ src/render/opengl/SDL_render_gl.c
@@ -20,7 +20,7 @@
 */
 #include "SDL_internal.h"
 
-#if SDL_VIDEO_RENDER_OGL && !defined(SDL_RENDER_DISABLED)
+#if defined(SDL_VIDEO_RENDER_OGL) && !defined(SDL_RENDER_DISABLED)
 #include "../../video/SDL_sysvideo.h" /* For SDL_RecreateWindow */
 #include <SDL3/SDL_opengl.h>
 #include "../SDL_sysrender.h"
