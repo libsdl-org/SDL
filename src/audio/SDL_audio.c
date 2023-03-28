e683043f82720521d550bb16b8a5fbb692a91787
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-29 01:51:15 +0200

    Fix -Wundef warnings due to use of unguarded SDL_AUDIO_DRIVER_PS2

--- src/audio/SDL_audio.c
+++ src/audio/SDL_audio.c
@@ -96,7 +96,7 @@ static const AudioBootStrap *const bootstrap[] = {
 #ifdef SDL_AUDIO_DRIVER_ANDROID
     &ANDROIDAUDIO_bootstrap,
 #endif
-#if SDL_AUDIO_DRIVER_PS2
+#ifdef SDL_AUDIO_DRIVER_PS2
     &PS2AUDIO_bootstrap,
 #endif
 #if SDL_AUDIO_DRIVER_PSP
