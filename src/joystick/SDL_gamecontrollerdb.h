4cc45f778c300fa4d524a1170952525e20f1f82a
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-29 02:21:28 +0200

    Fix -Wundef warnings due to use of unguarded SDL_JOYSTICK_PSP

--- src/joystick/SDL_gamecontrollerdb.h
+++ src/joystick/SDL_gamecontrollerdb.h
@@ -984,7 +984,7 @@ static const char *s_ControllerMappings[] = {
 #ifdef SDL_JOYSTICK_PS2
     "0000000050533220436f6e74726f6c00,PS2 Controller,crc:ed87,a:b14,b:b13,back:b0,dpdown:b6,dpleft:b7,dpright:b5,dpup:b4,leftshoulder:b10,leftstick:b1,lefttrigger:b8,leftx:a0,lefty:a1,rightshoulder:b11,rightstick:b2,righttrigger:b9,rightx:a2,righty:a3,start:b3,x:b15,y:b12,",
 #endif
-#if SDL_JOYSTICK_PSP
+#ifdef SDL_JOYSTICK_PSP
     "00000000505350206275696c74696e00,PSP builtin joypad,crc:bb86,a:b2,b:b1,back:b10,dpdown:b6,dpleft:b7,dpright:b9,dpup:b8,leftshoulder:b4,leftx:a0,lefty:a1,rightshoulder:b5,rightx:a2,righty:a3,start:b11,x:b3,y:b0,",
 #endif
 #if SDL_JOYSTICK_VITA
