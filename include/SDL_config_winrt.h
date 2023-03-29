2d022b6967092cbcb1320c4c010d2b476f31c92a
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-29 02:25:24 +0200

    Fix -Wundef warnings due to use of unguarded SDL_VIDEO_RENDER_D3D12

--- include/SDL_config_winrt.h
+++ include/SDL_config_winrt.h
@@ -208,7 +208,7 @@
 #define SDL_VIDEO_RENDER_D3D11  1
 
 /* Disable D3D12 as it's not implemented for WinRT */
-#define SDL_VIDEO_RENDER_D3D12  0
+/* #undef SDL_VIDEO_RENDER_D3D12 */
 
 #if SDL_VIDEO_OPENGL_ES2
 #define SDL_VIDEO_RENDER_OGL_ES2 1
--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -97,7 +97,7 @@ static const SDL_RenderDriver *render_drivers[] = {
 #ifdef SDL_VIDEO_RENDER_D3D11
     &D3D11_RenderDriver,
 #endif
-#if SDL_VIDEO_RENDER_D3D12
+#ifdef SDL_VIDEO_RENDER_D3D12
     &D3D12_RenderDriver,
 #endif
 #if SDL_VIDEO_RENDER_METAL
