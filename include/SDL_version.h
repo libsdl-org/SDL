ceece918e4ebe7adfe80ee35a0a4a928c0018252
Sam Lantinga <slouken@libsdl.org>
2023-06-20 11:50:19 -0700

    Updated to version 2.29.0 for development

--- include/SDL_version.h
+++ include/SDL_version.h
@@ -58,7 +58,7 @@ typedef struct SDL_version
 /* Printable format: "%d.%d.%d", MAJOR, MINOR, PATCHLEVEL
 */
 #define SDL_MAJOR_VERSION   2
-#define SDL_MINOR_VERSION   28
+#define SDL_MINOR_VERSION   29
 #define SDL_PATCHLEVEL      0
 
 /**
