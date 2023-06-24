f8391b3ffb58e18f22efcdfc7f9fdcc656622085
Sylvain <sylvain.becker@gmail.com>
2023-06-24 10:43:47 +0200

    Add SDL_disable_name_checking.h

---
+++ src/SDL_disable_name_checking.h
@@ -0,0 +1,138 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2023 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+#ifndef SDL_disable_name_checking_h_
+#define SDL_disable_name_checking_h_
+
+#undef abs
+#undef acos
+#undef acosf
+#undef asin
+#undef asinf
+#undef asprintf
+#undef atan
+#undef atan2
+#undef atan2f
+#undef atanf
+#undef atof
+#undef atoi
+#undef calloc
+#undef ceil
+#undef ceilf
+#undef copysign
+#undef copysignf
+#undef cos
+#undef cosf
+#undef crc32
+#undef exp
+#undef expf
+#undef fabs
+#undef fabsf
+#undef floor
+#undef floorf
+#undef fmod
+#undef fmodf
+#undef getenv
+#undef isalnum
+#undef isalpha
+#undef isblank
+#undef iscntrl
+#undef isdigit
+#undef isgraph
+#undef islower
+#undef isprint
+#undef ispunct
+#undef isspace
+#undef isupper
+#undef isxdigit
+#undef itoa
+#undef lltoa
+#undef log10
+#undef log10f
+#undef logf
+#undef lround
+#undef lroundf
+#undef ltoa
+#undef malloc
+#undef memcmp
+#undef memcpy
+#undef memcpy4
+#undef memmove
+#undef memset
+#undef pow
+#undef powf
+#undef qsort
+#undef realloc
+#undef round
+#undef roundf
+#undef scalbn
+#undef scalbnf
+#undef setenv
+#undef sin
+#undef sinf
+#undef snprintf
+#undef sqrt
+#undef sqrtf
+#undef sscanf
+#undef strcasecmp
+#undef strchr
+#undef strcmp
+#undef strdup
+#undef strlcat
+#undef strlcpy
+#undef strlen
+#undef strlwr
+#undef strncasecmp
+#undef strncmp
+#undef strrchr
+#undef strrev
+#undef strstr
+#undef strtod
+#undef strtokr
+#undef strtol
+#undef strtoll
+#undef strtoul
+#undef strupr
+#undef tan
+#undef tanf
+#undef tolower
+#undef toupper
+#undef trunc
+#undef truncf
+#undef uitoa
+#undef ulltoa
+#undef ultoa
+#undef utf8strlcpy
+#undef utf8strlen
+#undef vasprintf
+#undef vsnprintf
+#undef vsscanf
+#undef wcscasecmp
+#undef wcscmp
+#undef wcsdup
+#undef wcslcat
+#undef wcslcpy
+#undef wcslen
+#undef wcsncasecmp
+#undef wcsncmp
+#undef wcsstr
+
+#endif /* SDL_disable_name_checking_h_ */
+
