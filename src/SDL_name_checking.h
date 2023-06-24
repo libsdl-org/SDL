35f8fbabd4728e6c6b574e050b491b5ee1b13be0
Sylvain <sylvain.becker@gmail.com>
2023-06-24 10:43:28 +0200

    update SDL_name_checking.h

--- src/SDL_name_checking.h
+++ src/SDL_name_checking.h
@@ -21,180 +21,564 @@
 #ifndef SDL_name_checking_h_
 #define SDL_name_checking_h_
 
-#ifdef isalpha
-#undef isalpha
+#ifdef abs
+#undef abs
 #endif
+#define abs Please_use_SDL_abs_instead
+
+#ifdef acos
+#undef acos
+#endif
+#define acos Please_use_SDL_acos_instead
+
+#ifdef acosf
+#undef acosf
+#endif
+#define acosf Please_use_SDL_acosf_instead
+
+#ifdef asin
+#undef asin
+#endif
+#define asin Please_use_SDL_asin_instead
+
+#ifdef asinf
+#undef asinf
+#endif
+#define asinf Please_use_SDL_asinf_instead
+
+#ifdef asprintf
+#undef asprintf
+#endif
+#define asprintf Please_use_SDL_asprintf_instead
+
+#ifdef atan
+#undef atan
+#endif
+#define atan Please_use_SDL_atan_instead
+
+#ifdef atan2
+#undef atan2
+#endif
+#define atan2 Please_use_SDL_atan2_instead
+
+#ifdef atan2f
+#undef atan2f
+#endif
+#define atan2f Please_use_SDL_atan2f_instead
+
+#ifdef atanf
+#undef atanf
+#endif
+#define atanf Please_use_SDL_atanf_instead
+
+#ifdef atof
+#undef atof
+#endif
+#define atof Please_use_SDL_atof_instead
+
+#ifdef atoi
+#undef atoi
+#endif
+#define atoi Please_use_SDL_atoi_instead
+
+#ifdef calloc
+#undef calloc
+#endif
+#define calloc Please_use_SDL_calloc_instead
+
+#ifdef ceil
+#undef ceil
+#endif
+#define ceil Please_use_SDL_ceil_instead
+
+#ifdef ceilf
+#undef ceilf
+#endif
+#define ceilf Please_use_SDL_ceilf_instead
+
+#ifdef copysign
+#undef copysign
+#endif
+#define copysign Please_use_SDL_copysign_instead
+
+#ifdef copysignf
+#undef copysignf
+#endif
+#define copysignf Please_use_SDL_copysignf_instead
+
+#ifdef cos
+#undef cos
+#endif
+#define cos Please_use_SDL_cos_instead
+
+#ifdef cosf
+#undef cosf
+#endif
+#define cosf Please_use_SDL_cosf_instead
+
+#ifdef crc32
+#undef crc32
+#endif
+#define crc32 Please_use_SDL_crc32_instead
+
+#ifdef exp
+#undef exp
+#endif
+#define exp Please_use_SDL_exp_instead
+
+#ifdef expf
+#undef expf
+#endif
+#define expf Please_use_SDL_expf_instead
+
+#ifdef fabs
+#undef fabs
+#endif
+#define fabs Please_use_SDL_fabs_instead
+
+#ifdef fabsf
+#undef fabsf
+#endif
+#define fabsf Please_use_SDL_fabsf_instead
+
+#ifdef floor
+#undef floor
+#endif
+#define floor Please_use_SDL_floor_instead
+
+#ifdef floorf
+#undef floorf
+#endif
+#define floorf Please_use_SDL_floorf_instead
+
+#ifdef fmod
+#undef fmod
+#endif
+#define fmod Please_use_SDL_fmod_instead
+
+#ifdef fmodf
+#undef fmodf
+#endif
+#define fmodf Please_use_SDL_fmodf_instead
+
+#ifdef getenv
+#undef getenv
+#endif
+#define getenv Please_use_SDL_getenv_instead
 
 #ifdef isalnum
 #undef isalnum
 #endif
+#define isalnum Please_use_SDL_isalnum_instead
+
+#ifdef isalpha
+#undef isalpha
+#endif
+#define isalpha Please_use_SDL_isalpha_instead
 
 #ifdef isblank
 #undef isblank
 #endif
+#define isblank Please_use_SDL_isblank_instead
+
 #ifdef iscntrl
 #undef iscntrl
 #endif
+#define iscntrl Please_use_SDL_iscntrl_instead
 
 #ifdef isdigit
 #undef isdigit
 #endif
-
-#ifdef isupper
-#undef isupper
-#endif
-
-#ifdef isxdigit
-#undef isxdigit
-#endif
-
-#ifdef tolower
-#undef tolower
-#endif
-
-#ifdef toupper
-#undef toupper
-#endif
+#define isdigit Please_use_SDL_isdigit_instead
 
 #ifdef isgraph
 #undef isgraph
 #endif
+#define isgraph Please_use_SDL_isgraph_instead
 
 #ifdef islower
 #undef islower
 #endif
+#define islower Please_use_SDL_islower_instead
 
 #ifdef isprint
 #undef isprint
 #endif
+#define isprint Please_use_SDL_isprint_instead
 
 #ifdef ispunct
 #undef ispunct
 #endif
+#define ispunct Please_use_SDL_ispunct_instead
 
 #ifdef isspace
 #undef isspace
 #endif
-
-
-
-
-
-
-
-
-
-#define abs Please_use_SDL_abs_instead
-#define acosf Please_use_SDL_acosf_instead
-#define acos Please_use_SDL_acos_instead
-#define asinf Please_use_SDL_asinf_instead
-#define asin Please_use_SDL_asin_instead
-#define asprintf Please_use_SDL_asprintf_instead
-#define atan2f Please_use_SDL_atan2f_instead
-#define atan2 Please_use_SDL_atan2_instead
-#define atanf Please_use_SDL_atanf_instead
-#define atan Please_use_SDL_atan_instead
-#define atof Please_use_SDL_atof_instead
-#define atoi Please_use_SDL_atoi_instead
-#define calloc Please_use_SDL_calloc_instead
-#define ceilf Please_use_SDL_ceilf_instead
-#define ceil Please_use_SDL_ceil_instead
-#define copysignf Please_use_SDL_copysignf_instead
-#define copysign Please_use_SDL_copysign_instead
-#define cosf Please_use_SDL_cosf_instead
-#define cos Please_use_SDL_cos_instead
-#define crc32 Please_use_SDL_crc32_instead
-#define expf Please_use_SDL_expf_instead
-#define exp Please_use_SDL_exp_instead
-#define fabsf Please_use_SDL_fabsf_instead
-#define fabs Please_use_SDL_fabs_instead
-#define floorf Please_use_SDL_floorf_instead
-#define floor Please_use_SDL_floor_instead
-#define fmodf Please_use_SDL_fmodf_instead
-#define fmod Please_use_SDL_fmod_instead
-#define getenv Please_use_SDL_getenv_instead
-#define isalnum Please_use_SDL_isalnum_instead
-#define isalpha Please_use_SDL_isalpha_instead
-#define isblank Please_use_SDL_isblank_instead
-#define iscntrl Please_use_SDL_iscntrl_instead
-#define isdigit Please_use_SDL_isdigit_instead
-#define isgraph Please_use_SDL_isgraph_instead
-#define islower Please_use_SDL_islower_instead
-#define isprint Please_use_SDL_isprint_instead
-#define ispunct Please_use_SDL_ispunct_instead
 #define isspace Please_use_SDL_isspace_instead
+
+#ifdef isupper
+#undef isupper
+#endif
 #define isupper Please_use_SDL_isupper_instead
+
+#ifdef isxdigit
+#undef isxdigit
+#endif
 #define isxdigit Please_use_SDL_isxdigit_instead
+
+#ifdef itoa
+#undef itoa
+#endif
 #define itoa Please_use_SDL_itoa_instead
+
+#ifdef lltoa
+#undef lltoa
+#endif
 #define lltoa Please_use_SDL_lltoa_instead
-#define log10f Please_use_SDL_log10f_instead
+
+#ifdef log10
+#undef log10
+#endif
 #define log10 Please_use_SDL_log10_instead
+
+#ifdef log10f
+#undef log10f
+#endif
+#define log10f Please_use_SDL_log10f_instead
+
+#ifdef logf
+#undef logf
+#endif
 #define logf Please_use_SDL_logf_instead
-#define lroundf Please_use_SDL_lroundf_instead
+
+#ifdef lround
+#undef lround
+#endif
 #define lround Please_use_SDL_lround_instead
+
+#ifdef lroundf
+#undef lroundf
+#endif
+#define lroundf Please_use_SDL_lroundf_instead
+
+#ifdef ltoa
+#undef ltoa
+#endif
 #define ltoa Please_use_SDL_ltoa_instead
+
+#ifdef malloc
+#undef malloc
+#endif
 #define malloc Please_use_SDL_malloc_instead
+
+#ifdef memcmp
+#undef memcmp
+#endif
 #define memcmp Please_use_SDL_memcmp_instead
-#define memcpy4 Please_use_SDL_memcpy4_instead
+
+#ifdef memcpy
+#undef memcpy
+#endif
 #define memcpy Please_use_SDL_memcpy_instead
+
+#ifdef memcpy4
+#undef memcpy4
+#endif
+#define memcpy4 Please_use_SDL_memcpy4_instead
+
+#ifdef memmove
+#undef memmove
+#endif
 #define memmove Please_use_SDL_memmove_instead
+
+#ifdef memset
+#undef memset
+#endif
 #define memset Please_use_SDL_memset_instead
-#define powf Please_use_SDL_powf_instead
+
+#ifdef pow
+#undef pow
+#endif
 #define pow Please_use_SDL_pow_instead
+
+#ifdef powf
+#undef powf
+#endif
+#define powf Please_use_SDL_powf_instead
+
+#ifdef qsort
+#undef qsort
+#endif
 #define qsort Please_use_SDL_qsort_instead
+
+#ifdef realloc
+#undef realloc
+#endif
 #define realloc Please_use_SDL_realloc_instead
-#define roundf Please_use_SDL_roundf_instead
+
+#ifdef round
+#undef round
+#endif
 #define round Please_use_SDL_round_instead
-#define scalbnf Please_use_SDL_scalbnf_instead
+
+#ifdef roundf
+#undef roundf
+#endif
+#define roundf Please_use_SDL_roundf_instead
+
+#ifdef scalbn
+#undef scalbn
+#endif
 #define scalbn Please_use_SDL_scalbn_instead
+
+#ifdef scalbnf
+#undef scalbnf
+#endif
+#define scalbnf Please_use_SDL_scalbnf_instead
+
+#ifdef setenv
+#undef setenv
+#endif
 #define setenv Please_use_SDL_setenv_instead
-#define sinf Please_use_SDL_sinf_instead
+
+#ifdef sin
+#undef sin
+#endif
 #define sin Please_use_SDL_sin_instead
+
+#ifdef sinf
+#undef sinf
+#endif
+#define sinf Please_use_SDL_sinf_instead
+
+#ifdef snprintf
+#undef snprintf
+#endif
 #define snprintf Please_use_SDL_snprintf_instead
-#define sqrtf Please_use_SDL_sqrtf_instead
+
+#ifdef sqrt
+#undef sqrt
+#endif
 #define sqrt Please_use_SDL_sqrt_instead
+
+#ifdef sqrtf
+#undef sqrtf
+#endif
+#define sqrtf Please_use_SDL_sqrtf_instead
+
+#ifdef sscanf
+#undef sscanf
+#endif
 #define sscanf Please_use_SDL_sscanf_instead
+
+#ifdef strcasecmp
+#undef strcasecmp
+#endif
 #define strcasecmp Please_use_SDL_strcasecmp_instead
+
+#ifdef strchr
+#undef strchr
+#endif
 #define strchr Please_use_SDL_strchr_instead
+
+#ifdef strcmp
+#undef strcmp
+#endif
 #define strcmp Please_use_SDL_strcmp_instead
+
+#ifdef strdup
+#undef strdup
+#endif
 #define strdup Please_use_SDL_strdup_instead
+
+#ifdef strlcat
+#undef strlcat
+#endif
 #define strlcat Please_use_SDL_strlcat_instead
+
+#ifdef strlcpy
+#undef strlcpy
+#endif
 #define strlcpy Please_use_SDL_strlcpy_instead
+
+#ifdef strlen
+#undef strlen
+#endif
 #define strlen Please_use_SDL_strlen_instead
+
+#ifdef strlwr
+#undef strlwr
+#endif
 #define strlwr Please_use_SDL_strlwr_instead
+
+#ifdef strncasecmp
+#undef strncasecmp
+#endif
 #define strncasecmp Please_use_SDL_strncasecmp_instead
+
+#ifdef strncmp
+#undef strncmp
+#endif
 #define strncmp Please_use_SDL_strncmp_instead
+
+#ifdef strrchr
+#undef strrchr
+#endif
 #define strrchr Please_use_SDL_strrchr_instead
+
+#ifdef strrev
+#undef strrev
+#endif
 #define strrev Please_use_SDL_strrev_instead
+
+#ifdef strstr
+#undef strstr
+#endif
 #define strstr Please_use_SDL_strstr_instead
+
+#ifdef strtod
+#undef strtod
+#endif
 #define strtod Please_use_SDL_strtod_instead
+
+#ifdef strtokr
+#undef strtokr
+#endif
 #define strtokr Please_use_SDL_strtokr_instead
-#define strtoll Please_use_SDL_strtoll_instead
+
+#ifdef strtol
+#undef strtol
+#endif
 #define strtol Please_use_SDL_strtol_instead
+
+#ifdef strtoll
+#undef strtoll
+#endif
+#define strtoll Please_use_SDL_strtoll_instead
+
+#ifdef strtoul
+#undef strtoul
+#endif
 #define strtoul Please_use_SDL_strtoul_instead
+
+#ifdef strupr
+#undef strupr
+#endif
 #define strupr Please_use_SDL_strupr_instead
-#define tanf Please_use_SDL_tanf_instead
+
+#ifdef tan
+#undef tan
+#endif
 #define tan Please_use_SDL_tan_instead
+
+#ifdef tanf
+#undef tanf
+#endif
+#define tanf Please_use_SDL_tanf_instead
+
+#ifdef tolower
+#undef tolower
+#endif
 #define tolower Please_use_SDL_tolower_instead
+
+#ifdef toupper
+#undef toupper
+#endif
 #define toupper Please_use_SDL_toupper_instead
-#define truncf Please_use_SDL_truncf_instead
+
+#ifdef trunc
+#undef trunc
+#endif
 #define trunc Please_use_SDL_trunc_instead
+
+#ifdef truncf
+#undef truncf
+#endif
+#define truncf Please_use_SDL_truncf_instead
+
+#ifdef uitoa
+#undef uitoa
+#endif
 #define uitoa Please_use_SDL_uitoa_instead
+
+#ifdef ulltoa
+#undef ulltoa
+#endif
 #define ulltoa Please_use_SDL_ulltoa_instead
+
+#ifdef ultoa
+#undef ultoa
+#endif
 #define ultoa Please_use_SDL_ultoa_instead
+
+#ifdef utf8strlcpy
+#undef utf8strlcpy
+#endif
 #define utf8strlcpy Please_use_SDL_utf8strlcpy_instead
+
+#ifdef utf8strlen
+#undef utf8strlen
+#endif
 #define utf8strlen Please_use_SDL_utf8strlen_instead
+
+#ifdef vasprintf
+#undef vasprintf
+#endif
 #define vasprintf Please_use_SDL_vasprintf_instead
+
+#ifdef vsnprintf
+#undef vsnprintf
+#endif
 #define vsnprintf Please_use_SDL_vsnprintf_instead
+
+#ifdef vsscanf
+#undef vsscanf
+#endif
 #define vsscanf Please_use_SDL_vsscanf_instead
+
+#ifdef wcscasecmp
+#undef wcscasecmp
+#endif
 #define wcscasecmp Please_use_SDL_wcscasecmp_instead
+
+#ifdef wcscmp
+#undef wcscmp
+#endif
 #define wcscmp Please_use_SDL_wcscmp_instead
+
+#ifdef wcsdup
+#undef wcsdup
+#endif
 #define wcsdup Please_use_SDL_wcsdup_instead
+
+#ifdef wcslcat
+#undef wcslcat
+#endif
 #define wcslcat Please_use_SDL_wcslcat_instead
+
+#ifdef wcslcpy
+#undef wcslcpy
+#endif
 #define wcslcpy Please_use_SDL_wcslcpy_instead
+
+#ifdef wcslen
+#undef wcslen
+#endif
 #define wcslen Please_use_SDL_wcslen_instead
+
+#ifdef wcsncasecmp
+#undef wcsncasecmp
+#endif
 #define wcsncasecmp Please_use_SDL_wcsncasecmp_instead
+
+#ifdef wcsncmp
+#undef wcsncmp
+#endif
 #define wcsncmp Please_use_SDL_wcsncmp_instead
+
+#ifdef wcsstr
+#undef wcsstr
+#endif
 #define wcsstr Please_use_SDL_wcsstr_instead
 
 #endif /* SDL_name_checking_h_ */
