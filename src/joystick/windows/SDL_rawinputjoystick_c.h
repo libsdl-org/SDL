e8b6edd5a081d0b45c2ac39ab690e584a6b95a84
Sam Lantinga <slouken@libsdl.org>
2023-07-11 10:50:43 -0700

    RAWINPUT_RegisterNotifications() should return int, for consistency
    (cherry picked from commit 1903d7b1bf6c73ef23e4d6934c9fec5246dd0b2e)

--- src/joystick/windows/SDL_rawinputjoystick_c.h
+++ src/joystick/windows/SDL_rawinputjoystick_c.h
@@ -28,8 +28,8 @@ extern SDL_bool RAWINPUT_IsEnabled();
 extern SDL_bool RAWINPUT_IsDevicePresent(Uint16 vendor_id, Uint16 product_id, Uint16 version, const char *name);
 
 /* Registers for input events */
-extern SDL_bool RAWINPUT_RegisterNotifications(HWND hWnd);
-extern void RAWINPUT_UnregisterNotifications();
+extern int RAWINPUT_RegisterNotifications(HWND hWnd);
+extern int RAWINPUT_UnregisterNotifications();
 
 /* Returns 0 if message was handled */
 extern LRESULT CALLBACK RAWINPUT_WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
