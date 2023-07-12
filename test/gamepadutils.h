ee34805053bd845976ea000a3c98c0ac8fdf2fe3
Sam Lantinga <slouken@libsdl.org>
2023-07-11 18:56:29 -0700

    Added ABXY button labels to the gamepad image

--- test/gamepadutils.h
+++ test/gamepadutils.h
@@ -17,6 +17,7 @@ typedef struct GamepadImage GamepadImage;
 extern GamepadImage *CreateGamepadImage(SDL_Renderer *renderer);
 extern void SetGamepadImagePosition(GamepadImage *ctx, int x, int y);
 extern void SetGamepadImageShowingFront(GamepadImage *ctx, SDL_bool showing_front);
+extern void SetGamepadImageReverseDiamond(GamepadImage *ctx, SDL_bool reverse_diamond);
 extern void SetGamepadImageShowingBattery(GamepadImage *ctx, SDL_bool showing_battery);
 extern void SetGamepadImageShowingTouchpad(GamepadImage *ctx, SDL_bool showing_touchpad);
 extern void GetGamepadImageArea(GamepadImage *ctx, int *x, int *y, int *width, int *height);
