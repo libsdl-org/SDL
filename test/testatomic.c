ba0c270c7a5b9ce37f489e88103450b1b449493b
Sam Lantinga <slouken@libsdl.org>
2023-07-22 18:39:10 -0700

    Removed invalid assert in testatomic
    (cherry picked from commit 8adab0b88ab1c0aa69242fffc48afdba085b1dc4)

--- test/testatomic.c
+++ test/testatomic.c
@@ -234,7 +234,8 @@ static void RunEpicTest()
     v = SDL_AtomicGet(&good);
     SDL_Log("Atomic %d Non-Atomic %d\n", v, bad);
     SDL_assert(v == Expect);
-    SDL_assert(bad != Expect);
+    /* We can't guarantee that bad != Expect, this would happen on a single core system, for example. */
+    /*SDL_assert(bad != Expect);*/
 }
 
 /* End atomic operation test */
