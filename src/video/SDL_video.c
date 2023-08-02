fb3ab3f113b252dd731221f419dbcb92f7604efa
Ozkan Sezer <sezeroz@gmail.com>
2023-08-02 17:02:02 +0300

    SDL_video.c: move ngage video before offscreen.
    Reference issue: https://github.com/libsdl-org/SDL/pull/8069.

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -18,6 +18,7 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
+
 #include "SDL_internal.h"
 
 /* The high-level video driver subsystem */
@@ -118,11 +119,11 @@ static VideoBootStrap *bootstrap[] = {
 #ifdef SDL_VIDEO_DRIVER_QNX
     &QNX_bootstrap,
 #endif
-#ifdef SDL_VIDEO_DRIVER_OFFSCREEN
-    &OFFSCREEN_bootstrap,
-#endif
 #ifdef SDL_VIDEO_DRIVER_NGAGE
     &NGAGE_bootstrap,
+#endif
+#ifdef SDL_VIDEO_DRIVER_OFFSCREEN
+    &OFFSCREEN_bootstrap,
 #endif
 #ifdef SDL_VIDEO_DRIVER_DUMMY
     &DUMMY_bootstrap,
