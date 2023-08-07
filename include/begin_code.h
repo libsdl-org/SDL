15fef875390756c13c45c3977bf474af4695ed35
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-07 21:26:38 +0200

    MSVC has __declspec(deprecated)

--- include/begin_code.h
+++ include/begin_code.h
@@ -36,6 +36,8 @@
 #ifndef SDL_DEPRECATED
 #  if defined(__GNUC__) && (__GNUC__ >= 4)  /* technically, this arrived in gcc 3.1, but oh well. */
 #    define SDL_DEPRECATED __attribute__((deprecated))
+#  elif defined(_MSC_VER)
+#    define SDL_DEPRECATED __declspec(deprecated)
 #  else
 #    define SDL_DEPRECATED
 #  endif
