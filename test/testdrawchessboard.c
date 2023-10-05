183606d3d41750909a147137d9311ca4fa6efdbd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-10-05 13:41:01 +0200

    testdrawchessboard: clean up renderer and window

--- test/testdrawchessboard.c
+++ test/testdrawchessboard.c
@@ -151,6 +151,9 @@ int main(int argc, char *argv[])
     }
 #endif
 
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
     SDL_Quit();
     SDLTest_CommonDestroyState(state);
     return 0;
