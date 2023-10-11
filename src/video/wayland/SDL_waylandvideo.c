9a5f7b17c60c534df73cce9dacfb3a02857b5fd0
Frank Praznik <frank.praznik@gmail.com>
2023-10-11 11:23:24 -0400

    Use SDL wrapped getenv function

--- src/video/wayland/SDL_waylandvideo.c
+++ src/video/wayland/SDL_waylandvideo.c
@@ -129,8 +129,8 @@ static SDL_VideoDevice *Wayland_CreateDevice(void)
     struct wl_display *display;
 
     /* Are we trying to connect to or are currently in a Wayland session? */
-    if (!getenv("WAYLAND_DISPLAY")) {
-        const char *session = getenv("XDG_SESSION_TYPE");
+    if (!SDL_getenv("WAYLAND_DISPLAY")) {
+        const char *session = SDL_getenv("XDG_SESSION_TYPE");
         if (session && SDL_strcasecmp(session, "wayland")) {
             return NULL;
         }
