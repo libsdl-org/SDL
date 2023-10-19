0d7c5a2c560194486620187a4449e761c75e2933
Sam Lantinga <slouken@libsdl.org>
2023-10-19 10:12:34 -0700

    Updated Android API documentation

--- include/SDL3/SDL_system.h
+++ include/SDL3/SDL_system.h
@@ -289,28 +289,31 @@ extern DECLSPEC void * SDLCALL SDL_AndroidGetActivity(void);
 /**
  * Query Android API level of the current device.
  *
- * - API level 31: Android 12
- * - API level 30: Android 11
- * - API level 29: Android 10
- * - API level 28: Android 9
- * - API level 27: Android 8.1
- * - API level 26: Android 8.0
- * - API level 25: Android 7.1
- * - API level 24: Android 7.0
- * - API level 23: Android 6.0
- * - API level 22: Android 5.1
- * - API level 21: Android 5.0
- * - API level 20: Android 4.4W
- * - API level 19: Android 4.4
- * - API level 18: Android 4.3
- * - API level 17: Android 4.2
- * - API level 16: Android 4.1
- * - API level 15: Android 4.0.3
- * - API level 14: Android 4.0
- * - API level 13: Android 3.2
- * - API level 12: Android 3.1
- * - API level 11: Android 3.0
- * - API level 10: Android 2.3.3
+ * - API level 34: Android 14   (UPSIDE_DOWN_CAKE)
+ * - API level 33: Android 13   (TIRAMISU)
+ * - API level 32: Android 12L  (S_V2)
+ * - API level 31: Android 12   (S)
+ * - API level 30: Android 11   (R)
+ * - API level 29: Android 10   (Q)
+ * - API level 28: Android 9    (P)
+ * - API level 27: Android 8.1  (O_MR1)
+ * - API level 26: Android 8.0  (O)
+ * - API level 25: Android 7.1  (N_MR1)
+ * - API level 24: Android 7.0  (N)
+ * - API level 23: Android 6.0  (M)
+ * - API level 22: Android 5.1  (LOLLIPOP_MR1)
+ * - API level 21: Android 5.0  (LOLLIPOP, L)
+ * - API level 20: Android 4.4W (KITKAT_WATCH)
+ * - API level 19: Android 4.4  (KITKAT)
+ * - API level 18: Android 4.3  (JELLY_BEAN_MR2)
+ * - API level 17: Android 4.2  (JELLY_BEAN_MR1)
+ * - API level 16: Android 4.1  (JELLY_BEAN)
+ * - API level 15: Android 4.0.3 (ICE_CREAM_SANDWICH_MR1)
+ * - API level 14: Android 4.0  (ICE_CREAM_SANDWICH)
+ * - API level 13: Android 3.2  (HONEYCOMB_MR2)
+ * - API level 12: Android 3.1  (HONEYCOMB_MR1)
+ * - API level 11: Android 3.0  (HONEYCOMB)
+ * - API level 10: Android 2.3.3 (GINGERBREAD_MR1)
  *
  * \returns the Android API level.
  *
