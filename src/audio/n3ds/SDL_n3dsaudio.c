1023d8ec84056ffcc15e58107b11b9eaec808a12
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2023-10-24 19:09:53 -0400

    SDL_n3dsaudio.c - don't risk leaving current_priority uninitialized
    (cherry picked from commit 6623c87d0b7f6bae2ac16991aea53551899481b9)

--- src/audio/n3ds/SDL_n3dsaudio.c
+++ src/audio/n3ds/SDL_n3dsaudio.c
@@ -253,7 +253,7 @@ static void N3DSAUDIO_CloseDevice(SDL_AudioDevice *device)
 
 static void N3DSAUDIO_ThreadInit(SDL_AudioDevice *device)
 {
-    s32 current_priority;
+    s32 current_priority = 0x30;
     svcGetThreadPriority(&current_priority, CUR_THREAD_HANDLE);
     current_priority--;
     // 0x18 is reserved for video, 0x30 is the default for main thread
