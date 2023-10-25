f52b330ed84c477a94094302aa6a3baaca87019d
Sam Lantinga <slouken@libsdl.org>
2023-10-25 09:00:26 -0700

    Added support for the HP HyperX Clutch Gladiate controller

--- src/joystick/hidapi/SDL_hidapijoystick.c
+++ src/joystick/hidapi/SDL_hidapijoystick.c
@@ -260,6 +260,7 @@ static SDL_GamepadType SDL_GetJoystickGameControllerProtocol(const char *name, U
         interface_protocol == XBONE_IFACE_PROTOCOL) {
 
         static const int SUPPORTED_VENDORS[] = {
+            0x03f0, /* HP */
             0x044f, /* Thrustmaster */
             0x045e, /* Microsoft */
             0x0738, /* Mad Catz */
