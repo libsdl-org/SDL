60e8ff16dc147780c7ffee030adc870b9e6042d1
Frank Praznik <frank.praznik@gmail.com>
2023-10-29 14:23:14 -0400

    wayland: Sanity check pointers and protocols before confining
    (cherry picked from commit 875e45e70b4086ca8cdff405231fc616f06cda7e)

--- src/video/wayland/SDL_waylandevents.c
+++ src/video/wayland/SDL_waylandevents.c
@@ -2581,6 +2581,10 @@ static void lock_pointer_to_window(SDL_Window *window,
     SDL_VideoData *d = input->display;
     struct zwp_locked_pointer_v1 *locked_pointer;
 
+    if (!d->pointer_constraints || !input->pointer) {
+        return;
+    }
+
     if (w->locked_pointer) {
         return;
     }
