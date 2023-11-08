8049af3355975c25bc5ac29ac518893d776a769b
Sam Lantinga <slouken@libsdl.org>
2023-11-08 00:34:50 -0800

    Assume all 8BitDo Xbox controllers have a share button
    Fixes https://github.com/libsdl-org/SDL/issues/8174

--- src/joystick/usb_ids.h
+++ src/joystick/usb_ids.h
@@ -55,7 +55,6 @@
 #define USB_VENDOR_VALVE        0x28de
 #define USB_VENDOR_ZEROPLUS     0x0c12
 
-#define USB_PRODUCT_8BITDO_XBOX_CONTROLLER                0x2002
 #define USB_PRODUCT_AMAZON_LUNA_CONTROLLER                0x0419
 #define USB_PRODUCT_ASTRO_C40_XBOX360                     0x0024
 #define USB_PRODUCT_BACKBONE_ONE_IOS                      0x0103
