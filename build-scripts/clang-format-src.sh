80e56b3337d96549974daf0235580897605a3283
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-09 20:36:12 +0100

    project: Also run clang-format on tests and build-scripts

--- build-scripts/clang-format-src.sh
+++ build-scripts/clang-format-src.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
-cd "$(dirname $0)/../src"
+cd "$(dirname $0)/.."
 
-echo "Running clang-format in $(pwd)"
-
-find . -regex '.*\.[chm]p*' -exec clang-format -i {} \;
+for subdir in build-scripts src test; do
+    echo "Running clang-format in $(pwd)/$subdir"
+    find $subdir -regex '.*\.[chm]p*' -exec clang-format -i {} \;
+done
 
 # Revert third-party code
 git checkout \
-    events/imKStoUCS.* \
-    hidapi \
-    joystick/controller_type.c \
-    joystick/controller_type.h \
-    joystick/hidapi/steam/controller_constants.h \
-    joystick/hidapi/steam/controller_structs.h \
-    libm \
-    stdlib/SDL_malloc.c \
-    stdlib/SDL_qsort.c \
-    stdlib/SDL_strtokr.c \
-    video/arm \
-    video/khronos \
-    video/x11/edid-parse.c \
-    video/yuv2rgb
-clang-format -i hidapi/SDL_hidapi.c
+    src/events/imKStoUCS.* \
+    src/hidapi \
+    src/joystick/controller_type.c \
+    src/joystick/controller_type.h \
+    src/joystick/hidapi/steam/controller_constants.h \
+    src/joystick/hidapi/steam/controller_structs.h \
+    src/libm \
+    src/stdlib/SDL_malloc.c \
+    src/stdlib/SDL_qsort.c \
+    src/stdlib/SDL_strtokr.c \
+    src/video/arm \
+    src/video/khronos \
+    src/video/x11/edid-parse.c \
+    src/video/yuv2rgb
+clang-format -i src/hidapi/SDL_hidapi.c
 
 # Revert generated code
-git checkout dynapi/SDL_dynapi_overrides.h
-git checkout dynapi/SDL_dynapi_procs.h
-git checkout render/metal/SDL_shaders_metal_*.h
+git checkout \
+    src/audio/SDL_audio_channel_converters.h \
+    src/audio/SDL_audio_resampler_filter.h \
+    src/dynapi/SDL_dynapi_overrides.h \
+    src/dynapi/SDL_dynapi_procs.h \
+    src/render/metal/SDL_shaders_metal_*.h
 
 echo "clang-format complete!"
