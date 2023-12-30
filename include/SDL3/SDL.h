e3d50619f89ed747f3e15e8410c13d5dd47956d7
Sam Lantinga <slouken@libsdl.org>
2023-12-29 20:04:40 -0800

    Fixed fatal error: SDL_pen.h: No such file or directory

--- include/SDL3/SDL.h
+++ include/SDL3/SDL.h
@@ -58,6 +58,7 @@
 #include <SDL3/SDL_misc.h>
 #include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_mutex.h>
+#include <SDL3/SDL_pen.h>
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_platform.h>
 #include <SDL3/SDL_power.h>
--- src/video/x11/SDL_x11pen.c
+++ src/video/x11/SDL_x11pen.c
@@ -24,7 +24,6 @@
 
 #include "../../events/SDL_pen_c.h"
 #include "../SDL_sysvideo.h"
-#include "SDL_pen.h"
 #include "SDL_x11pen.h"
 #include "SDL_x11video.h"
 #include "SDL_x11xinput2.h"
