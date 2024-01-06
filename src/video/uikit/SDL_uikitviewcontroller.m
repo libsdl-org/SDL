3a9a52fe6c40aee8d174bb756b4a339d35386633
Sam Lantinga <slouken@libsdl.org>
2024-01-06 07:31:10 -0800

    updateKeyboard should use the SDL window's screen instead of the view window's screen, which may be nil.
    Fixes https://github.com/libsdl-org/SDL/issues/8200

--- src/video/uikit/SDL_uikitviewcontroller.m
+++ src/video/uikit/SDL_uikitviewcontroller.m
@@ -511,12 +511,14 @@ static void SDLCALL SDL_HideHomeIndicatorHintChanged(void *userdata, const char 
 
 - (void)updateKeyboard
 {
+    SDL_UIKitWindowData *data = (__bridge SDL_UIKitWindowData *) window->driverdata;
+
     CGAffineTransform t = self.view.transform;
     CGPoint offset = CGPointMake(0.0, 0.0);
 #if TARGET_OS_XR
     CGRect frame = UIKit_ComputeViewFrame(window);
 #else
-    CGRect frame = UIKit_ComputeViewFrame(window, self.view.window.screen);
+    CGRect frame = UIKit_ComputeViewFrame(window, data.uiwindow.screen);
 #endif
 
     if (self.keyboardHeight) {
