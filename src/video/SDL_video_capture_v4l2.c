0c6b07076149775ced022227cde118ef333993cf
George Broughton <broughton_g182@hotmail.com>
2024-01-11 16:11:12 +0100

    Capture debug macros off by default

--- src/video/SDL_video_capture_v4l2.c
+++ src/video/SDL_video_capture_v4l2.c
@@ -32,7 +32,7 @@
 #include "../../core/linux/SDL_udev.h"
 #include <limits.h>      /* INT_MAX */
 
-#define DEBUG_VIDEO_CAPTURE_CAPTURE 1
+#define DEBUG_VIDEO_CAPTURE_CAPTURE 0
 
 #if defined(__linux__) && !defined(__ANDROID__)
 
@@ -692,6 +692,7 @@ GetFrameSize(SDL_VideoCaptureDevice *_this, Uint32 format, int index, int *width
 
 
 
+#if DEBUG_VIDEO_CAPTURE_CAPTURE
 static void
 dbg_v4l2_pixelformat(const char *str, int f) {
     SDL_Log("%s  V4L2_format=%d  %c%c%c%c", str, f,
@@ -700,6 +701,7 @@ dbg_v4l2_pixelformat(const char *str, int f) {
                 (f >> 16) & 0xff,
                 (f >> 24) & 0xff);
 }
+#endif
 
 int
 GetDeviceSpec(SDL_VideoCaptureDevice *_this, SDL_VideoCaptureSpec *spec)
