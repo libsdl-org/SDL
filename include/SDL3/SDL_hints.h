fa57ff940cffe703a00e707a842c7ec3aa4d038b
Edoardo Lolletti <edoardo762@gmail.com>
2024-01-17 22:32:59 +0100

    Remove no longer needed hint

--- include/SDL3/SDL_hints.h
+++ include/SDL3/SDL_hints.h
@@ -2162,22 +2162,6 @@ extern "C" {
 #define SDL_HINT_WAVE_TRUNCATION   "SDL_WAVE_TRUNCATION"
 
 /**
- * Tell SDL not to name threads on Windows with the 0x406D1388 Exception.
- *        The 0x406D1388 Exception is a trick used to inform Visual Studio of a
- *        thread's name, but it tends to cause problems with other debuggers,
- *        and the .NET runtime. Note that SDL 2.0.6 and later will still use
- *        the (safer) SetThreadDescription API, introduced in the Windows 10
- *        Creators Update, if available.
- *
- * The variable can be set to the following values:
- *   "0"       - SDL will raise the 0x406D1388 Exception to name threads.
- *               This is the default behavior of SDL <= 2.0.4.
- *   "1"       - SDL will not raise this exception, and threads will be unnamed. (default)
- *               This is necessary with .NET languages or debuggers that aren't Visual Studio.
- */
-#define SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING "SDL_WINDOWS_DISABLE_THREAD_NAMING"
-
-/**
  *  Controls whether menus can be opened with their keyboard shortcut (Alt+mnemonic).
  *
  *  If the mnemonics are enabled, then menus can be opened by pressing the Alt
