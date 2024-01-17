6e1b11368d72aef6c412764a8b3d50bb22e936de
Sam Lantinga <slouken@libsdl.org>
2024-01-16 21:43:54 -0800

    Removed SDL_CPUINFO_DISABLED
    CPU info is a core part of the SDL API, and shouldn't be disabled

--- src/audio/SDL_audiotypecvt.c
+++ src/audio/SDL_audiotypecvt.c
@@ -25,7 +25,7 @@
 // TODO: NEON is disabled until https://github.com/libsdl-org/SDL/issues/8352 can be fixed
 #undef SDL_NEON_INTRINSICS
 
-#ifndef SDL_CPUINFO_DISABLED
+#ifndef __EMSCRIPTEN__
 #if defined(__x86_64__) && defined(SDL_SSE2_INTRINSICS)
 #define NEED_SCALAR_CONVERTER_FALLBACKS 0 // x86_64 guarantees SSE2.
 #elif defined(__MACOS__) && defined(SDL_SSE2_INTRINSICS)
@@ -35,10 +35,10 @@
 #elif defined(__APPLE__) && defined(__ARM_ARCH) && (__ARM_ARCH >= 7) && defined(SDL_NEON_INTRINSICS)
 #define NEED_SCALAR_CONVERTER_FALLBACKS 0 // All Apple ARMv7 chips promise NEON support.
 #endif
-#endif
+#endif /* __EMSCRIPTEN__ */
 
 // Set to zero if platform is guaranteed to use a SIMD codepath here.
-#if !defined(NEED_SCALAR_CONVERTER_FALLBACKS) || defined(SDL_CPUINFO_DISABLED)
+#if !defined(NEED_SCALAR_CONVERTER_FALLBACKS)
 #define NEED_SCALAR_CONVERTER_FALLBACKS 1
 #endif
 
