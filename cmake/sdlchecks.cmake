libsdl-org/SDL

commit 31d133db40bf9d3140e91259502ab6025e879e0d

Define SDL_PLATFORM_* macros instead of underscored ones (#8875)

cmake/sdlchecks.cmake
@@ -849,7 +849,7 @@ macro(CheckPTHREAD)
         check_c_source_compiles("
             #include <pthread.h>
             int main(int argc, char **argv) {
-              #ifdef __APPLE__
+              #ifdef SDL_PLATFORM_APPLE
               pthread_setname_np(\"\");
               #else
               pthread_setname_np(pthread_self(),\"\");

commit e0d88b82b05b080ecf15d635e989d994bed1fbd3

Don't try to use SDL platform macros in configure-time checks
At the point that we run this, nothing SDL-specific is set up yet.
__APPLE__ is a compiler predefined macro that forms part of the API on
Apple platforms, so it's fine to rely on it.

This partially reverts commit 31d133db.

Fixes: 31d133db "Define SDL_PLATFORM_* macros instead of underscored ones (#8875)"
Signed-off-by: Simon McVittie <smcv@collabora.com>

cmake/sdlchecks.cmake
@@ -849,7 +849,7 @@ macro(CheckPTHREAD)
         check_c_source_compiles("
             #include <pthread.h>
             int main(int argc, char **argv) {
-              #ifdef SDL_PLATFORM_APPLE
+              #ifdef __APPLE__
               pthread_setname_np(\"\");
               #else
               pthread_setname_np(pthread_self(),\"\");