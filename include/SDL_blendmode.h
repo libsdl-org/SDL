7aab3f0db6c6e1d49a8b9464a8f6a9343f3e3565
Ryan C. Gordon <icculus@icculus.org>
2024-01-25 17:48:12 -0500

    blendmode: Corrected docs for `SDL_BLENDOPERATION_[REV_]SUBTRACT`.
    They were documented backwards.
    
    Fixes #8910.
    
    (cherry picked from commit ecbbef8b8df954da96829c06bac4ad208f83dddd)

--- include/SDL_blendmode.h
+++ include/SDL_blendmode.h
@@ -65,8 +65,8 @@ typedef enum
 typedef enum
 {
     SDL_BLENDOPERATION_ADD              = 0x1,  /**< dst + src: supported by all renderers */
-    SDL_BLENDOPERATION_SUBTRACT         = 0x2,  /**< dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES */
-    SDL_BLENDOPERATION_REV_SUBTRACT     = 0x3,  /**< src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES */
+    SDL_BLENDOPERATION_SUBTRACT         = 0x2,  /**< src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES */
+    SDL_BLENDOPERATION_REV_SUBTRACT     = 0x3,  /**< dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES */
     SDL_BLENDOPERATION_MINIMUM          = 0x4,  /**< min(dst, src) : supported by D3D9, D3D11 */
     SDL_BLENDOPERATION_MAXIMUM          = 0x5   /**< max(dst, src) : supported by D3D9, D3D11 */
 } SDL_BlendOperation;
