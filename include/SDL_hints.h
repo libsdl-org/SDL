a6d5c1fe05d0f830d687296bbc4d002984cdec05
Sam Lantinga <slouken@libsdl.org>
2024-01-27 19:11:04 -0800

    Added the environment variable SDL_LOGGING to control default log output
    (cherry picked from commit 12bdb2b4d04a7338a4687755967a3eaad25696b0)
    (cherry picked from commit f0b8fee88f4f37eecf0d16b146ff0171804298e7)

--- include/SDL_hints.h
+++ include/SDL_hints.h
@@ -1283,6 +1283,22 @@ extern "C" {
 #define SDL_HINT_LINUX_JOYSTICK_DEADZONES "SDL_LINUX_JOYSTICK_DEADZONES"
 
 /**
+ *  \brief A variable controlling the default SDL log levels.
+ *
+ *  This variable is a comma separated set of category=level tokens that define the default logging levels for SDL applications.
+ *
+ *  The category can be a numeric category, one of "app", "error", "assert", "system", "audio", "video", "render", "input", "test", or `*` for any unspecified category.
+ *
+ *  The level can be a numeric level, one of "verbose", "debug", "info", "warn", "error", "critical", or "quiet" to disable that category.
+ *
+ *  You can omit the category if you want to set the logging level for all categories.
+ *
+ *  If this hint isn't set, the default log levels are equivalent to:
+ *  "app=info,assert=warn,test=verbose,*=error"
+ */
+#define SDL_HINT_LOGGING   "SDL_LOGGING"
+
+/**
 *  \brief  When set don't force the SDL app to become a foreground process
 *
 *  This hint only applies to Mac OS X.
