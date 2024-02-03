98d7b346aaea697830ff348c647a5d27e2f3ff27
Y-T10 <t06t06ca2@gmail.com>
2024-02-03 13:44:21 +0900

    型のエイリアスを内部に移動

--- include/SDL3/SDL_events.hpp
+++ include/SDL3/SDL_events.hpp
@@ -51,12 +51,12 @@ namespace SDLcpp {
     static_assert(std::input_iterator<event_queue_iterator<true>>);
     static_assert(std::input_iterator<event_queue_iterator<false>>);
 
-    /// インクリメント時に移動元の要素を破棄する．
-    using EQ_pop_iter = event_queue_iterator<true>;
-
     inline std::vector<SDL_Event> FetchAllEvents() noexcept {
         SDL_LockEventQueue();
         assert(SDL_IsEventQueueActive());
+        /// インクリメント時に移動元の要素を破棄する．
+        using EQ_pop_iter = event_queue_iterator<true>;
+        
         // キューからイベントを移す．
         auto events = std::vector<SDL_Event>(SDL_NumOfEvent());
         std::move(EQ_pop_iter(SDL_EventQueueBegin()), EQ_pop_iter(SDL_EventQueueEnd()), events.begin());
