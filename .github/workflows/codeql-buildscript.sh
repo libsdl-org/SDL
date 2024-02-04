f7d5a7c4a2cce02051a43060bb3adc31fbb919a3
Brian <89487381+b4yuan@users.noreply.github.com>
2024-02-04 14:56:36 -0500

    Update CodeQL buildscript

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-
-cmake -S. -Bbuild -GNinja && cmake --build build
+sudo apt-get update
+sudo apt-get install build-essential git \
+    pkg-config cmake ninja-build gnome-desktop-testing libasound2-dev libpulse-dev \
+    libaudio-dev libjack-dev libsndio-dev libsamplerate0-dev libx11-dev libxext-dev \
+    libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev libwayland-dev \
+    libxkbcommon-dev libdrm-dev libgbm-dev libgl1-mesa-dev libgles2-mesa-dev \
+    libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev libudev-dev fcitx-libs-dev
+cmake -S . ~/build
+cmake --build ~/build
