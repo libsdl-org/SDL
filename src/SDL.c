26b7acce4bb2751621d2b709db540225e99003f1
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-02-24 13:46:06 +0100

    cpuinfo: reset cpu features on SDL_Quit

--- src/SDL.c
+++ src/SDL.c
@@ -41,6 +41,7 @@
 #include "SDL_log_c.h"
 #include "SDL_properties_c.h"
 #include "audio/SDL_sysaudio.h"
+#include "cpuinfo/SDL_cpuinfo_c.h"
 #include "video/SDL_video_c.h"
 #include "events/SDL_events_c.h"
 #include "haptic/SDL_haptic_c.h"
@@ -543,6 +544,8 @@ void SDL_Quit(void)
 
     SDL_ClearHints();
     SDL_AssertionsQuit();
+
+    SDL_QuitCPUInfo();
 
     SDL_QuitProperties();
     SDL_QuitLog();
