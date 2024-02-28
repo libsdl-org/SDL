ba34025423164685be80306d658e2b9e453673c1
Sam Lantinga <slouken@libsdl.org>
2024-02-28 09:00:36 -0800

    Use direct3d11 as the default renderer on Windows
    The D3D12 renderer initializes but has poor performance or graphical issues on older Intel hardware.
    
    Fixes https://github.com/libsdl-org/SDL/issues/7634
    Fixes https://github.com/libsdl-org/SDL/issues/9093

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -89,11 +89,11 @@ this should probably be removed at some point in the future.  --ryan. */
 
 #ifndef SDL_RENDER_DISABLED
 static const SDL_RenderDriver *render_drivers[] = {
-#if SDL_VIDEO_RENDER_D3D12
-    &D3D12_RenderDriver,
-#endif
 #if SDL_VIDEO_RENDER_D3D11
     &D3D11_RenderDriver,
+#endif
+#if SDL_VIDEO_RENDER_D3D12
+    &D3D12_RenderDriver,
 #endif
 #if SDL_VIDEO_RENDER_D3D
     &D3D_RenderDriver,
