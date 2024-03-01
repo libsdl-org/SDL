dadba81fd8af513d88f9b8c95624c1bd2dd9754a
Jade Macho <ade@0x0a.de>
2024-03-01 18:21:27 +0100

    d3d12: Move root sigs to D3D12_Shader_Common.hlsli, fix mismatch

--- src/render/direct3d12/D3D12_PixelShader_Colors.hlsl
+++ src/render/direct3d12/D3D12_PixelShader_Colors.hlsl
@@ -1,12 +1,5 @@
 
-#include "D3D12_PixelShader_Common.incl"
-
-#define ColorRS \
-    "RootFlags ( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |" \
-    "DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
-    "DENY_GEOMETRY_SHADER_ROOT_ACCESS |" \
-    "DENY_HULL_SHADER_ROOT_ACCESS )," \
-    "RootConstants(num32BitConstants=24, b1)"
+#include "D3D12_PixelShader_Common.hlsli"
 
 [RootSignature(ColorRS)]
 float4 main(PixelShaderInput input) : SV_TARGET0
--- src/render/direct3d12/D3D12_VertexShader.hlsl
+++ src/render/direct3d12/D3D12_VertexShader.hlsl
@@ -1,10 +1,4 @@
-#pragma pack_matrix( row_major )
-
-cbuffer VertexShaderConstants : register(b0)
-{
-    matrix model;
-    matrix projectionAndView;
-};
+#include "D3D12_Shader_Common.hlsli"
 
 struct VertexShaderInput
 {
@@ -19,36 +13,6 @@ struct VertexShaderOutput
     float2 tex : TEXCOORD0;
     float4 color : COLOR0;
 };
-
-#define ColorRS \
-    "RootFlags ( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |" \
-    "DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
-    "DENY_GEOMETRY_SHADER_ROOT_ACCESS |" \
-    "DENY_HULL_SHADER_ROOT_ACCESS )," \
-    "RootConstants(num32BitConstants=32, b0)," \
-    "RootConstants(num32BitConstants=20, b1)"\
-
-#define TextureRS \
-    "RootFlags ( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |" \
-    "            DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
-    "            DENY_GEOMETRY_SHADER_ROOT_ACCESS |" \
-    "            DENY_HULL_SHADER_ROOT_ACCESS )," \
-    "RootConstants(num32BitConstants=32, b0),"\
-    "RootConstants(num32BitConstants=20, b1),"\
-    "DescriptorTable ( SRV(t0), visibility = SHADER_VISIBILITY_PIXEL ),"\
-    "DescriptorTable ( Sampler(s0), visibility = SHADER_VISIBILITY_PIXEL )"
-
-#define AdvancedRS \
-    "RootFlags ( ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |" \
-    "            DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
-    "            DENY_GEOMETRY_SHADER_ROOT_ACCESS |" \
-    "            DENY_HULL_SHADER_ROOT_ACCESS )," \
-    "RootConstants(num32BitConstants=32, b0),"\
-    "RootConstants(num32BitConstants=20, b1),"\
-    "DescriptorTable ( SRV(t0), visibility = SHADER_VISIBILITY_PIXEL ),"\
-    "DescriptorTable ( SRV(t1), visibility = SHADER_VISIBILITY_PIXEL ),"\
-    "DescriptorTable ( SRV(t2), visibility = SHADER_VISIBILITY_PIXEL ),"\
-    "DescriptorTable ( Sampler(s0), visibility = SHADER_VISIBILITY_PIXEL )"
 
 [RootSignature(ColorRS)]
 VertexShaderOutput mainColor(VertexShaderInput input)
