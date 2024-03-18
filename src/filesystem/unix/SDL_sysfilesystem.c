c91f7e8d3c730e89c353f7a21228213cccbd651e
Petar Popovic <pjoetri@gmail.com>
2024-03-18 13:22:12 +0100

    src/filesystem/unix/SDL_sysfilesystem.c add <stdio.h> inclusion
    gets rid of some warnings

--- src/filesystem/unix/SDL_sysfilesystem.c
+++ src/filesystem/unix/SDL_sysfilesystem.c
@@ -25,6 +25,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* System dependent filesystem routines                                */
 
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
