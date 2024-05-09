54fb629da5d46187ee36d750bf5520ad08921b68
Sam Lantinga <slouken@libsdl.org>
2024-05-09 10:11:44 -0700

    SDL_WindowFlags is now 64-bit
    Fixes https://github.com/libsdl-org/SDL/issues/7321

--- include/SDL3/SDL_video.h
+++ include/SDL3/SDL_video.h
@@ -131,32 +131,32 @@ typedef struct SDL_Window SDL_Window;
  *
  * \sa SDL_GetWindowFlags
  */
-typedef Uint32 SDL_WindowFlags;
+typedef Uint64 SDL_WindowFlags;
 
-#define SDL_WINDOW_FULLSCREEN           0x00000001u /**< window is in fullscreen mode */
-#define SDL_WINDOW_OPENGL               0x00000002u /**< window usable with OpenGL context */
-#define SDL_WINDOW_OCCLUDED             0x00000004u /**< window is occluded */
-#define SDL_WINDOW_HIDDEN               0x00000008u /**< window is neither mapped onto the desktop nor shown in the taskbar/dock/window list; SDL_ShowWindow() is required for it to become visible */
-#define SDL_WINDOW_BORDERLESS           0x00000010u /**< no window decoration */
-#define SDL_WINDOW_RESIZABLE            0x00000020u /**< window can be resized */
-#define SDL_WINDOW_MINIMIZED            0x00000040u /**< window is minimized */
-#define SDL_WINDOW_MAXIMIZED            0x00000080u /**< window is maximized */
-#define SDL_WINDOW_MOUSE_GRABBED        0x00000100u /**< window has grabbed mouse input */
-#define SDL_WINDOW_INPUT_FOCUS          0x00000200u /**< window has input focus */
-#define SDL_WINDOW_MOUSE_FOCUS          0x00000400u /**< window has mouse focus */
-#define SDL_WINDOW_EXTERNAL             0x00000800u /**< window not created by SDL */
-#define SDL_WINDOW_MODAL                0x00001000u /**< window is modal */
-#define SDL_WINDOW_HIGH_PIXEL_DENSITY   0x00002000u /**< window uses high pixel density back buffer if possible */
-#define SDL_WINDOW_MOUSE_CAPTURE        0x00004000u /**< window has mouse captured (unrelated to MOUSE_GRABBED) */
-#define SDL_WINDOW_ALWAYS_ON_TOP        0x00008000u /**< window should always be above others */
-#define SDL_WINDOW_UTILITY              0x00020000u /**< window should be treated as a utility window, not showing in the task bar and window list */
-#define SDL_WINDOW_TOOLTIP              0x00040000u /**< window should be treated as a tooltip and does not get mouse or keyboard focus, requires a parent window */
-#define SDL_WINDOW_POPUP_MENU           0x00080000u /**< window should be treated as a popup menu, requires a parent window */
-#define SDL_WINDOW_KEYBOARD_GRABBED     0x00100000u /**< window has grabbed keyboard input */
-#define SDL_WINDOW_VULKAN               0x10000000u /**< window usable for Vulkan surface */
-#define SDL_WINDOW_METAL                0x20000000u /**< window usable for Metal view */
-#define SDL_WINDOW_TRANSPARENT          0x40000000u /**< window with transparent buffer */
-#define SDL_WINDOW_NOT_FOCUSABLE        0x80000000u /**< window should not be focusable */
+#define SDL_WINDOW_FULLSCREEN           SDL_UINT64_C(0x0000000000000001)    /**< window is in fullscreen mode */
+#define SDL_WINDOW_OPENGL               SDL_UINT64_C(0x0000000000000002)    /**< window usable with OpenGL context */
+#define SDL_WINDOW_OCCLUDED             SDL_UINT64_C(0x0000000000000004)    /**< window is occluded */
+#define SDL_WINDOW_HIDDEN               SDL_UINT64_C(0x0000000000000008)    /**< window is neither mapped onto the desktop nor shown in the taskbar/dock/window list; SDL_ShowWindow() is required for it to become visible */
+#define SDL_WINDOW_BORDERLESS           SDL_UINT64_C(0x0000000000000010)    /**< no window decoration */
+#define SDL_WINDOW_RESIZABLE            SDL_UINT64_C(0x0000000000000020)    /**< window can be resized */
+#define SDL_WINDOW_MINIMIZED            SDL_UINT64_C(0x0000000000000040)    /**< window is minimized */
+#define SDL_WINDOW_MAXIMIZED            SDL_UINT64_C(0x0000000000000080)    /**< window is maximized */
+#define SDL_WINDOW_MOUSE_GRABBED        SDL_UINT64_C(0x0000000000000100)    /**< window has grabbed mouse input */
+#define SDL_WINDOW_INPUT_FOCUS          SDL_UINT64_C(0x0000000000000200)    /**< window has input focus */
+#define SDL_WINDOW_MOUSE_FOCUS          SDL_UINT64_C(0x0000000000000400)    /**< window has mouse focus */
+#define SDL_WINDOW_EXTERNAL             SDL_UINT64_C(0x0000000000000800)    /**< window not created by SDL */
+#define SDL_WINDOW_MODAL                SDL_UINT64_C(0x0000000000001000)    /**< window is modal */
+#define SDL_WINDOW_HIGH_PIXEL_DENSITY   SDL_UINT64_C(0x0000000000002000)    /**< window uses high pixel density back buffer if possible */
+#define SDL_WINDOW_MOUSE_CAPTURE        SDL_UINT64_C(0x0000000000004000)    /**< window has mouse captured (unrelated to MOUSE_GRABBED) */
+#define SDL_WINDOW_ALWAYS_ON_TOP        SDL_UINT64_C(0x0000000000008000)    /**< window should always be above others */
+#define SDL_WINDOW_UTILITY              SDL_UINT64_C(0x0000000000020000)    /**< window should be treated as a utility window, not showing in the task bar and window list */
+#define SDL_WINDOW_TOOLTIP              SDL_UINT64_C(0x0000000000040000)    /**< window should be treated as a tooltip and does not get mouse or keyboard focus, requires a parent window */
+#define SDL_WINDOW_POPUP_MENU           SDL_UINT64_C(0x0000000000080000)    /**< window should be treated as a popup menu, requires a parent window */
+#define SDL_WINDOW_KEYBOARD_GRABBED     SDL_UINT64_C(0x0000000000100000)    /**< window has grabbed keyboard input */
+#define SDL_WINDOW_VULKAN               SDL_UINT64_C(0x0000000010000000)    /**< window usable for Vulkan surface */
+#define SDL_WINDOW_METAL                SDL_UINT64_C(0x0000000020000000)    /**< window usable for Metal view */
+#define SDL_WINDOW_TRANSPARENT          SDL_UINT64_C(0x0000000040000000)    /**< window with transparent buffer */
+#define SDL_WINDOW_NOT_FOCUSABLE        SDL_UINT64_C(0x0000000080000000)    /**< window should not be focusable */
 
 
 /**
