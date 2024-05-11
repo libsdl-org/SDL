026edbeab0f968346f7ac6b543dc09573c406509
Sam Lantinga <slouken@libsdl.org>
2024-05-10 17:14:30 -0700

    Use the correct type for the modifier in SDL_Keysym
    This shrinks the structure by 32-bits because we've removed implicit padding between the fields

--- include/SDL3/SDL_keyboard.h
+++ include/SDL3/SDL_keyboard.h
@@ -64,8 +64,8 @@ typedef struct SDL_Keysym
 {
     SDL_Scancode scancode;      /**< SDL physical key code - see ::SDL_Scancode for details */
     SDL_Keycode sym;            /**< SDL virtual key code - see ::SDL_Keycode for details */
-    Uint16 mod;                 /**< current key modifiers */
-    Uint32 unused;
+    SDL_Keymod mod;             /**< current key modifiers */
+    Uint16 unused;
 } SDL_Keysym;
 
 /* Function prototypes */
