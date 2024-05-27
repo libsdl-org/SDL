7d81e9d46cdf10491cc42bf2ee27fe9eab997f67
Sam Lantinga <slouken@libsdl.org>
2024-05-27 08:05:40 -0700

    Note that you can redraw in response to SDL_EVENT_WINDOW_EXPOSED from an event watch callback

--- include/SDL3/SDL_events.h
+++ include/SDL3/SDL_events.h
@@ -108,7 +108,7 @@ typedef enum SDL_EventType
     /* 0x201 was SDL_EVENT_SYSWM, reserve the number for sdl2-compat */
     SDL_EVENT_WINDOW_SHOWN = 0x202,     /**< Window has been shown */
     SDL_EVENT_WINDOW_HIDDEN,            /**< Window has been hidden */
-    SDL_EVENT_WINDOW_EXPOSED,           /**< Window has been exposed and should be redrawn */
+    SDL_EVENT_WINDOW_EXPOSED,           /**< Window has been exposed and should be redrawn, and can be redrawn directly from event watchers for this event */
     SDL_EVENT_WINDOW_MOVED,             /**< Window has been moved to data1, data2 */
     SDL_EVENT_WINDOW_RESIZED,           /**< Window has been resized to data1xdata2 */
     SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED,/**< The pixel size of the window has changed to data1xdata2 */
