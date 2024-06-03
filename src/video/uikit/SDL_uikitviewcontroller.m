297fa0bf3c4f4698b6e66d994fd28b9fbe8953d5
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-03 22:46:25 +0200

    video/uikit: fix -Wfloat-conversino warning

--- src/video/uikit/SDL_uikitviewcontroller.m
+++ src/video/uikit/SDL_uikitviewcontroller.m
@@ -523,7 +523,7 @@ static void SDLCALL SDL_HideHomeIndicatorHintChanged(void *userdata, const char 
 
     if (self.keyboardHeight) {
         int rectbottom = (int)(self.textInputRect.y + self.textInputRect.h);
-        int keybottom = self.view.bounds.size.height - self.keyboardHeight;
+        int keybottom = (int)(self.view.bounds.size.height - self.keyboardHeight);
         if (keybottom < rectbottom) {
             offset.y = keybottom - rectbottom;
         }
