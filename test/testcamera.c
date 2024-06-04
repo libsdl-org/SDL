60396971ce5469af2988933e00112ce139f374d1
Sam Lantinga <slouken@libsdl.org>
2024-06-04 10:42:44 -0700

    Move the main header with SDL_MAIN_USE_CALLBACKS

--- test/testcamera.c
+++ test/testcamera.c
@@ -11,9 +11,9 @@
 */
 
 #define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL_main.h>
 #include <SDL3/SDL_test.h>
 #include <SDL3/SDL_test_common.h>
-#include <SDL3/SDL_main.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
