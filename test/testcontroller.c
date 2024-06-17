3a68febedf7be3d83ee3259a738588070bfb5b15
Sam Lantinga <slouken@libsdl.org>
2024-06-17 15:05:12 -0700

    Enable HIDAPI controller support on mobile platforms

--- test/testcontroller.c
+++ test/testcontroller.c
@@ -1998,6 +1998,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI, "1");
     SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_PS4_RUMBLE, "1");
     SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_PS5_RUMBLE, "1");
     SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_STEAM, "1");
