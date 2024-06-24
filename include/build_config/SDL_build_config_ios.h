89cdadf7c3364617059ea5298ace6ab3b5a3a20d
Sam Lantinga <slouken@libsdl.org>
2024-06-24 09:39:53 -0700

    Added SDL_isinf(), SDL_isinff(), SDL_isnan(), and SDL_isnanf()

--- include/build_config/SDL_build_config_ios.h
+++ include/build_config/SDL_build_config_ios.h
@@ -101,6 +101,10 @@
 #define HAVE_FLOORF 1
 #define HAVE_FMOD   1
 #define HAVE_FMODF  1
+#define HAVE_ISINF  1
+#define HAVE_ISINF_FLOAT_MACRO 1
+#define HAVE_ISNAN  1
+#define HAVE_ISNAN_FLOAT_MACRO 1
 #define HAVE_LOG    1
 #define HAVE_LOGF   1
 #define HAVE_LOG10  1
