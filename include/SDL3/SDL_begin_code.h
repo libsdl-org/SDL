ecfa363889e03b905e9a5428615587d53327a996
Ryan C. Gordon <icculus@icculus.org>
2024-06-27 20:38:45 -0400

    begin_code: Solaris Studio has `__has_attribute` defined by isn't usable here.
    Fixes #10095.
    
    (cherry picked from commit d96f1d5360be6dd5f57c3f100deb80c7f39da2a5)

--- include/SDL3/SDL_begin_code.h
+++ include/SDL3/SDL_begin_code.h
@@ -165,7 +165,7 @@
     (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202000L)
 #define SDL_FALLTHROUGH [[fallthrough]]
 #else
-#ifdef __has_attribute
+#if defined(__has_attribute) && !defined(__SUNPRO_C) && !defined(__SUNPRO_CC)
 #define SDL_HAS_FALLTHROUGH __has_attribute(__fallthrough__)
 #else
 #define SDL_HAS_FALLTHROUGH 0
