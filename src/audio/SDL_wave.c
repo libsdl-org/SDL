e559b271b560e42a8c7eee3ae81a5030e7f5ed67
Sam Lantinga <slouken@libsdl.org>
2024-07-05 12:44:25 -0700

    Clear the wave format so the channel map is cleared before returning from SDL_LoadWAV_IO()

--- src/audio/SDL_wave.c
+++ src/audio/SDL_wave.c
@@ -2080,6 +2080,16 @@ int SDL_LoadWAV_IO(SDL_IOStream *src, SDL_bool closeio, SDL_AudioSpec *spec, Uin
     int result = -1;
     WaveFile file;
 
+    if (spec) {
+        SDL_zerop(spec);
+    }
+    if (audio_buf) {
+        *audio_buf = NULL;
+    }
+    if (audio_len) {
+        *audio_len = 0;
+    }
+
     /* Make sure we are passed a valid data source */
     if (!src) {
         goto done;  /* Error may come from SDL_IOStream. */
@@ -2093,9 +2103,6 @@ int SDL_LoadWAV_IO(SDL_IOStream *src, SDL_bool closeio, SDL_AudioSpec *spec, Uin
         SDL_InvalidParamError("audio_len");
         goto done;
     }
-
-    *audio_buf = NULL;
-    *audio_len = 0;
 
     SDL_zero(file);
     file.riffhint = WaveGetRiffSizeHint();
