3899cb16969d5c700a94c815b123f0e3b4bbe30a
Sam Lantinga <slouken@libsdl.org>
2024-07-08 14:46:15 -0700

    Revert "Fixed build"
    This reverts commit 026dfc63053984ef02708a5f5281b2d128fc8a05.
    
    Fixed build again!

--- src/render/direct3d/SDL_render_d3d.c
+++ src/render/direct3d/SDL_render_d3d.c
@@ -1601,8 +1601,7 @@ int D3D_CreateRenderer(SDL_Renderer *renderer, SDL_Window *window, Uint32 flags)
     if (!SDL_GetWindowWMInfo(window, &windowinfo) ||
         windowinfo.subsystem != SDL_SYSWM_WINDOWS) {
         SDL_free(data);
-        SDL_SetError("Couldn't get window handle");
-        return NULL;
+        return SDL_SetError("Couldn't get window handle");
     }
 
     window_flags = SDL_GetWindowFlags(window);
