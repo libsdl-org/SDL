026dfc63053984ef02708a5f5281b2d128fc8a05
Sam Lantinga <slouken@libsdl.org>
2024-07-08 11:39:08 -0700

    Fixed build

--- src/render/direct3d/SDL_render_d3d.c
+++ src/render/direct3d/SDL_render_d3d.c
@@ -1601,7 +1601,8 @@ int D3D_CreateRenderer(SDL_Renderer *renderer, SDL_Window *window, Uint32 flags)
     if (!SDL_GetWindowWMInfo(window, &windowinfo) ||
         windowinfo.subsystem != SDL_SYSWM_WINDOWS) {
         SDL_free(data);
-        return SDL_SetError("Couldn't get window handle");
+        SDL_SetError("Couldn't get window handle");
+        return NULL;
     }
 
     window_flags = SDL_GetWindowFlags(window);
