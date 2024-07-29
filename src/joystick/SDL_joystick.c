906d0e9c0315f44cfcd4c306dff15485824b2891
Sam Lantinga <slouken@libsdl.org>
2024-07-29 12:48:40 -0700

    Fixed crash with joystick rumble after disconnection
    This prevents continuing a rumble after the first one fails, and fixes a long standing crash issue if rumble is started immediately before the controller is disconnected.
    
    Thanks to @AntTheAlchemist for the key bug report that showed what was happening here.
    
    Fixes https://github.com/libsdl-org/SDL/issues/10422
    
    (cherry picked from commit 0a924b185d2f459a2efbe54484394a17b97f1632)
    (cherry picked from commit 7fdf7943774d2a54e48718316ca6d55d51a37696)

--- src/joystick/SDL_joystick.c
+++ src/joystick/SDL_joystick.c
@@ -1412,9 +1412,13 @@ int SDL_JoystickRumble(SDL_Joystick *joystick, Uint16 low_frequency_rumble, Uint
             retval = 0;
         } else {
             retval = joystick->driver->Rumble(joystick, low_frequency_rumble, high_frequency_rumble);
-            joystick->rumble_resend = SDL_GetTicks() + SDL_RUMBLE_RESEND_MS;
-            if (!joystick->rumble_resend) {
-                joystick->rumble_resend = 1;
+            if (retval == 0) {
+                joystick->rumble_resend = SDL_GetTicks() + SDL_RUMBLE_RESEND_MS;
+                if (joystick->rumble_resend == 0) {
+                    joystick->rumble_resend = 1;
+                }
+            } else {
+                joystick->rumble_resend = 0;
             }
         }
 
@@ -2177,12 +2181,14 @@ void SDL_JoystickUpdate(void)
 #endif /* SDL_JOYSTICK_HIDAPI */
 
     for (joystick = SDL_joysticks; joystick; joystick = joystick->next) {
-        if (joystick->attached) {
-            joystick->driver->Update(joystick);
+        if (!joystick->attached) {
+            continue;
+        }
 
-            if (joystick->delayed_guide_button) {
-                SDL_GameControllerHandleDelayedGuideButton(joystick);
-            }
+        joystick->driver->Update(joystick);
+
+        if (joystick->delayed_guide_button) {
+            SDL_GameControllerHandleDelayedGuideButton(joystick);
         }
 
         now = SDL_GetTicks();
