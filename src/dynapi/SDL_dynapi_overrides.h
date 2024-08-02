3ea0167e3f57992414a49452a6b3b3211312606f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-02 23:47:03 +0200

    Added SDL_GetWindowFromEvent

--- src/dynapi/SDL_dynapi_overrides.h
+++ src/dynapi/SDL_dynapi_overrides.h
@@ -496,6 +496,7 @@
 #define SDL_GetWindowBordersSize SDL_GetWindowBordersSize_REAL
 #define SDL_GetWindowDisplayScale SDL_GetWindowDisplayScale_REAL
 #define SDL_GetWindowFlags SDL_GetWindowFlags_REAL
+#define SDL_GetWindowFromEvent SDL_GetWindowFromEvent_REAL
 #define SDL_GetWindowFromID SDL_GetWindowFromID_REAL
 #define SDL_GetWindowFullscreenMode SDL_GetWindowFullscreenMode_REAL
 #define SDL_GetWindowICCProfile SDL_GetWindowICCProfile_REAL
