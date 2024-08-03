d4d5ef96ac9bcd49658f2c907ca163a6258fab59
Sam Lantinga <slouken@libsdl.org>
2024-08-03 09:33:32 -0700

    Added SDL_HINT_OPENGL_LIBRARY and SDL_HINT_VULKAN_LIBRARY

--- src/video/cocoa/SDL_cocoavulkan.m
+++ src/video/cocoa/SDL_cocoavulkan.m
@@ -61,7 +61,7 @@ int Cocoa_Vulkan_LoadLibrary(SDL_VideoDevice *_this, const char *path)
 
     /* Load the Vulkan loader library */
     if (!path) {
-        path = SDL_getenv("SDL_VULKAN_LIBRARY");
+        path = SDL_GetHint(SDL_HINT_VULKAN_LIBRARY);
     }
 
     if (!path) {
