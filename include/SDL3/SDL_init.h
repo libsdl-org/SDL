ff76494da846515d2fd5a41ab259f2ef7a9fa568
Maia <66437537+maia-s@users.noreply.github.com>
2024-08-27 21:44:52 +0200

    Fix trailing doc comments

--- include/SDL3/SDL_init.h
+++ include/SDL3/SDL_init.h
@@ -89,9 +89,9 @@ typedef Uint32 SDL_InitFlags;
  */
 typedef enum SDL_AppResult
 {
-    SDL_APP_CONTINUE,   /** Value that requests that the app continue from the main callbacks. */
-    SDL_APP_SUCCESS,    /** Value that requests termination with success from the main callbacks. */
-    SDL_APP_FAILURE     /** Value that requests termination with error from the main callbacks. */
+    SDL_APP_CONTINUE,   /**< Value that requests that the app continue from the main callbacks. */
+    SDL_APP_SUCCESS,    /**< Value that requests termination with success from the main callbacks. */
+    SDL_APP_FAILURE     /**< Value that requests termination with error from the main callbacks. */
 } SDL_AppResult;
 
 typedef SDL_AppResult (SDLCALL *SDL_AppInit_func)(void **appstate, int argc, char *argv[]);
