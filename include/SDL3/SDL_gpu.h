7d93e980c37d00ee37e680fecfff73bceeed304d
cosmonaut <evan@moonside.games>
2024-09-04 16:31:25 -0700

    Fix typo

--- include/SDL3/SDL_gpu.h
+++ include/SDL3/SDL_gpu.h
@@ -381,7 +381,7 @@ typedef enum SDL_GPUTextureType
 {
     SDL_GPU_TEXTURETYPE_2D,        /**< The texture is a 2-dimensional image. */
     SDL_GPU_TEXTURETYPE_2D_ARRAY,  /**< The texture is a 2-dimensional array image. */
-    SDL_GPU_TEXTURETYPE_3D,        /**< The texture is a 3-dimensional array image. */
+    SDL_GPU_TEXTURETYPE_3D,        /**< The texture is a 3-dimensional image. */
     SDL_GPU_TEXTURETYPE_CUBE       /**< The texture is a cube image. */
 } SDL_GPUTextureType;
 
