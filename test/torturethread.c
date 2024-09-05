165e8a7d3b3aafa49352d806c796ca777ce16d70
Sam Lantinga <slouken@libsdl.org>
2024-09-05 05:35:03 -0700

    Use atomic variables for thread communication
    Fixes https://github.com/libsdl-org/SDL/issues/10711

--- test/torturethread.c
+++ test/torturethread.c
@@ -37,7 +37,8 @@ quit(int rc)
 static int SDLCALL
 SubThreadFunc(void *data)
 {
-    while (!*(int volatile *)data) {
+    SDL_AtomicInt *flag = (SDL_AtomicInt *)data;
+    while (!SDL_AtomicGet(flag)) {
         SDL_Delay(10);
     }
     return 0;
@@ -47,7 +48,7 @@ static int SDLCALL
 ThreadFunc(void *data)
 {
     SDL_Thread *sub_threads[NUMTHREADS];
-    int flags[NUMTHREADS];
+    SDL_AtomicInt flags[NUMTHREADS];
     int i;
     int tid = (int)(uintptr_t)data;
 
@@ -56,7 +57,7 @@ ThreadFunc(void *data)
     for (i = 0; i < NUMTHREADS; i++) {
         char name[64];
         (void)SDL_snprintf(name, sizeof(name), "Child%d_%d", tid, i);
-        flags[i] = 0;
+        SDL_AtomicSet(&flags[i], 0);
         sub_threads[i] = SDL_CreateThread(SubThreadFunc, name, &flags[i]);
     }
 
@@ -67,7 +68,7 @@ ThreadFunc(void *data)
 
     SDL_Log("Thread '%d' sending signals to subthreads\n", tid);
     for (i = 0; i < NUMTHREADS; i++) {
-        flags[i] = 1;
+        SDL_AtomicSet(&flags[i], 1);
         SDL_WaitThread(sub_threads[i], NULL);
     }
 
