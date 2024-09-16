8291b1be363ceada6ad64844a4467a5e09763fdf
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-16 22:56:12 +0200

    Port SDL3 release scripts to SDL2
    [skip ci]

--- mingw/pkg-support/cmake/sdl2-config-version.cmake
+++ mingw/pkg-support/cmake/sdl2-config-version.cmake
@@ -2,9 +2,9 @@
 # This file is meant to be placed in a cmake subfolder of SDL2-devel-2.x.y-mingw
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 4)
-    set(sdl2_config_path "${CMAKE_CURRENT_LIST_DIR}/../i686-w64-mingw32/lib/cmake/SDL2/sdl2-config-version.cmake")
+    set(sdl2_config_path "${CMAKE_CURRENT_LIST_DIR}/../i686-w64-mingw32/lib/cmake/SDL2/SDL2ConfigVersion.cmake")
 elseif(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(sdl2_config_path "${CMAKE_CURRENT_LIST_DIR}/../x86_64-w64-mingw32/lib/cmake/SDL2/sdl2-config-version.cmake")
+    set(sdl2_config_path "${CMAKE_CURRENT_LIST_DIR}/../x86_64-w64-mingw32/lib/cmake/SDL2/SDL2ConfigVersion.cmake")
 else()
     set(PACKAGE_VERSION_UNSUITABLE TRUE)
     return()
