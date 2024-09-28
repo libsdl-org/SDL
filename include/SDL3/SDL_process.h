5910aa4c9e7a14d0789caf5532c627b14aef5c63
Petar Popovic <pjoetri@gmail.com>
2024-09-28 15:17:14 +0200

    Remove trailing comma in enum

--- include/SDL3/SDL_process.h
+++ include/SDL3/SDL_process.h
@@ -145,7 +145,7 @@ typedef enum SDL_ProcessIO
     SDL_PROCESS_STDIO_INHERITED,    /**< The I/O stream is inherited from the application. */
     SDL_PROCESS_STDIO_NULL,         /**< The I/O stream is ignored. */
     SDL_PROCESS_STDIO_APP,          /**< The I/O stream is connected to a new SDL_IOStream that the application can read or write */
-    SDL_PROCESS_STDIO_REDIRECT,     /**< The I/O stream is redirected to an existing SDL_IOStream. */
+    SDL_PROCESS_STDIO_REDIRECT      /**< The I/O stream is redirected to an existing SDL_IOStream. */
 } SDL_ProcessIO;
 
 /**
