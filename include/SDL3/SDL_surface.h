84ecf7fd3e1fc2f694b309216a5ae6e1a58395a0
Ryan C. Gordon <icculus@icculus.org>
2024-10-04 16:35:34 -0400

    docs: Add CategorySurface text.

--- include/SDL3/SDL_surface.h
+++ include/SDL3/SDL_surface.h
@@ -22,7 +22,17 @@
 /**
  * # CategorySurface
  *
- * SDL_Surface definition and management functions.
+ * SDL surfaces are buffers of pixels in system RAM. These are useful for
+ * passing around and manipulating images that are not stored in GPU memory.
+ *
+ * SDL_Surface makes serious efforts to manage images in various formats,
+ * and provides a reasonable toolbox for transforming the data, including
+ * copying between surfaces, filling rectangles in the image data, etc.
+ *
+ * There is also a simple .bmp loader, SDL_LoadBMP(). SDL itself does not
+ * provide loaders for various other file formats, but there are several
+ * excellent external libraries that do, including it's own satellite library
+ * [SDL_image](https://github.com/libsdl-org/SDL_image).
  */
 
 #ifndef SDL_surface_h_
