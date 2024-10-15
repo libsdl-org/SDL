72bae5167e6e02d48a2dd454ad313b9c5fc28a49
Sam Lantinga <slouken@libsdl.org>
2024-10-14 20:28:09 -0700

    Updated documentation
    Fixes https://github.com/libsdl-org/sdlwiki/issues/616

--- include/SDL3/SDL_gpu.h
+++ include/SDL3/SDL_gpu.h
@@ -128,9 +128,7 @@
  * explaining this situation.
  *
  * It is optimal for apps to pre-compile the shader formats they might use,
- * but for ease of use SDL provides a
- * [satellite single-header library](https://github.com/libsdl-org/SDL_gpu_shadercross
- * )
+ * but for ease of use SDL provides a separate project, [SDL_gpu_shadercross](https://github.com/libsdl-org/SDL_gpu_shadercross),
  * for performing runtime shader cross-compilation.
  *
  * This is an extremely quick overview that leaves out several important
