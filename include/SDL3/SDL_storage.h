0051c66016671bebf2c03a1ae4e3a5f4777d3939
Petar Popovic <pjoetri@gmail.com>
2024-10-27 20:34:32 +0100

    SDL_Enumerate[Storage]Directory() comments: Use enum constant names instead of integers.

--- include/SDL3/SDL_storage.h
+++ include/SDL3/SDL_storage.h
@@ -318,10 +318,10 @@ extern SDL_DECLSPEC bool SDLCALL SDL_CreateStorageDirectory(SDL_Storage *storage
  *
  * This function provides every directory entry through an app-provided
  * callback, called once for each directory entry, until all results have been
- * provided or the callback returns <= 0.
+ * provided or the callback returns either SDL_ENUM_SUCCESS or SDL_ENUM_FAILURE.
  *
  * This will return false if there was a system problem in general, or if a
- * callback returns -1. A successful return means a callback returned 1 to
+ * callback returns SDL_ENUM_FAILURE. A successful return means a callback returned SDL_ENUM_SUCCESS to
  * halt enumeration, or all directory entries were enumerated.
  *
  * \param storage a storage container.
