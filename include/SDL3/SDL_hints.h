ab674ee7d56bc6c7c09a0fd1c6a3c916652794e1
Ryan C. Gordon <icculus@icculus.org>
2024-11-20 20:05:54 -0500

    kmsdrm: Restore atomic support.

--- include/SDL3/SDL_hints.h
+++ include/SDL3/SDL_hints.h
@@ -2474,6 +2474,28 @@ extern "C" {
 #define SDL_HINT_KMSDRM_REQUIRE_DRM_MASTER "SDL_KMSDRM_REQUIRE_DRM_MASTER"
 
 /**
+ * A variable that controls whether KMSDRM will use "atomic" functionality.
+ *
+ * The KMSDRM backend can use atomic commits, if both DRM_CLIENT_CAP_ATOMIC
+ * and DRM_CLIENT_CAP_UNIVERSAL_PLANES is supported by the system. As of
+ * SDL 3.4.0, it will favor this functionality, but in case this doesn't
+ * work well on a given system or other surprises, this hint can be used
+ * to disable it.
+ *
+ * This hint can not enable the functionality if it isn't available.
+ *
+ * The variable can be set to the following values:
+ *
+ * - "0": SDL will not use the KMSDRM "atomic" functionality.
+ * - "1": SDL will allow usage of the KMSDRM "atomic" functionality. (default)
+ *
+ * This hint should be set before SDL is initialized.
+ *
+ * \since This hint is available since SDL 3.4.0.
+ */
+#define SDL_HINT_KMSDRM_ATOMIC "SDL_KMSDRM_ATOMIC"
+
+/**
  * A variable controlling the default SDL log levels.
  *
  * This variable is a comma separated set of category=level tokens that define
