5c8bed378040007d0e2e9267ff4bc76c016cee8f
Caleb Cornett <caleb.cornett@outlook.com>
2024-11-25 22:33:48 -0500

    GPU: Fix D3D12 tearing query

--- src/gpu/d3d12/SDL_gpu_d3d12.c
+++ src/gpu/d3d12/SDL_gpu_d3d12.c
@@ -733,7 +733,7 @@ struct D3D12Renderer
     SDL_SharedObject *dxgidebug_dll;
 #endif
     ID3D12Debug *d3d12Debug;
-    bool supportsTearing;
+    BOOL supportsTearing;
     SDL_SharedObject *d3d12_dll;
     ID3D12Device *device;
     PFN_D3D12_SERIALIZE_ROOT_SIGNATURE D3D12SerializeRootSignature_func;
