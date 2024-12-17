080b3002c69f888d83f5de25be876ec4a672ed43
Sam Lantinga <slouken@libsdl.org>
2024-12-16 22:33:05 -0800

    Fixed building with an older Windows SDK

--- src/file/windows/SDL_asyncio_windows_ioring.c
+++ src/file/windows/SDL_asyncio_windows_ioring.c
@@ -56,7 +56,7 @@ static void *ioring_handle = NULL;
     SDL_IORING_FUNC(HRESULT, SetIoRingCompletionEvent, (HIORING ioRing, HANDLE hEvent)) \
     SDL_IORING_FUNC(HRESULT, BuildIoRingCancelRequest, (HIORING ioRing, IORING_HANDLE_REF file, UINT_PTR opToCancel, UINT_PTR userData)) \
     SDL_IORING_FUNC(HRESULT, BuildIoRingReadFile, (HIORING ioRing, IORING_HANDLE_REF fileRef, IORING_BUFFER_REF dataRef, UINT32 numberOfBytesToRead, UINT64 fileOffset, UINT_PTR userData, IORING_SQE_FLAGS sqeFlags)) \
-    SDL_IORING_FUNC(HRESULT, BuildIoRingWriteFile, (HIORING ioRing, IORING_HANDLE_REF fileRef, IORING_BUFFER_REF bufferRef, UINT32 numberOfBytesToWrite, UINT64 fileOffset, FILE_WRITE_FLAGS writeFlags, UINT_PTR userData, IORING_SQE_FLAGS sqeFlags)) \
+    SDL_IORING_FUNC(HRESULT, BuildIoRingWriteFile, (HIORING ioRing, IORING_HANDLE_REF fileRef, IORING_BUFFER_REF bufferRef, UINT32 numberOfBytesToWrite, UINT64 fileOffset, int /*FILE_WRITE_FLAGS*/ writeFlags, UINT_PTR userData, IORING_SQE_FLAGS sqeFlags)) \
     SDL_IORING_FUNC(HRESULT, BuildIoRingFlushFile, (HIORING ioRing, IORING_HANDLE_REF fileRef, FILE_FLUSH_MODE flushMode, UINT_PTR userData, IORING_SQE_FLAGS sqeFlags)) \
 
 #define SDL_IORING_FUNC(ret, fn, args) typedef ret (WINAPI *SDL_fntype_##fn) args;
@@ -413,7 +413,7 @@ static bool ioring_asyncio_write(void *userdata, SDL_AsyncIOTask *task)
     // have to hold a lock because otherwise two threads could get_sqe and submit while one request isn't fully set up.
     SDL_LockMutex(queuedata->sqe_lock);
     bool retval;
-    const HRESULT hr = ioring.BuildIoRingWriteFile(queuedata->ring, href, bref, (UINT32) task->requested_size, task->offset, FILE_WRITE_FLAGS_NONE, (UINT_PTR) task, IOSQE_FLAGS_NONE);
+    const HRESULT hr = ioring.BuildIoRingWriteFile(queuedata->ring, href, bref, (UINT32) task->requested_size, task->offset, 0 /*FILE_WRITE_FLAGS_NONE*/, (UINT_PTR) task, IOSQE_FLAGS_NONE);
     if (FAILED(hr)) {
         retval = WIN_SetErrorFromHRESULT("BuildIoRingWriteFile", hr);
     } else {
