6fa6297441e387abdc565daa44a04e07acd55624
Ozkan Sezer <sezeroz@gmail.com>
2024-12-17 11:15:56 +0300

    fix typo from commit b84f150d0669592f

--- src/hidapi/SDL_hidapi.c
+++ src/hidapi/SDL_hidapi.c
@@ -1242,7 +1242,7 @@ int SDL_hid_init(void)
         return -1;
     }
 
-#if defined(HAVE_PLATFORM_BACKEND) && !defined(SDL_HIDAPI_DISABLED)
+#if defined(SDL_PLATFORM_MACOS) && !defined(SDL_HIDAPI_DISABLED)
     hid_darwin_set_open_exclusive(0);
 #endif
 
