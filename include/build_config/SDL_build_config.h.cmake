beece416d2197e9883cfb41d95bd41c1dd828e0e
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-12-23 18:46:25 -0800

    cmake: Escape SDL_GPU_OPENXR_DYNAMIC in cmake script

--- include/build_config/SDL_build_config.h.cmake
+++ include/build_config/SDL_build_config.h.cmake
@@ -436,7 +436,7 @@
 #cmakedefine SDL_GPU_VULKAN 1
 #cmakedefine SDL_GPU_METAL 1
 #cmakedefine HAVE_GPU_OPENXR @HAVE_GPU_OPENXR@
-#cmakedefine SDL_GPU_OPENXR_DYNAMIC "@SDL_GPU_OPENXR_DYNAMIC@"
+#cmakedefine SDL_GPU_OPENXR_DYNAMIC @SDL_GPU_OPENXR_DYNAMIC@
 
 /* Enable system power support */
 #cmakedefine SDL_POWER_ANDROID 1
