22d8e73530c39f81f39f1567e695d8779a762459
Ryan C. Gordon <icculus@icculus.org>
2025-01-16 13:16:18 -0500

    include: filled in category documentation for SDL_messagebox.h.
    Reference Issue #11847.

--- include/SDL3/SDL_messagebox.h
+++ include/SDL3/SDL_messagebox.h
@@ -22,7 +22,18 @@
 /**
  * # CategoryMessagebox
  *
- * Message box support routines.
+ * SDL offers a simple message box API, which is useful for simple alerts,
+ * such as informing the user when something fatal happens at startup without
+ * the need to build a UI for it (or informing the user _before_ your UI is
+ * ready).
+ *
+ * These message boxes are native system dialogs where possible.
+ *
+ * There is both a customizable function (SDL_ShowMessageBox()) that offers
+ * lots of options for what to display and reports on what choice the user
+ * made, and also a much-simplified version (SDL_ShowSimpleMessageBox()),
+ * merely takes a text message and title, and waits until the user presses
+ * a single "OK" UI button. Often, this is all that is necessary.
  */
 
 #ifndef SDL_messagebox_h_
