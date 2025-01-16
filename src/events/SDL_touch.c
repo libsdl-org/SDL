878ea48b607f23e4ec8c12d1395b86ab529e30d0
ROllerozxa <rollerozxa@voxelmanip.se>
2025-01-16 20:18:11 +0100

    Vita: Fix off-by-one error for synthetic mouse events
    (cherry picked from commit cdc5483cf9683fabf0a897954a510574e61af418)

--- src/events/SDL_touch.c
+++ src/events/SDL_touch.c
@@ -252,7 +252,7 @@ int SDL_SendTouch(SDL_TouchID id, SDL_FingerID fingerid, SDL_Window *window,
     /* SDL_HINT_VITA_TOUCH_MOUSE_DEVICE: controlling which touchpad should generate synthetic mouse events, PSVita-only */
     {
 #if defined(__vita__)
-        if (mouse->touch_mouse_events && ((mouse->vita_touch_mouse_device == id) || (mouse->vita_touch_mouse_device == 2))) {
+        if (mouse->touch_mouse_events && ((mouse->vita_touch_mouse_device == id) || (mouse->vita_touch_mouse_device == 3))) {
 #else
         if (mouse->touch_mouse_events) {
 #endif
