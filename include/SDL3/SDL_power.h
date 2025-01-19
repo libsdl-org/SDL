0851322fb1d3af03c3637062cb2ee03043f05214
Ryan C. Gordon <icculus@icculus.org>
2025-01-19 12:11:16 -0500

    include: Add category documentation to SDL_power.h.
    Reference Issue #11847.

--- include/SDL3/SDL_power.h
+++ include/SDL3/SDL_power.h
@@ -26,6 +26,15 @@
  * # CategoryPower
  *
  * SDL power management routines.
+ *
+ * There is a single function in this category: SDL_GetPowerInfo().
+ *
+ * This function is useful for games on the go. This allows an app
+ * to know if it's running on a draining battery, which can be useful if
+ * the app wants to reduce processing, or perhaps framerate, to extend the
+ * duration of the battery's charge. Perhaps the app just wants to show
+ * a battery meter when fullscreen, or alert the user when the power is
+ * getting extremely low, so they can save their game.
  */
 
 #include <SDL3/SDL_stdinc.h>
