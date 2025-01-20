90b2e2527eeb6614cedd15f61062f62680252be6
Ryan C. Gordon <icculus@icculus.org>
2025-01-20 01:39:37 -0500

    include: Added a tiny bit to SDL_sensor.h's category documentation.
    Reference Issue #11847.

--- include/SDL3/SDL_sensor.h
+++ include/SDL3/SDL_sensor.h
@@ -24,6 +24,8 @@
  *
  * SDL sensor management.
  *
+ * These APIs grant access to gyros and accelerometers on various platforms.
+ *
  * In order to use these functions, SDL_Init() must have been called with the
  * SDL_INIT_SENSOR flag. This causes SDL to scan the system for sensors, and
  * load appropriate drivers.
