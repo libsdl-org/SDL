cf249b0cb28336d14bbd6cb0bd4f711f6ad4db87
Timothee Besset <ttimo@valvesoftware.com>
2025-01-28 13:32:57 -0600

    fix -Wformat problem in 32 bit builds

--- src/video/wayland/SDL_waylanddatamanager.c
+++ src/video/wayland/SDL_waylanddatamanager.c
@@ -382,7 +382,7 @@ void *Wayland_data_offer_receive(SDL_WaylandDataOffer *offer,
         close(pipefd[0]);
     }
     SDL_LogTrace(SDL_LOG_CATEGORY_INPUT,
-                 ". In Wayland_data_offer_receive for '%s', buffer (%ld) at %p",
+                 ". In Wayland_data_offer_receive for '%s', buffer (%zu) at %p",
                  mime_type, *length, buffer);
     return buffer;
 }
@@ -418,7 +418,7 @@ void *Wayland_primary_selection_offer_receive(SDL_WaylandPrimarySelectionOffer *
         close(pipefd[0]);
     }
     SDL_LogTrace(SDL_LOG_CATEGORY_INPUT,
-                 ". In Wayland_primary_selection_offer_receive for '%s', buffer (%ld) at %p",
+                 ". In Wayland_primary_selection_offer_receive for '%s', buffer (%zu) at %p",
                  mime_type, *length, buffer);
     return buffer;
 }
