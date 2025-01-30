71c085234e518d6fb99ccf287e1205d62b1477de
John Alanbrook <john@pockle.world>
2025-01-30 13:15:25 -0600

    check for backslashes as well as the forward slash

--- src/filesystem/SDL_filesystem.c
+++ src/filesystem/SDL_filesystem.c
@@ -363,16 +363,16 @@ char **SDL_InternalGlobDirectory(const char *path, const char *pattern, SDL_Glob
         return NULL;
     }
 
-    // if path ends with any '/', chop them off, so we don't confuse the pattern matcher later.
+    // if path ends with any slash, chop them off, so we don't confuse the pattern matcher later.
     char *pathcpy = NULL;
     size_t pathlen = SDL_strlen(path);
-    if ((pathlen > 1) && (path[pathlen-1] == '/')) {
+    if ((pathlen > 1) && ((path[pathlen-1] == '/') || (path[pathlen-1] == '\\'))) {
         pathcpy = SDL_strdup(path);
         if (!pathcpy) {
             return NULL;
         }
         char *ptr = &pathcpy[pathlen-1];
-        while ((ptr >= pathcpy) && (*ptr == '/')) {
+        while ((ptr >= pathcpy) && ((*ptr == '/') || (*ptr == '\\'))) {
             *(ptr--) = '\0';
         }
         path = pathcpy;
