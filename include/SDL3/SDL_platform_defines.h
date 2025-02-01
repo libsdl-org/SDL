2adf68f48b17cfcdccc1358ab7ee1fe4cc2b7a06
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-02-01 19:38:39 +0100

    [Nokia N-Gage] Update configuration after adding stdbool.h, stdint.h and wchar.h to the toolchain.

--- include/SDL3/SDL_platform_defines.h
+++ include/SDL3/SDL_platform_defines.h
@@ -482,16 +482,6 @@
  */
 #define SDL_PLATFORM_NGAGE 1
 
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
-typedef signed long long int64_t;
-typedef unsigned long long uint64_t;
-typedef unsigned long uintptr_t;
-typedef signed long intptr_t;
 typedef long ssize_t;
 
 #undef __NGAGE__
