523e6530a89f72f993f49f5ecb7dc709974319a3
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-19 19:06:39 +0100

    SDL_test: fix "'function': different 'const' qualifiers" warning in SDL_test_harness

--- src/test/SDL_test_harness.c
+++ src/test/SDL_test_harness.c
@@ -515,7 +515,7 @@ int SDLTest_ExecuteTestSuiteRunner(SDLTest_TestSuiteRunner *runner)
 
     arraySuites = SDL_malloc(nbSuites * sizeof(int));
     if (!arraySuites) {
-        SDL_free(failedTests);
+        SDL_free((void *)failedTests);
         return SDL_OutOfMemory();
     }
     for (i = 0; i < nbSuites; i++) {
@@ -588,7 +588,7 @@ int SDLTest_ExecuteTestSuiteRunner(SDLTest_TestSuiteRunner *runner)
             arrayTestCases = SDL_malloc(nbTestCases * sizeof(int));
             if (!arrayTestCases) {
                 SDL_free(arraySuites);
-                SDL_free(failedTests);
+                SDL_free((void *)failedTests);
                 return SDL_OutOfMemory();
             }
             for (j = 0; j < nbTestCases; j++) {
