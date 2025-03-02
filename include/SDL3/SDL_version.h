292e4317483afe437baf7ffcf04526e7f6d0d684
Sam Lantinga <slouken@libsdl.org>
2025-03-01 18:30:34 -0800

    Updated to version 3.2.6 for release

--- include/SDL3/SDL_version.h
+++ include/SDL3/SDL_version.h
@@ -62,7 +62,7 @@ extern "C" {
  *
  * \since This macro is available since SDL 3.2.0.
  */
-#define SDL_MICRO_VERSION   5
+#define SDL_MICRO_VERSION   6
 
 /**
  * This macro turns the version numbers into a numeric value.
