1d6bb62870d86bf6ced549ed2223c31b9dc418d9
Frank Praznik <frank.praznik@gmail.com>
2025-03-03 18:39:44 -0500

    Use _this pointer to reference the video device
    (cherry picked from commit 3b9f0dff1023dfde49e645e318cf7cfb7b83c7ba)

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -1435,7 +1435,7 @@ void SDL_SetDesktopDisplayMode(SDL_VideoDisplay *display, const SDL_DisplayMode 
 {
     SDL_DisplayMode last_mode;
 
-    if (display->fullscreen_window || (display->device && display->device->setting_display_mode)) {
+    if (display->fullscreen_window || _this->setting_display_mode) {
         // This is a temporary mode change, don't save the desktop mode
         return;
     }
