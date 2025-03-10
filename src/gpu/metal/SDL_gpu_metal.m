d375d06ac275e54d67ff1478fb55685399d5fe9e
Ethan Lee <flibitijibibo@gmail.com>
2025-03-10 18:42:56 -0400

    gpu: Wrap METAL_PrepareDriver in autoreleasepool for the device

--- src/gpu/metal/SDL_gpu_metal.m
+++ src/gpu/metal/SDL_gpu_metal.m
@@ -4276,36 +4276,38 @@ static bool METAL_SupportsTextureFormat(
 
 static bool METAL_PrepareDriver(SDL_VideoDevice *this)
 {
-    if (@available(macOS 10.14, iOS 13.0, tvOS 13.0, *) && (this->Metal_CreateView != NULL)) {
-        id<MTLDevice> device = NULL;
-        bool hasHardwareSupport = false;
+    @autoreleasepool {
+        if (@available(macOS 10.14, iOS 13.0, tvOS 13.0, *) && (this->Metal_CreateView != NULL)) {
+            id<MTLDevice> device = NULL;
+            bool hasHardwareSupport = false;
 
-        // Just use the default device, we don't care about low-power support for this check
-        device = MTLCreateSystemDefaultDevice();
-        if (device == NULL) {
-            SDL_SetError("Failed to create Metal device");
-            return false;
-        }
+            // Just use the default device, we don't care about low-power support for this check
+            device = MTLCreateSystemDefaultDevice();
+            if (device == NULL) {
+                SDL_SetError("Failed to create Metal device");
+                return false;
+            }
 
 #ifdef SDL_PLATFORM_MACOS
-        hasHardwareSupport = true;
-        if (@available(macOS 10.15, *)) {
-            hasHardwareSupport = [device supportsFamily:MTLGPUFamilyMac2];
-        } else if (@available(macOS 10.14, *)) {
-            hasHardwareSupport = [device supportsFeatureSet:MTLFeatureSet_macOS_GPUFamily2_v1];
-        }
+            hasHardwareSupport = true;
+            if (@available(macOS 10.15, *)) {
+                hasHardwareSupport = [device supportsFamily:MTLGPUFamilyMac2];
+            } else if (@available(macOS 10.14, *)) {
+                hasHardwareSupport = [device supportsFeatureSet:MTLFeatureSet_macOS_GPUFamily2_v1];
+            }
 #else
-        if (@available(iOS 13.0, tvOS 13.0, *)) {
-            hasHardwareSupport = [device supportsFamily:MTLGPUFamilyApple3];
-        }
+            if (@available(iOS 13.0, tvOS 13.0, *)) {
+                hasHardwareSupport = [device supportsFamily:MTLGPUFamilyApple3];
+            }
 #endif
 
-        if (!hasHardwareSupport) {
-            SDL_SetError("Device does not meet the hardware requirements for SDL_GPU Metal");
+            if (!hasHardwareSupport) {
+                SDL_SetError("Device does not meet the hardware requirements for SDL_GPU Metal");
+            }
+            return hasHardwareSupport;
         }
-        return hasHardwareSupport;
+        return false;
     }
-    return false;
 }
 
 static void METAL_INTERNAL_InitBlitResources(
