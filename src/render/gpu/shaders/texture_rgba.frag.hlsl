aa5f53c49812a62a03f5c2287118772c5e191f89
Sam Lantinga <slouken@libsdl.org>
2025-03-10 20:59:03 -0700

    Switched GPU renderer shaders to shadercross and HLSL

---
+++ src/render/gpu/shaders/texture_rgba.frag.hlsl
@@ -0,0 +1,17 @@
+Texture2D u_texture : register(t0, space2);
+SamplerState u_sampler : register(s0, space2);
+
+struct PSInput {
+    float4 v_color : COLOR0;
+    float2 v_uv : TEXCOORD0;
+};
+
+struct PSOutput {
+    float4 o_color : SV_Target;
+};
+
+PSOutput main(PSInput input) {
+    PSOutput output;
+    output.o_color = u_texture.Sample(u_sampler, input.v_uv) * input.v_color;
+    return output;
+}
