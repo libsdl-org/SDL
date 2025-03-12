18b727ece8a682829639af779618d3e172fcd788
Rémy Tassoux <contact@rt2.fr>
2025-03-12 18:33:41 +0100

    Add progress state tests

--- src/test/SDL_test_common.c
+++ src/test/SDL_test_common.c
@@ -2456,6 +2456,31 @@ SDL_AppResult SDLTest_CommonEventMainCallbacks(SDLTest_CommonState *state, const
                 }
             }
             break;
+        case SDLK_P:
+            if (withControl) {
+                /* Ctrl-P Cycle through progress states */
+                SDL_Window *window = SDL_GetWindowFromEvent(event);
+                if (window) {
+                    state->progress_state += 1;
+                    if (state->progress_state > SDL_PROGRESS_STATE_ERROR) {
+                        state->progress_state = SDL_PROGRESS_STATE_NONE;
+                    }
+                    SDL_SetWindowProgressState(window, state->progress_state);
+                }
+            }
+            else if (withAlt)
+            {
+                /* Alt-P Increase progress value */
+                SDL_Window *window = SDL_GetWindowFromEvent(event);
+                if (window) {
+                    state->progress_value += 0.1f;
+                    if (state->progress_value > 1.f) {
+                        state->progress_value = 0.f;
+                    }
+                    SDL_SetWindowProgressValue(window, state->progress_value);
+                }
+            }
+            break;
         case SDLK_G:
             if (withControl) {
                 /* Ctrl-G toggle mouse grab */
