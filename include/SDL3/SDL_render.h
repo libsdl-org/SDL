89e4267c05744b9a8734b0f65bd4013cb35fc618
Maia <66437537+maia-s@users.noreply.github.com>
2025-03-14 17:56:17 +0100

    Fix trailing doc comments

--- include/SDL3/SDL_render.h
+++ include/SDL3/SDL_render.h
@@ -2726,13 +2726,13 @@ typedef struct SDL_GPURenderStateDesc
     SDL_GPUShader *fragment_shader; /**< The fragment shader to use when this render state is active */
 
     Sint32 num_sampler_bindings;    /**< The number of additional fragment samplers to bind when this render state is active */
-    const SDL_GPUTextureSamplerBinding *sampler_bindings;   /** Additional fragment samplers to bind when this render state is active */
+    const SDL_GPUTextureSamplerBinding *sampler_bindings;   /**< Additional fragment samplers to bind when this render state is active */
 
     Sint32 num_storage_textures;    /**< The number of storage textures to bind when this render state is active */
-    SDL_GPUTexture *const *storage_textures;    /** Storage textures to bind when this render state is active */
+    SDL_GPUTexture *const *storage_textures;    /**< Storage textures to bind when this render state is active */
 
     Sint32 num_storage_buffers;    /**< The number of storage buffers to bind when this render state is active */
-    SDL_GPUBuffer *const *storage_buffers;      /** Storage buffers to bind when this render state is active */
+    SDL_GPUBuffer *const *storage_buffers;      /**< Storage buffers to bind when this render state is active */
 } SDL_GPURenderStateDesc;
 
 /* Check the size of SDL_GPURenderStateDesc
