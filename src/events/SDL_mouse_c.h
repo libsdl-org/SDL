e5f80430377bdc0259eab1f477661216c0ffcc0a
expikr <77922942+expikr@users.noreply.github.com>
2025-03-19 02:05:17 +0800

    chore: rename integer mode field names
    (cherry picked from commit f52f982b1e9ce2f510f2c26c6119e79dbb5b20a0)

--- src/events/SDL_mouse_c.h
+++ src/events/SDL_mouse_c.h
@@ -91,6 +91,13 @@ typedef struct
     void (*ApplySystemScale)(void *internal, Uint64 timestamp, SDL_Window *window, SDL_MouseID mouseID, float *x, float *y);
     void *system_scale_data;
 
+    // integer mode data
+    Uint8 integer_mode_flags; // 1 to enable mouse quantization, 2 to enable wheel quantization
+    float integer_mode_residual_motion_x;
+    float integer_mode_residual_motion_y;
+    float integer_mode_residual_scroll_x;
+    float integer_mode_residual_scroll_y;
+
     // Data common to all mice
     SDL_Window *focus;
     float x;
@@ -98,13 +105,8 @@ typedef struct
     float x_accu;
     float y_accu;
     float last_x, last_y; // the last reported x and y coordinates
-    float xrel_frac;
-    float yrel_frac;
-    float wheel_x_frac;
-    float wheel_y_frac;
     double click_motion_x;
     double click_motion_y;
-    Uint8 integer_mode;
     bool has_position;
     bool relative_mode;
     bool relative_mode_warp_motion;
