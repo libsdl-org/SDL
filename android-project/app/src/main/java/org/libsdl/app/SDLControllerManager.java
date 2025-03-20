de60a5f3988bfd3969f6574cf72fb94e9c8ee6ac
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-03-20 19:31:48 +0100

    Revert "Make native functions be public otherwise it fails at run-time."
    This reverts commit 4fd4d89b633d281cdc66814694401a59c28826ba.
    
    This ended up not being necessary since the Android build system
    does not minify by default.
    When you're minifying, you need to use the proguard rules anyways.
    
    https://github.com/libsdl-org/SDL/pull/12553#issuecomment-2740795848

--- android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
@@ -17,23 +17,23 @@ import android.view.MotionEvent;
 import android.view.View;
 
 
-public class SDLControllerManager
+class SDLControllerManager
 {
 
-    public static native int nativeSetupJNI();
+    static native int nativeSetupJNI();
 
-    public static native void nativeAddJoystick(int device_id, String name, String desc,
+    static native void nativeAddJoystick(int device_id, String name, String desc,
                                                 int vendor_id, int product_id,
                                                 int button_mask,
                                                 int naxes, int axis_mask, int nhats, boolean can_rumble);
-    public static native void nativeRemoveJoystick(int device_id);
-    public static native void nativeAddHaptic(int device_id, String name);
-    public static native void nativeRemoveHaptic(int device_id);
-    public static native boolean onNativePadDown(int device_id, int keycode);
-    public static native boolean onNativePadUp(int device_id, int keycode);
-    public static native void onNativeJoy(int device_id, int axis,
+    static native void nativeRemoveJoystick(int device_id);
+    static native void nativeAddHaptic(int device_id, String name);
+    static native void nativeRemoveHaptic(int device_id);
+    static native boolean onNativePadDown(int device_id, int keycode);
+    static native boolean onNativePadUp(int device_id, int keycode);
+    static native void onNativeJoy(int device_id, int axis,
                                           float value);
-    public static native void onNativeHat(int device_id, int hat_id,
+    static native void onNativeHat(int device_id, int hat_id,
                                           int x, int y);
 
     protected static SDLJoystickHandler mJoystickHandler;
@@ -69,35 +69,35 @@ public class SDLControllerManager
     /**
      * This method is called by SDL using JNI.
      */
-    public static void pollInputDevices() {
+    static void pollInputDevices() {
         mJoystickHandler.pollInputDevices();
     }
 
     /**
      * This method is called by SDL using JNI.
      */
-    public static void pollHapticDevices() {
+    static void pollHapticDevices() {
         mHapticHandler.pollHapticDevices();
     }
 
     /**
      * This method is called by SDL using JNI.
      */
-    public static void hapticRun(int device_id, float intensity, int length) {
+    static void hapticRun(int device_id, float intensity, int length) {
         mHapticHandler.run(device_id, intensity, length);
     }
 
     /**
      * This method is called by SDL using JNI.
      */
-    public static void hapticRumble(int device_id, float low_frequency_intensity, float high_frequency_intensity, int length) {
+    static void hapticRumble(int device_id, float low_frequency_intensity, float high_frequency_intensity, int length) {
         mHapticHandler.rumble(device_id, low_frequency_intensity, high_frequency_intensity, length);
     }
 
     /**
      * This method is called by SDL using JNI.
      */
-    public static void hapticStop(int device_id)
+    static void hapticStop(int device_id)
     {
         mHapticHandler.stop(device_id);
     }
