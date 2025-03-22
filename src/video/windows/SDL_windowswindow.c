0b83d4f9ae996131360bd47e1c61e712d63ea684
Petar Popovic <pjoetri@gmail.com>
2025-03-23 00:48:43 +0100

    WIN_SetWindowProgressValue(): Fix value clamp

--- src/video/windows/SDL_windowswindow.c
+++ src/video/windows/SDL_windowswindow.c
@@ -2298,7 +2298,7 @@ bool WIN_SetWindowProgressValue(SDL_VideoDevice *_this, SDL_Window *window, floa
         return false;
     };
 
-    SDL_clamp(value, 0.0f, 1.f);
+    value = SDL_clamp(value, 0.0f, 1.f);
     HRESULT ret = taskbar_list->lpVtbl->SetProgressValue(taskbar_list, window->internal->hwnd, (ULONGLONG)(value * 10000.f), 10000);
     if (FAILED(ret)) {
         return WIN_SetErrorFromHRESULT("ITaskbarList3::SetProgressValue()", ret);
