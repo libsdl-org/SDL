fa0a86409c67d580598654582f6ff4c874f00810
Petar Popovic <pjoetri@gmail.com>
2025-03-23 16:32:41 +0100

    Additions to progress bar comments

--- include/SDL3/SDL_video.h
+++ include/SDL3/SDL_video.h
@@ -2825,6 +2825,7 @@ extern SDL_DECLSPEC bool SDLCALL SDL_FlashWindow(SDL_Window *window, SDL_FlashOp
  *
  * \param window the window whose progress state is to be modified.
  * \param state the progress state.
+ *              `SDL_PROGRESS_STATE_NONE` stops displaying the progress bar.
  * \returns true on success or false on failure; call SDL_GetError() for more
  *          information.
  *
@@ -2837,8 +2838,12 @@ extern SDL_DECLSPEC bool SDLCALL SDL_SetWindowProgressState(SDL_Window *window, 
 /**
  * Sets the value of the progress bar for the given window’s taskbar icon.
  *
+ * If the state is `SDL_PROGRESS_STATE_NONE` or `SDL_PROGRESS_STATE_INDETERMINATE`,
+ * it gets changed to `SDL_PROGRESS_STATE_NORMAL`.
+ *
  * \param window the window whose progress value is to be modified.
  * \param value the progress value (0.0f - start, 1.0f - end).
+ *              If the value is outside the valid range, it gets clamped.
  * \returns true on success or false on failure; call SDL_GetError() for more
  *          information.
  *
