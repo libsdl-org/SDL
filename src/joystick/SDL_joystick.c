f102ff16a204415b30994d2b77c308eaab30d30d
Sam Lantinga <slouken@libsdl.org>
2025-04-08 11:07:20 -0700

    Don't treat the Keychron K1 Pro System Control keyboard as a joystick
    Fixes https://github.com/libsdl-org/SDL/issues/12758
    
    (cherry picked from commit d29bb902f3cf0cb22b4dc313dc1003abfd9fb1be)

--- src/joystick/SDL_joystick.c
+++ src/joystick/SDL_joystick.c
@@ -277,6 +277,7 @@ static Uint32 initial_blacklist_devices[] = {
     MAKE_VIDPID(0x1532, 0x0282), // Razer Huntsman Mini Analog, non-functional DInput device
     MAKE_VIDPID(0x26ce, 0x01a2), // ASRock LED Controller
     MAKE_VIDPID(0x20d6, 0x0002), // PowerA Enhanced Wireless Controller for Nintendo Switch (charging port only)
+    MAKE_VIDPID(0x3434, 0x0211), // Keychron K1 Pro System Control
 };
 static SDL_vidpid_list blacklist_devices = {
     SDL_HINT_JOYSTICK_BLACKLIST_DEVICES, 0, 0, NULL,
