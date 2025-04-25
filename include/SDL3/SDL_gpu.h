faa2e4040629ccc7180b00d54d04cafa644023d2
Ryan C. Gordon <icculus@icculus.org>
2025-04-25 14:19:04 -0400

    gpu: Warn about Direct3D 12 texture alignment requirements.
    Fixes #12835.

--- include/SDL3/SDL_gpu.h
+++ include/SDL3/SDL_gpu.h
@@ -1347,6 +1347,12 @@ typedef struct SDL_GPUViewport
  * SDL_DownloadFromGPUTexture are used as default values respectively and data
  * is considered to be tightly packed.
  *
+ * **WARNING**: Direct3D 12 requires texture data row pitch to be 256 byte
+ * aligned, and offsets to be aligned to 512 bytes. If they are not, SDL will
+ * make a temporary copy of the data that is properly aligned, but this adds
+ * overhead to the transfer process. Apps can avoid this by aligning their
+ * data appropriately, or using a different GPU backend than Direct3D 12.
+ *
  * \since This struct is available since SDL 3.2.0.
  *
  * \sa SDL_UploadToGPUTexture
