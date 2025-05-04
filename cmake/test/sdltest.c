5e58e6380170f2e7c746c68b378496a6cdffce40
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-05-04 21:49:14 +0200

    Fix cmake/test

--- cmake/test/sdltest.c
+++ cmake/test/sdltest.c
@@ -1,9 +1,24 @@
+#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <SDL3/SDL_test.h>
 
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    return SDL_APP_SUCCESS;
+}
 
-int main(int argc, char *argv[]) {
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_SUCCESS;
+}
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
     SDLTest_CommonState state;
     SDLTest_CommonDefaultArgs(&state, argc, argv);
-    return 0;
+    return SDL_APP_SUCCESS;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
 }
