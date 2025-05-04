50d02ad732b3823185eca64db38d214b4d9c6562
Sam Lantinga <slouken@libsdl.org>
2025-05-04 10:56:10 -0700

    Updated to version 3.2.13 for development

--- include/SDL3/SDL.h
+++ include/SDL3/SDL.h
@@ -20,7 +20,7 @@
 */
 
 /**
- * Main include header for the SDL library, version 3.2.12
+ * Main include header for the SDL library, version 3.2.13
  *
  * It is almost always best to include just this one header instead of
  * picking out individual headers included here. There are exceptions to
