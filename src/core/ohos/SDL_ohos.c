4cd71f2ac18f84aa4dc69b4fd1c9713dcb993ef5
Jack253-png <guoxiuchen20170402@163.com>
2025-05-24 09:37:09 +0800

    OpenHarmony Port (napi call)

--- src/core/ohos/SDL_ohos.c
+++ src/core/ohos/SDL_ohos.c
@@ -4,8 +4,37 @@
 
 #include "napi/native_api.h"
 
+static napi_value add(napi_env env, napi_callback_info info)
+{
+    size_t argc = 2;
+    napi_value args[2] = { NULL };
+
+    napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+
+    napi_valuetype valuetype0;
+    napi_typeof(env, args[0], &valuetype0);
+
+    napi_valuetype valuetype1;
+    napi_typeof(env, args[1], &valuetype1);
+
+    double value0;
+    napi_get_value_double(env, args[0], &value0);
+
+    double value1;
+    napi_get_value_double(env, args[1], &value1);
+
+    napi_value sum;
+    napi_create_double(env, value0 + value1, &sum);
+
+    return sum;
+}
+
 static napi_value SDL_OHOS_NAPI_Init(napi_env env, napi_value exports)
 {
+    napi_property_descriptor desc[] = {
+        { "add", NULL, add, NULL, NULL, NULL, napi_default, NULL }
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
 
@@ -16,7 +45,7 @@ napi_module OHOS_NAPI_Module = {
     .nm_register_func = SDL_OHOS_NAPI_Init,
     .nm_modname = "SDL3",
     .nm_priv = ((void *)0),
-    .reserved = {0},
+    .reserved = { 0 },
 };
 
 __attribute__((constructor)) void RegisterEntryModule(void)
