2bc3ec44b1b41fdd41b97d17afa16754579c1502
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2025-06-23 22:05:02 +0300

    keyboard: Add reference to SDL_Keymod in the comments

--- include/SDL_keyboard.h
+++ include/SDL_keyboard.h
@@ -49,7 +49,7 @@ typedef struct SDL_Keysym
 {
     SDL_Scancode scancode;      /**< SDL physical key code - see SDL_Scancode for details */
     SDL_Keycode sym;            /**< SDL virtual key code - see SDL_Keycode for details */
-    Uint16 mod;                 /**< current key modifiers */
+    Uint16 mod;                 /**< current key modifiers - see SDL_Keymod for details */
     Uint32 unused;
 } SDL_Keysym;
 
