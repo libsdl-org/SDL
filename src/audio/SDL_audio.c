ff28eac5884d822be35bcc6a7296f5cb94553c7a
Ryan C. Gordon <icculus@icculus.org>
2025-07-14 00:14:15 -0400

    hints: Added SDL_HINT_LOG_BACKENDS.
    Fixes #13354.

--- src/audio/SDL_audio.c
+++ src/audio/SDL_audio.c
@@ -1006,7 +1006,9 @@ bool SDL_InitAudio(const char *driver_name)
         }
     }
 
-    if (!initialized) {
+    if (initialized) {
+        SDL_LogBackend("audio", current_audio.name);
+    } else {
         // specific drivers will set the error message if they fail, but otherwise we do it here.
         if (!tried_to_init) {
             if (driver_name) {
