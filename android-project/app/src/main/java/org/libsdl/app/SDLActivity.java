2db8436d8cb90f81c18b73e4c2793e32cccf4a31
Sam Lantinga <slouken@libsdl.org>
2025-09-01 15:12:02 -0700

    Updated to version 2.32.11 for development

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -61,7 +61,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     private static final String TAG = "SDL";
     private static final int SDL_MAJOR_VERSION = 2;
     private static final int SDL_MINOR_VERSION = 32;
-    private static final int SDL_MICRO_VERSION = 10;
+    private static final int SDL_MICRO_VERSION = 11;
 /*
     // Display InputType.SOURCE/CLASS of events and devices
     //
--- include/SDL_version.h
+++ include/SDL_version.h
@@ -58,7 +58,7 @@ typedef struct SDL_version
 */
 #define SDL_MAJOR_VERSION   2
 #define SDL_MINOR_VERSION   32
-#define SDL_PATCHLEVEL      10
+#define SDL_PATCHLEVEL      11
 
 /**
  * Macro to determine SDL version program was compiled against.
