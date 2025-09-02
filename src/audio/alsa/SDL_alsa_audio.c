05c9b16183743925e59a489c4348141c8a050603
Ozkan Sezer <sezeroz@gmail.com>
2025-09-02 17:02:28 +0300

    alsa: fix signature of snd_pcm_info_free
    Reference issue: https://github.com/libsdl-org/SDL/issues/13845.

--- src/audio/alsa/SDL_alsa_audio.c
+++ src/audio/alsa/SDL_alsa_audio.c
@@ -88,7 +88,7 @@ static const char *(*ALSA_snd_pcm_info_get_name)(const snd_pcm_info_t *);
 static int (*ALSA_snd_pcm_info_get_card)(const snd_pcm_info_t *);
 static int (*ALSA_snd_card_get_name)(int, char **);
 static int (*ALSA_snd_pcm_info_malloc)(snd_pcm_info_t **);
-static int (*ALSA_snd_pcm_info_free)(snd_pcm_info_t *);
+static void (*ALSA_snd_pcm_info_free)(snd_pcm_info_t *);
 #ifdef SND_CHMAP_API_VERSION
 static snd_pcm_chmap_t *(*ALSA_snd_pcm_get_chmap)(snd_pcm_t *);
 static int (*ALSA_snd_pcm_chmap_print)(const snd_pcm_chmap_t *map, size_t maxlen, char *buf);
