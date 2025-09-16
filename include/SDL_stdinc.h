f1d7e28f73d1792af772f9f10ee5ca118b3f0c64
krnl32 <krnl32@protonmail.com>
2025-09-16 13:48:27 -0400

    Added a default case to the switch statement in the HEADER FILE to prevent compiler warnings about missing defaults. This change helps avoid compilation breaks when warnings are treated as errors.

--- include/SDL_stdinc.h
+++ include/SDL_stdinc.h
@@ -584,6 +584,7 @@ SDL_FORCE_INLINE void SDL_memset4(void *dst, Uint32 val, size_t dwords)
         return;
     }
     switch (dwords % 4) {
+        default:
         case 0: do {    *_p++ = _val;   SDL_FALLTHROUGH;
         case 3:         *_p++ = _val;   SDL_FALLTHROUGH;
         case 2:         *_p++ = _val;   SDL_FALLTHROUGH;
