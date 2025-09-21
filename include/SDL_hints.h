f172ee3d0e794a6fde7a472a2a572baabcdeccf5
Fierelier <fierelier@gmail.com>
2025-09-21 14:50:14 +0000

    [SDL3] [PS2] Framebuffer resolution + 240p/480p + PAL support (#13993)
    * Do not override NTSC/PAL
    
    * Fix PS2 build instructions
    
    * Add PS2 GS hints
    Allows for switching between NTSC/PAL, progressive/interlaced, etc
    
    (cherry picked from commit 3fd0b46215edb263a21989ff93ea080c1c5a1f59)
    (cherry picked from commit 7b28fb29bd9931fd049a732563692e6ab4ad9e49)

--- include/SDL_hints.h
+++ include/SDL_hints.h
@@ -2025,6 +2025,37 @@ extern "C" {
 #define SDL_HINT_ROG_GAMEPAD_MICE_EXCLUDED "SDL_ROG_GAMEPAD_MICE_EXCLUDED"
 
 /**
+ * Variable controlling the width of the PS2's framebuffer in pixels
+ *
+ * By default, this variable is "640"
+ */
+#define SDL_HINT_PS2_GS_WIDTH    "SDL_PS2_GS_WIDTH"
+
+/**
+ * Variable controlling the height of the PS2's framebuffer in pixels
+ *
+ * By default, this variable is "448"
+ */
+#define SDL_HINT_PS2_GS_HEIGHT    "SDL_PS2_GS_HEIGHT"
+
+/**
+ * Variable controlling whether the signal is interlaced or progressive
+ *
+ * - "0": Image is interlaced. (default)
+ * - "1": Image is progressive
+ */
+#define SDL_HINT_PS2_GS_PROGRESSIVE    "SDL_PS2_GS_PROGRESSIVE"
+
+/**
+ * Variable controlling the video mode of the console
+ *
+ * - "": Console-native. (default)
+ * - "NTSC": 60hz region
+ * - "PAL": 50hz region
+ */
+#define SDL_HINT_PS2_GS_MODE    "SDL_PS2_GS_MODE"
+
+/**
  * A variable controlling if VSYNC is automatically disable if doesn't reach
  * the enough FPS
  *
