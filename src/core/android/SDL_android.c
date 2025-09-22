12b97944f353631c2978f667d812233cb1014b84
Brenton Bostick <bostick@gmail.com>
2025-09-22 08:56:07 -0400

    fix: Extra parameter(s) for call to 'getCacheDir()'.
    (cherry picked from commit 6590a5bc0be05c5550d2f9530c0957de7accc60c)

--- src/core/android/SDL_android.c
+++ src/core/android/SDL_android.c
@@ -2418,7 +2418,7 @@ const char *SDL_GetAndroidCachePath(void)
         // fileObj = context.getExternalFilesDir();
         mid = (*env)->GetMethodID(env, (*env)->GetObjectClass(env, context),
                                   "getCacheDir", "()Ljava/io/File;");
-        fileObject = (*env)->CallObjectMethod(env, context, mid, NULL);
+        fileObject = (*env)->CallObjectMethod(env, context, mid);
         if (!fileObject) {
             SDL_SetError("Couldn't get cache directory");
             LocalReferenceHolder_Cleanup(&refs);
