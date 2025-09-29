f61e271e113e931f3cb5fff158ab9df23f351a9b
Sam Lantinga <slouken@libsdl.org>
2025-09-29 06:55:36 -0700

    Generalize and validate optional GPU feature properties
    Fixes https://github.com/libsdl-org/SDL/issues/13139
    Fixes https://github.com/libsdl-org/SDL/issues/13138

--- src/gpu/SDL_sysgpu.h
+++ src/gpu/SDL_sysgpu.h
@@ -1096,6 +1096,9 @@ struct SDL_GPUDevice
 
     // Store this for SDL_gpu.c's debug layer
     bool debug_mode;
+    bool validate_feature_depth_clamp_disabled;
+    bool validate_feature_indirect_draw_first_instance_disabled;
+    bool validate_feature_anisotropy_disabled;
 };
 
 #define ASSIGN_DRIVER_FUNC(func, name) \
