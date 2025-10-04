libsdl-org/SDL

commit 65b367216e8c8672df9b3816d88cf7bfd073828e

unix: Mark SDL_FRIBIDI_DYNAMIC as a weak dependency
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/core/unix/SDL_fribidi.c
@@ -25,6 +25,13 @@
 #include "SDL_fribidi.h"
 #include <fribidi.h>
 
+SDL_ELF_NOTE_DLOPEN(
+    "fribidi",
+    "Bidirectional text support",
+    SDL_ELF_NOTE_DLOPEN_PRIORITY_SUGGESTED,
+    SDL_FRIBIDI_DYNAMIC
+);
+
 SDL_FriBidi *SDL_FriBidi_Create(void) {
 	SDL_FriBidi *fribidi;
 

commit 395f32b442cc6264913798e3880250b4d6f98287

Only emit dlopen note for SDL_FRIBIDI_DYNAMIC if not a hard dependency
Fixes: 65b36721 "unix: Mark SDL_FRIBIDI_DYNAMIC as a weak dependency"
Thanks: Anonymous Maarten

src/core/unix/SDL_fribidi.c
@@ -25,12 +25,14 @@
 #include "SDL_fribidi.h"
 #include <fribidi.h>
 
+#ifdef SDL_FRIBIDI_DYNAMIC
 SDL_ELF_NOTE_DLOPEN(
     "fribidi",
     "Bidirectional text support",
     SDL_ELF_NOTE_DLOPEN_PRIORITY_SUGGESTED,
     SDL_FRIBIDI_DYNAMIC
 );
+#endif
 
 SDL_FriBidi *SDL_FriBidi_Create(void) {
 	SDL_FriBidi *fribidi;