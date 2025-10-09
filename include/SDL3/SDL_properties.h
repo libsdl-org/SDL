9a7cd95ac3cd616fde00b39dd9dea8c76355a6b0
Ryan C. Gordon <icculus@icculus.org>
2025-10-08 21:50:18 -0400

    properties: Added SDL_PROP_NAME_STRING.
    Fixes #14155.

--- include/SDL3/SDL_properties.h
+++ include/SDL3/SDL_properties.h
@@ -81,6 +81,22 @@ typedef enum SDL_PropertyType
 } SDL_PropertyType;
 
 /**
+ * A generic property for naming things.
+ *
+ * This property is intended to be added to any property set that needs the
+ * set named, or needs a generic name for the object that the properties are
+ * associated with. It is not guaranteed that any property set will include
+ * this property, but it is convenient to have a standard property name that
+ * any piece of code could reasonable agree to use.
+ *
+ * There is no format for the value set with this key; it is expected to be
+ * human-readable.
+ *
+ * \since This macro is available since SDL 3.4.0.
+ */
+#define SDL_PROP_NAME_STRING "SDL.name"
+
+/**
  * Get the global SDL properties.
  *
  * \returns a valid property ID on success or 0 on failure; call
