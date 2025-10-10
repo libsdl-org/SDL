786115401fc943b07618cd9a05e02be3107dcd42
Sam Lantinga <slouken@libsdl.org>
2025-10-10 16:17:06 -0700

    testclipboard: make the window resizable
    This is handy to see multiple image formats on the clipboard

--- test/testclipboard.c
+++ test/testclipboard.c
@@ -46,7 +46,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("testclipboard", 640, 480, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("testclipboard", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
