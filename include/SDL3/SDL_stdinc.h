aa579849281d86427f12483ba6b45c230d380f11
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-10-15 03:17:25 +0200

    stdinc: add strtok_r prototype for clang-tidy

--- include/SDL3/SDL_stdinc.h
+++ include/SDL3/SDL_stdinc.h
@@ -6001,6 +6001,10 @@ size_t wcslcpy(wchar_t *dst, const wchar_t *src, size_t size);
 size_t wcslcat(wchar_t *dst, const wchar_t *src, size_t size);
 #endif
 
+#if !defined(HAVE_STRTOK_R) && !defined(strtok_r)
+char *strtok_r(char *str, const char *delim, char **saveptr);
+#endif
+
 #ifndef _WIN32
 /* strdup is not ANSI but POSIX, and its prototype might be hidden... */
 /* not for windows: might conflict with string.h where strdup may have
