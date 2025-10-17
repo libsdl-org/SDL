4b9f9f2f84c5683551f90fd573fd8d48e59630ac
Petar Popovic <pjoetri@gmail.com>
2025-10-17 13:20:15 +0200

    Update comment in `SDL_UserEvent` for member `type`

--- include/SDL3/SDL_events.h
+++ include/SDL3/SDL_events.h
@@ -992,7 +992,7 @@ typedef struct SDL_QuitEvent
  */
 typedef struct SDL_UserEvent
 {
-    Uint32 type;        /**< SDL_EVENT_USER through SDL_EVENT_LAST-1, Uint32 because these are not in the SDL_EventType enumeration */
+    Uint32 type;        /**< SDL_EVENT_USER through SDL_EVENT_LAST, Uint32 because these are not in the SDL_EventType enumeration */
     Uint32 reserved;
     Uint64 timestamp;   /**< In nanoseconds, populated using SDL_GetTicksNS() */
     SDL_WindowID windowID; /**< The associated window if any */
