3b80fcd1a0c62f2dc06ea4bd63e10aebc8f9e31f
Quentin Thébault <quentin.thebault@defenso.fr>
2025-10-29 16:52:32 +0000

    KMSDRM/EVDEV: add VT switching support for FreeBSD (#14346)
    Signed-off-by: Quentin Thébault <quentin.thebault@defenso.fr>

--- src/core/linux/SDL_evdev.c
+++ src/core/linux/SDL_evdev.c
@@ -329,6 +329,11 @@ void SDL_EVDEV_Poll(void)
 
     for (item = _this->first; item; item = item->next) {
         while ((len = read(item->fd, events, sizeof(events))) > 0) {
+#ifdef SDL_INPUT_FBSDKBIO
+            if (SDL_GetAtomicInt(&vt_current) == VT_THEIRS) {
+                continue;
+            }
+#endif
             len /= sizeof(events[0]);
             for (i = 0; i < len; ++i) {
                 struct input_event *event = &events[i];
