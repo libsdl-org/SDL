92cc5b56ba7b1e58c1811da908f3867f6c771b62
Sam Lantinga <slouken@libsdl.org>
2025-11-09 14:37:38 -0800

    gpu renderer: add color to the point/line vertex data
    This allows us to batch color changes in a single draw call

--- src/render/gpu/shaders/linepoint.vert.hlsl
+++ src/render/gpu/shaders/linepoint.vert.hlsl
@@ -1,14 +1,14 @@
 cbuffer Context : register(b0, space1) {
     float4x4 mvp;  // Model-View-Projection matrix
-    float4 color;  // Color
 };
 
 struct VSInput {
     float2 a_position : POSITION;
+    float4 a_color : COLOR0;
 };
 
 struct VSOutput {
-    float4 v_color : COLOR;
+    float4 v_color : COLOR0;
     float4 gl_Position : SV_POSITION;
     [[vk::builtin("PointSize")]]
     float gl_PointSize : PSIZE;
@@ -16,8 +16,8 @@ struct VSOutput {
 
 VSOutput main(VSInput input) {
     VSOutput output;
-    output.v_color = color;
     output.gl_Position = mul(mvp, float4(input.a_position, 0.0, 1.0));
     output.gl_PointSize = 1.0f;
+    output.v_color = input.a_color;
     return output;
 }
