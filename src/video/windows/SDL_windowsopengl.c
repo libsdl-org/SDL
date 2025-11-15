14d543caaba1ddb7843723ec2bc332351ddafe98
Sam Lantinga <slouken@libsdl.org>
2025-11-15 07:43:59 -0800

    Fixed EGL framebuffer colors on AMD drivers

--- src/video/windows/SDL_windowsopengl.c
+++ src/video/windows/SDL_windowsopengl.c
@@ -659,9 +659,9 @@ static bool WIN_GL_SetupWindowInternal(SDL_VideoDevice *_this, SDL_Window *windo
         *iAttr++ = WGL_TYPE_RGBA_FLOAT_ARB;
     }
 
-    if ((_this->gl_config.framebuffer_srgb_capable >= 0) && _this->gl_data->HAS_WGL_ARB_framebuffer_sRGB) {
+    if (_this->gl_data->HAS_WGL_ARB_framebuffer_sRGB) {
         *iAttr++ = WGL_FRAMEBUFFER_SRGB_CAPABLE_ARB;
-        *iAttr++ = _this->gl_config.framebuffer_srgb_capable ? GL_TRUE : GL_FALSE;
+        *iAttr++ = (_this->gl_config.framebuffer_srgb_capable > 0) ? GL_TRUE : GL_FALSE;
     }
 
     /* We always choose either FULL or NO accel on Windows, because of flaky
