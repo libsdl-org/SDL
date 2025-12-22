09af48d08d01218663588dffba018a6c45288b6e
Edgar J San Martin <29460583+ej-sanmartin@users.noreply.github.com>
2025-12-22 01:32:10 -0500

    Clarify misleading comment, clarifying no copy between depth and color and suggest workaround.

--- include/SDL3/SDL_gpu.h
+++ include/SDL3/SDL_gpu.h
@@ -3903,8 +3903,8 @@ extern SDL_DECLSPEC void SDLCALL SDL_UploadToGPUBuffer(
  * This copy occurs on the GPU timeline. You may assume the copy has finished
  * in subsequent commands.
  *
- * This function does not support depth or depth-stencil textures. For those,
- * you must manually manage multiple depth textures.
+ * This function does not support copying between depth and color textures. For
+ * those, copy the texture to a buffer and then to the destination texture.
  *
  * \param copy_pass a copy pass handle.
  * \param source a source texture region.
