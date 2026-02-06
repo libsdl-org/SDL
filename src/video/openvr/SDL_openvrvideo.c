879941e294e927687d64518fa998d90a8c780a1d
Max Seidenstücker <max.seidenstuecker@gmail.com>
2026-02-06 16:20:00 +0100

    strict prototypes
    clang: a function declaration without a prototype is deprecated in all versions of C [-Wstrict-prototypes]

--- src/video/openvr/SDL_openvrvideo.c
+++ src/video/openvr/SDL_openvrvideo.c
@@ -74,7 +74,7 @@ SDL_ELF_NOTE_DLOPEN(
 // For access to functions that don't get the video data context.
 SDL_VideoData * global_openvr_driver;
 
-static void InitializeMouseFunctions();
+static void InitializeMouseFunctions(void);
 
 struct SDL_CursorData
 {
@@ -92,9 +92,9 @@ static void (APIENTRY *ov_glRenderbufferStorage)(GLenum target, GLenum internalf
 static void (APIENTRY *ov_glFramebufferRenderbuffer)(GLenum target, GLenum attachment, GLenum renderbuffertarget, GLuint renderbuffer);
 static void (APIENTRY *ov_glFramebufferTexture2D)(GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level);
 static GLenum (APIENTRY *ov_glCheckNamedFramebufferStatus)(GLuint framebuffer, GLenum target);
-static GLenum (APIENTRY *ov_glGetError)();
-static void (APIENTRY *ov_glFlush)();
-static void (APIENTRY *ov_glFinish)();
+static GLenum (APIENTRY *ov_glGetError)(void);
+static void (APIENTRY *ov_glFlush)(void);
+static void (APIENTRY *ov_glFinish)(void);
 static void (APIENTRY *ov_glGenTextures)(GLsizei n, GLuint *textures);
 static void (APIENTRY *ov_glDeleteTextures)(GLsizei n, GLuint *textures);
 static void (APIENTRY *ov_glTexParameterf)(GLenum target, GLenum pname, GLfloat param);
@@ -1431,7 +1431,7 @@ static bool OPENVR_ShowMessageBox(SDL_VideoDevice *_this,const SDL_MessageBoxDat
     return true;
 }
 
-static void InitializeMouseFunctions()
+static void InitializeMouseFunctions(void)
 {
     SDL_Mouse *mouse = SDL_GetMouse();
     mouse->CreateCursor = OPENVR_CreateCursor;
