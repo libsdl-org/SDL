e6650c91dfdc3f3bab6b65b1a87c9669bceadf77
Alynne <alviscera@gmail.com>
2026-04-30 22:47:23 -0400

    License attr

--- src/joystick/hidapi/SDL_hidapi_steam_triton.c
+++ src/joystick/hidapi/SDL_hidapi_steam_triton.c
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 2023 Max Maisel <max.maisel@posteo.de>
+  Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
