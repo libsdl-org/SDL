ecede8f2be1facd801d810e3bd7c5f130dea4057
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2026-05-05 22:06:21 +0800

    update dynapi for SVE2

--- src/dynapi/SDL_dynapi_overrides.h
+++ src/dynapi/SDL_dynapi_overrides.h
@@ -1312,3 +1312,4 @@
 #define SDL_GDKSuspendRenderer SDL_GDKSuspendRenderer_REAL
 #define SDL_GDKResumeRenderer SDL_GDKResumeRenderer_REAL
 #define SDL_IsPhone SDL_IsPhone_REAL
+#define SDL_HasSVE2 SDL_HasSVE2_REAL
