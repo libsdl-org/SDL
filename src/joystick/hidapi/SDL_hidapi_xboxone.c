1e179615757fb37bca2c0a3e3ed29b3f91e67ce5
QwertyChouskie <qwertychouskie@users.noreply.github.com>
2026-06-09 01:26:28 -0700

    SDL_hidapi_xboxone.c: Respect SDL_HINT_JOYSTICK_MFI
    This fixes being able to disable MFI on macOS without recompiling SDL itself, e.g. with `
    SDL_JOYSTICK_MFI=0` at runtime.
    
    Same fix logic as https://github.com/libsdl-org/SDL/commit/0e4566a4aea72b4fed778a9e1181ca6aace1389b for SDL_hidapi_xbox360.c

--- src/joystick/hidapi/SDL_hidapi_xboxone.c
+++ src/joystick/hidapi/SDL_hidapi_xboxone.c
@@ -368,10 +368,11 @@ static bool HIDAPI_DriverXboxOne_IsSupportedDevice(SDL_HIDAPI_Device *device, co
     static const int XBONE_IFACE_PROTOCOL = 208;
 
 #if defined(SDL_PLATFORM_MACOS) && defined(SDL_JOYSTICK_MFI)
-    if (!SDL_IsJoystickBluetoothXboxOne(vendor_id, product_id)) {
+    if (SDL_GetHintBoolean(SDL_HINT_JOYSTICK_MFI, true) &&
+        !SDL_IsJoystickBluetoothXboxOne(vendor_id, product_id)) {
         // On macOS we get a shortened version of the real report and
         // you can't write output reports for wired controllers, so
-        // we'll just use the GCController support instead.
+        // we'll just use the GCController support instead, if available.
         return false;
     }
 #endif
