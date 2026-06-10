6fb6ab82feb42384ae8d5222614e471b3b0be6e7
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-06-10 09:07:08 +0200

    OpenBSD needed fpic

--- .github/workflows/create-test-plan.py
+++ .github/workflows/create-test-plan.py
@@ -879,6 +879,7 @@ def spec_to_job(spec: JobSpec, key: str, trackmem_symbol_names: bool, ctest_args
                     job.cpactions_arch = "x86-64"
                     job.cpactions_setup_cmd = "sudo pkg_add -u"
                     job.cpactions_install_cmd = "sudo pkg_add cmake ninja pkgconf wayland wayland-protocols libxkbcommon libinotify pulseaudio dbus ibus"
+                    fpic = True
         case SdlPlatform.NGage:
             build_parallel = False
             job.cmake_build_type = "Release"
