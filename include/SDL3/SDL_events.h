b0d284439c52e7353b2263f8659e20599024c0ab
brentfpage <brentfpage@gmail.com>
2026-06-17 08:43:31 -0700

    make documentation for mobile-only members of SDL_PinchFingerEvent more explicit

--- include/SDL3/SDL_events.h
+++ include/SDL3/SDL_events.h
@@ -795,6 +795,8 @@ typedef struct SDL_TouchFingerEvent
 
 /**
  * Pinch event structure (event.pinch.*)
+ *
+ * span_(x/y) and focus_(x/y) are only available for pinch gestures on mobile devices
  */
 typedef struct SDL_PinchFingerEvent
 {
@@ -803,10 +805,10 @@ typedef struct SDL_PinchFingerEvent
     Uint64 timestamp;   /**< In nanoseconds, populated using SDL_GetTicksNS() */
     float scale;        /**< The scale change since the last SDL_EVENT_PINCH_UPDATE. Scale < 1 is "zoom out". Scale > 1 is "zoom in". */
     SDL_WindowID windowID; /**< The window underneath the finger, if any */
-    float span_x;        /**< The average X distance between each of the pointers forming the pinch in screen pixel coordinates.  Or, -1 if this information is unavailable. */
-    float span_y;        /**< The average Y distance between each of the pointers forming the pinch in screen pixel coordinates.  Or, -1 if this information is unavailable. */
-    float focus_x;        /**< The X coordinate of the current gesture's focal point in screen pixel coordinates.  Or, -1 if this information is unavailable. */
-    float focus_y;        /**< The Y coordinate of the current gesture's focal point in screen pixel coordinates.  Or, -1 if this information is unavailable. */
+    float span_x;        /**< On mobile devices, the average X distance between each of the pointers forming the pinch in screen pixel coordinates.  Otherwise, -1. */
+    float span_y;        /**< On mobile devices, the average Y distance between each of the pointers forming the pinch in screen pixel coordinates.  Otherwise, -1. */
+    float focus_x;        /**< On mobile devices, the X coordinate of the current gesture's focal point in screen pixel coordinates.  Otherwise, -1. */
+    float focus_y;        /**< On mobile devices, the Y coordinate of the current gesture's focal point in screen pixel coordinates.  Otherwise, -1. */
 } SDL_PinchFingerEvent;
 
 /**
