1fbce8a06a0071df0d43aad1330c21e0c3987da3
Ryan C. Gordon <icculus@icculus.org>
2026-06-22 22:11:49 -0400

    locale: Fixed `\since` on SDL_Locale.

--- include/SDL_locale.h
+++ include/SDL_locale.h
@@ -47,7 +47,7 @@ extern "C" {
  * would be "en"), and the country, if not NULL, will be an ISO-3166 country
  * code (so Canada would be "CA").
  *
- * \since This function is available since SDL 3.2.0.
+ * \since This struct is available since SDL 2.0.14.
  *
  * \sa SDL_GetPreferredLocales
  */
