bd7c9e467bed6ec5103925fc1fb0a9e3703d6e54
Sam Lantinga <slouken@libsdl.org>
2026-07-01 15:53:18 -0700

    Removed helpCursor and cellCursor NSCursor selectors
    These don't actually exist and we don't have a better fallback.

--- src/video/cocoa/SDL_cocoamouse.m
+++ src/video/cocoa/SDL_cocoamouse.m
@@ -238,10 +238,10 @@ static SDL_Cursor *Cocoa_CreateSystemCursor(SDL_SystemCursor id)
             nscursor = [NSCursor contextualMenuCursor];
             break;
         case SDL_SYSTEM_CURSOR_HELP:
-            nscursor = LoadHiddenSystemCursor(@"help", @selector(helpCursor));
+            nscursor = LoadHiddenSystemCursor(@"help", @selector(arrowCursor));
             break;
         case SDL_SYSTEM_CURSOR_CELL:
-            nscursor = LoadHiddenSystemCursor(@"cell", @selector(cellCursor));
+            nscursor = LoadHiddenSystemCursor(@"cell", @selector(arrowCursor));
             break;
         case SDL_SYSTEM_CURSOR_VERTICAL_TEXT:
             nscursor = [NSCursor IBeamCursorForVerticalLayout];
