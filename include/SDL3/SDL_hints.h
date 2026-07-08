782d6bc1163733d6267887d82e0ae73f224a0323
Sam Lantinga <slouken@libsdl.org>
2026-07-08 16:07:08 -0700

    Note that Windows doesn't see mouse button releases when RIDEV_NOLEGACY is active.

--- include/SDL3/SDL_hints.h
+++ include/SDL3/SDL_hints.h
@@ -4758,6 +4758,8 @@ extern "C" {
  * players are using high DPI mice, but should be disabled while showing
  * custom assert dialogs in your application code.
  *
+ * Caution: Windows will not see mouse button releases in relative mode with this active. This means you should not enable relative mode while a mouse button is currently pressed.
+ *
  * - "0": Windows mouse events will be generated while relative motion is
  *   enabled. (default)
  * - "1": Windows mouse events will not be generated while relative motion is
