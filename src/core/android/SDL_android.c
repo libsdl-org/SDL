74ae1ab487c17d258afd7c100762939b3ed78eab
Hadrian Tang <hadrianwttang@outlook.com>
2026-07-19 03:45:58 +0800

    Place the clear first for multithreaded scenarios

--- src/core/android/SDL_android.c
+++ src/core/android/SDL_android.c
@@ -1313,9 +1313,9 @@ retry:
 #endif
 
         if (data->native_window) {
+            SDL_SetPointerProperty(SDL_GetWindowProperties(Android_Window), SDL_PROP_WINDOW_ANDROID_WINDOW_POINTER, NULL);
             ANativeWindow_release(data->native_window);
             data->native_window = NULL;
-            SDL_SetPointerProperty(SDL_GetWindowProperties(Android_Window), SDL_PROP_WINDOW_ANDROID_WINDOW_POINTER, NULL);
         }
 
         // GL Context handling is done in the event loop because this function is run from the Java thread
