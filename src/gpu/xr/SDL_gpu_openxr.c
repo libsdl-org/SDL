eeb832d8a1784fa06a2ceff5cbc3b49ee4e164d5
Evan Hemsley <2342303+thatcosmonaut@users.noreply.github.com>
2026-08-03 13:11:32 -0700

    GPU OpenXR: Fix zero-length copy (#16112)

--- src/gpu/xr/SDL_gpu_openxr.c
+++ src/gpu/xr/SDL_gpu_openxr.c
@@ -115,7 +115,9 @@ XrResult SDL_OPENXR_INTERNAL_GPUInitOpenXR(
 
     // allocate enough space for the validation layer + the user's api layers
     const char **apiLayerNames = SDL_stack_alloc(const char *, userApiLayerCount + 1);
-    SDL_memcpy((void *)apiLayerNames, userApiLayerNames, sizeof(const char *) * (userApiLayerCount));
+    if (userApiLayerCount > 0) {
+        SDL_memcpy((void *)apiLayerNames, userApiLayerNames, sizeof(const char *) * userApiLayerCount);
+    }
     apiLayerNames[userApiLayerCount] = VALIDATION_LAYER_API_NAME;
 
     // On Android, we need an extra extension for android_create_instance
