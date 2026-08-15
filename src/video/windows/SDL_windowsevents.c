9155d194b6cce3278616ac5f1fffc2844866a23f
Caleb Cornett <caleb.cornett@outlook.com>
2026-08-15 14:26:20 -0400

    gdk: Send window resize events on Xbox

--- src/video/windows/SDL_windowsevents.c
+++ src/video/windows/SDL_windowsevents.c
@@ -2499,7 +2499,18 @@ LRESULT CALLBACK WIN_WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
             WIN_UpdateDisplayUsableBounds(SDL_GetVideoDevice());
         }
         break;
+#else
+    case WM_SIZE:
+    {
+        RECT rect;
+        int w, h;
 
+        if (GetClientRect(hwnd, &rect) && WIN_WindowRectValid(&rect)) {
+            w = rect.right;
+            h = rect.bottom;
+            SDL_SendWindowEvent(data->window, SDL_EVENT_WINDOW_RESIZED, w, h);
+        }
+    } break;
 #endif // !defined(SDL_PLATFORM_XBOXONE) && !defined(SDL_PLATFORM_XBOXSERIES)
 
     default:
