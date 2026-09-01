4aa40070ef6c58c24ac5f3bf30750058bd3bda38
Ethan Lee <flibitijibibo@gmail.com>
2026-09-01 08:25:20 -0400

    gpu: Return early when attempting to bind 0 samplers.
    This is a common occurrence for vertex shaders specifically, and allows backends
    to not have to handle this edge case every time.

--- src/gpu/SDL_gpu.c
+++ src/gpu/SDL_gpu.c
@@ -2090,6 +2090,10 @@ void SDL_BindGPUVertexSamplers(
         }
     }
 
+    if (num_bindings == 0) {
+        return;
+    }
+
     RENDERPASS_DEVICE->BindVertexSamplers(
         RENDERPASS_COMMAND_BUFFER,
         first_slot,
@@ -2195,6 +2199,10 @@ void SDL_BindGPUFragmentSamplers(
         for (Uint32 i = 0; i < num_bindings; i += 1) {
             ((RenderPass *)render_pass)->fragment_sampler_bound[first_slot + i] = true;
         }
+    }
+
+    if (num_bindings == 0) {
+        return;
     }
 
     RENDERPASS_DEVICE->BindFragmentSamplers(
