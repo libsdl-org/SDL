fd30f06df47c141b1a78074d04867d0d46247da4
Frank Praznik <frank.praznik@gmail.com>
2026-09-13 12:33:57 -0400

    dbus: Use the minimum privileges when opening an fd for the OpenURI portal
    Open files and directories as read-only, as the fd is only used as proof of access by the portal, and a mismatch between fd write permissions and the portal write parameter will cause the fd to be rejected.
    
    (cherry picked from commit fb8cdf0aa8c176c4935c5c007f66e83739f1c4c1)

--- src/core/linux/SDL_dbus.c
+++ src/core/linux/SDL_dbus.c
@@ -23,6 +23,7 @@
 #include "../../stdlib/SDL_vacopy.h"
 
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #ifdef SDL_USE_LIBDBUS
@@ -510,7 +511,19 @@ bool SDL_DBus_OpenURI(const char *uri, const char *window_id, const char *activa
             }
             uri = decoded_path;
         }
-        fd = open(uri, O_RDWR | O_CLOEXEC);
+
+        struct stat st;
+        if (stat(uri, &st) == 0) {
+            /* Open files and directories as read-only, as the fd is only used as proof
+             * of access by the portal, and a mismatch between fd write permissions and
+             * the portal write parameter will cause the fd to be rejected.
+             */
+            int oflags = O_RDONLY | O_CLOEXEC;
+            if (S_ISDIR(st.st_mode)) {
+                oflags |= O_DIRECTORY;
+            }
+            fd = open(uri, oflags);
+        }
         SDL_free(decoded_path);
         if (fd >= 0) {
             msg = dbus.message_new_method_call(bus_name, path, interface, "OpenFile");
