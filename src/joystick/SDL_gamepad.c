367a161778b74b4e7f6c66d9f1a978d88a5a1738
wankey <chengwq@flydigi.com>
2026-09-18 15:41:53 +0800

    Flydigi Apex 6 serials support

--- src/joystick/SDL_gamepad.c
+++ src/joystick/SDL_gamepad.c
@@ -1300,7 +1300,7 @@ static GamepadMapping_t *SDL_CreateMappingForHIDAPIGamepad(SDL_GUID guid)
                     // Vader 5 has additional shoulder macro buttons and a circle button
                     SDL_strlcat(mapping_string, "misc4:b17,misc5:b18,misc6:b19", sizeof(mapping_string));
                 }
-            } else if (guid.data[15] == SDL_FLYDIGI_APEX5) {
+            } else if (guid.data[15] == SDL_FLYDIGI_APEX5 || guid.data[15] == SDL_FLYDIGI_APEX6) {
                 // Apex 5 has additional shoulder macro buttons
                 SDL_strlcat(mapping_string, "misc2:b15,misc3:b16,", sizeof(mapping_string));
             }
