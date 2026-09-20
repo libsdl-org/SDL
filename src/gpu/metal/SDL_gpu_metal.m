libsdl-org/SDL

commit 6772e3fd17472d0c50ccb7ab0c824392f3016889

Fixed Metal ARC leak when freeing GPU window data (thanks @imjakey!)
Fixes https://github.com/libsdl-org/SDL/issues/16345

src/gpu/metal/SDL_gpu_metal.m
@@ -3837,6 +3837,10 @@ static void METAL_ReleaseWindow(
         }
         SDL_UnlockMutex(renderer->windowLock);
 
+        windowData->layer = nil;
+        windowData->drawable = nil;
+        windowData->texture.hanlde = nil;
+
         SDL_free(windowData);
 
         SDL_ClearProperty(SDL_GetWindowProperties(window), WINDOW_PROPERTY_DATA);

commit 0e924911de3923b2a2ecaeb2a4de49458c85d2a2

Fix typo in src/gpu/metal/SDL_gpu_metal.m
Introduced by 6772e3fd17472d0c50ccb7ab0c824392f3016889

src/gpu/metal/SDL_gpu_metal.m
@@ -3839,7 +3839,7 @@ static void METAL_ReleaseWindow(
 
         windowData->layer = nil;
         windowData->drawable = nil;
-        windowData->texture.hanlde = nil;
+        windowData->texture.handle = nil;
 
         SDL_free(windowData);
 
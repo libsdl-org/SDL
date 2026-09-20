0e924911de3923b2a2ecaeb2a4de49458c85d2a2
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-09-20 20:19:00 +0200

    Fix typo in src/gpu/metal/SDL_gpu_metal.m
    Introduced by 6772e3fd17472d0c50ccb7ab0c824392f3016889

--- src/gpu/metal/SDL_gpu_metal.m
+++ src/gpu/metal/SDL_gpu_metal.m
@@ -3839,7 +3839,7 @@ static void METAL_ReleaseWindow(
 
         windowData->layer = nil;
         windowData->drawable = nil;
-        windowData->texture.hanlde = nil;
+        windowData->texture.handle = nil;
 
         SDL_free(windowData);
 
