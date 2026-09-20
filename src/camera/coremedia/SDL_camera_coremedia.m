f48d1e245f520eca1d0599da7111a38644424edc
David Amador <423769+DJLink@users.noreply.github.com>
2026-09-20 11:29:56 +0100

    Fix for iOS 27 no op on statusBarOrientation check in favor of didUpdateEffectiveGeometry from iOS 27.X forward
    statusBarOrientation has been depracated since iOS 13, but it's now a no-op in SDK27

--- src/camera/coremedia/SDL_camera_coremedia.m
+++ src/camera/coremedia/SDL_camera_coremedia.m
@@ -35,6 +35,7 @@
 
 #ifdef USE_UIKIT_DEVICE_ROTATION
 #import <UIKit/UIKit.h>
+#include "../../video/uikit/SDL_uikitvideo.h"
 #endif
 
 /*
@@ -238,10 +239,7 @@ static SDL_CameraFrameResult COREMEDIA_AcquireFrame(SDL_Camera *device, SDL_Surf
         hidden.last_device_orientation = device_orientation;  // update the last known-good orientation for later.
     }
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    const UIInterfaceOrientation ui_orientation = [UIApplication sharedApplication].statusBarOrientation;
-#pragma clang diagnostic pop
+    const UIInterfaceOrientation ui_orientation = UIKit_GetInterfaceOrientation();
 
     // there is probably math for this, but this is easy to slap into a table.
     // rotation = rotations[uiorientation-1][devorientation-1];
@@ -472,10 +470,7 @@ static bool COREMEDIA_OpenDevice(SDL_Camera *device, const SDL_CameraSpec *spec)
     hidden.last_device_orientation = uidevice.orientation;
     if (!UIDeviceOrientationIsValidInterfaceOrientation(hidden.last_device_orientation)) {
         // accelerometer isn't ready yet or the phone is laying flat or something. Just try to guess from how the UI is oriented at the moment.
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        switch ([UIApplication sharedApplication].statusBarOrientation) {
-#pragma clang diagnostic pop
+        switch (UIKit_GetInterfaceOrientation()) {
             case UIInterfaceOrientationPortrait: hidden.last_device_orientation = UIDeviceOrientationPortrait; break;
             case UIInterfaceOrientationPortraitUpsideDown: hidden.last_device_orientation = UIDeviceOrientationPortraitUpsideDown; break;
             case UIInterfaceOrientationLandscapeLeft: hidden.last_device_orientation = UIDeviceOrientationLandscapeRight; break;  // Apple docs say UI and device orientations are reversed in landscape.
--- src/video/uikit/SDL_uikitmodes.m
+++ src/video/uikit/SDL_uikitmodes.m
@@ -331,10 +331,7 @@ bool UIKit_IsDisplayLandscape(UIScreen *uiscreen)
 {
 #ifndef SDL_PLATFORM_TVOS
     if (uiscreen == [UIScreen mainScreen]) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        return UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation);
-#pragma clang diagnostic pop
+        return UIInterfaceOrientationIsLandscape(UIKit_GetInterfaceOrientation());
     } else
 #endif // !SDL_PLATFORM_TVOS
     {
@@ -488,10 +485,7 @@ void UIKit_QuitModes(SDL_VideoDevice *_this)
 #if !defined(SDL_PLATFORM_TVOS) && !defined(SDL_PLATFORM_VISIONOS)
 void SDL_OnApplicationDidChangeStatusBarOrientation(void)
 {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    BOOL isLandscape = UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation);
-#pragma clang diagnostic pop
+    BOOL isLandscape = UIInterfaceOrientationIsLandscape(UIKit_GetInterfaceOrientation());
     SDL_VideoDisplay *display = SDL_GetVideoDisplay(SDL_GetPrimaryDisplay());
 
     if (display) {
@@ -525,10 +519,7 @@ void SDL_OnApplicationDidChangeStatusBarOrientation(void)
             }
         }
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        switch ([UIApplication sharedApplication].statusBarOrientation) {
-#pragma clang diagnostic pop
+        switch (UIKit_GetInterfaceOrientation()) {
         case UIInterfaceOrientationPortrait:
             orientation = SDL_ORIENTATION_PORTRAIT;
             break;
