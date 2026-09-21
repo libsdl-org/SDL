62aa5cc961c5a923cc0282bf708a25e8aded5033
Sam Lantinga <slouken@libsdl.org>
2026-09-21 07:21:06 -0700

    Don't use a cast, instead use the correct D3D12 resource state

--- src/gpu/d3d12/SDL_gpu_d3d12.c
+++ src/gpu/d3d12/SDL_gpu_d3d12.c
@@ -1979,7 +1979,7 @@ static void D3D12_INTERNAL_TextureTransitionToDefaultUsage(
 static D3D12_RESOURCE_STATES D3D12_INTERNAL_DefaultBufferResourceState(
     D3D12Buffer *buffer)
 {
-    D3D12_RESOURCE_STATES states = (D3D12_RESOURCE_STATES)0;
+    D3D12_RESOURCE_STATES states = D3D12_RESOURCE_STATE_COMMON;
 
     if (buffer->container->usage & SDL_GPU_BUFFERUSAGE_VERTEX) {
         states |= D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER;
