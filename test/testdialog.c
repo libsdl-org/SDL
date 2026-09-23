d75afc368e586ba01499fdff05971cdde2351563
Maksim Krylosov <aequiternus@gmail.com>
2026-09-23 15:42:20 +0300

    Added render of loaded image for OpenFile dialog test

--- test/testdialog.c
+++ test/testdialog.c
@@ -20,6 +20,7 @@
 static const SDL_FRect OPEN_FILE_RECT = { 50, 50, 220, 140 };
 static const SDL_FRect SAVE_FILE_RECT = { 50, 290, 220, 140 };
 static const SDL_FRect OPEN_FOLDER_RECT = { 370, 50, 220, 140 };
+static const SDL_Rect IMAGE_RECT = { 370, 290, 220, 140 };
 static const char DEFAULT_FILENAME[] = "Untitled.index";
 
 static const SDL_DialogFileFilter filters[] = {
@@ -29,9 +30,18 @@ static const SDL_DialogFileFilter filters[] = {
     { "PNG images", "png" }
 };
 
+typedef struct
+{
+    SDLTest_CommonState *state;
+    SDL_Texture *texture;
+    const char *initial_path;
+    char *last_saved_path;
+    bool is_save_dialog;
+} file_dialog;
+
 static void SDLCALL callback(void *userdata, const char *const *files, int filter)
 {
-    char **saved_path = userdata;
+    file_dialog *dialog = userdata;
 
     if (files) {
         const char *filter_name = "(filter fetching unsupported)";
@@ -46,11 +56,27 @@ static void SDLCALL callback(void *userdata, const char *const *files, int filte
 
         SDL_Log("Filter used: '%s'", filter_name);
 
-        if (*files && saved_path) {
-            *saved_path = SDL_strdup(*files);
-            /* Create the file */
-            SDL_IOStream *stream = SDL_IOFromFile(*saved_path, "w");
-            SDL_CloseIO(stream);
+        if (*files) {
+            if (dialog->is_save_dialog) {
+                dialog->last_saved_path = SDL_strdup(*files);
+                /* Create the file */
+                SDL_IOStream *stream = SDL_IOFromFile(dialog->last_saved_path, "w");
+                SDL_CloseIO(stream);
+            } else {
+                SDL_Surface *surface = SDL_LoadSurface(*files);
+                if (surface) {
+                    SDL_Surface *scaled = SDL_ScaleSurface(surface, IMAGE_RECT.w, IMAGE_RECT.h, SDL_SCALEMODE_LINEAR);
+                    SDL_DestroySurface(surface);
+                    if (scaled) {
+                        SDL_Surface *converted = SDL_ConvertSurface(scaled, SDL_PIXELFORMAT_RGBA8888);
+                        SDL_DestroySurface(scaled);
+                        if (converted) {
+                            SDL_UpdateTexture(dialog->texture, NULL, converted->pixels, converted->pitch);
+                            SDL_DestroySurface(converted);
+                        }
+                    }
+                }
+            }
         }
 
         while (*files) {
@@ -78,13 +104,6 @@ static char *concat_strings(const char *a, const char *b)
 
     return out;
 }
-
-typedef struct
-{
-    SDLTest_CommonState *state;
-    const char *initial_path;
-    char *last_saved_path;
-} file_dialog;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -115,6 +134,22 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         SDL_Log("Will not use an initial path, couldn't get the home directory path: %s", SDL_GetError());
     }
 
+    SDL_Renderer *renderer = dialog->state->renderers[0];
+    if (!renderer) {
+        SDL_Log("Couldn't create renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    dialog->texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, IMAGE_RECT.w, IMAGE_RECT.h);
+    if (!dialog->texture) {
+        SDL_Log("Couldn't create texture: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    } else {
+        static const Uint32 blank[220 * 140];
+        const SDL_Rect rect = { 0, 0, IMAGE_RECT.w, IMAGE_RECT.h };
+        SDL_UpdateTexture(dialog->texture, &rect, blank, IMAGE_RECT.w * sizeof(Uint32));
+    }
+
     *appstate = dialog;
     return SDL_APP_CONTINUE;
 }
@@ -122,6 +157,10 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     file_dialog *dialog = appstate;
+
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
 
     if (event->type == SDL_EVENT_MOUSE_BUTTON_UP) {
         const SDL_FPoint p = { event->button.x, event->button.y };
@@ -136,17 +175,20 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
          * - Nonzero if the user is allowed to choose multiple entries (not for SDL_ShowSaveFileDialog)
          */
         if (SDL_PointInRectFloat(&p, &OPEN_FILE_RECT)) {
-            SDL_ShowOpenFileDialog(callback, NULL, w, filters, SDL_arraysize(filters), dialog->initial_path, 1);
+            dialog->is_save_dialog = false;
+            SDL_ShowOpenFileDialog(callback, dialog, w, filters, SDL_arraysize(filters), dialog->initial_path, 1);
         } else if (SDL_PointInRectFloat(&p, &OPEN_FOLDER_RECT)) {
-            SDL_ShowOpenFolderDialog(callback, NULL, w, dialog->initial_path, 1);
+            dialog->is_save_dialog = false;
+            SDL_ShowOpenFolderDialog(callback, dialog, w, dialog->initial_path, 1);
         } else if (SDL_PointInRectFloat(&p, &SAVE_FILE_RECT)) {
+            dialog->is_save_dialog = true;
             char *save_path = NULL;
             if (dialog->last_saved_path) {
                 save_path = SDL_strdup(dialog->last_saved_path);
             } else {
                 save_path = concat_strings(dialog->initial_path, DEFAULT_FILENAME);
             }
-            SDL_ShowSaveFileDialog(callback, &dialog->last_saved_path, w, filters, SDL_arraysize(filters), save_path ? save_path : DEFAULT_FILENAME);
+            SDL_ShowSaveFileDialog(callback, dialog, w, filters, SDL_arraysize(filters), save_path ? save_path : DEFAULT_FILENAME);
             SDL_free(save_path);
         }
     }
@@ -177,6 +219,12 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         SDLTest_DrawString(r, OPEN_FILE_RECT.x + 5, OPEN_FILE_RECT.y + OPEN_FILE_RECT.h / 2, "Open File...");
         SDLTest_DrawString(r, SAVE_FILE_RECT.x + 5, SAVE_FILE_RECT.y + SAVE_FILE_RECT.h / 2, "Save File...");
         SDLTest_DrawString(r, OPEN_FOLDER_RECT.x + 5, OPEN_FOLDER_RECT.y + OPEN_FOLDER_RECT.h / 2, "Open Folder...");
+
+        if (i == 0) {
+            SDL_FRect rect;
+            SDL_RectToFRect(&IMAGE_RECT, &rect);
+            SDL_RenderTexture(r, dialog->texture, NULL, &rect);
+        }
 
         SDL_RenderPresent(r);
     }
